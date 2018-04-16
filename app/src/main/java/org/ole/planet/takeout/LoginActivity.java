f2c51bafac27f99b16a262c350d2b0a96926c6f5
Leonard <leomaxi@outlook.com>
2018-04-16 14:52:06 +0000

    Change Logo color - Animated logo color

--- app/src/main/java/org/ole/planet/takeout/LoginActivity.java
+++ app/src/main/java/org/ole/planet/takeout/LoginActivity.java
@@ -1,5 +1,7 @@
 package org.ole.planet.takeout;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
@@ -8,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.widget.ImageView;
 
 import static java.security.AccessController.getContext;
 
@@ -17,8 +20,40 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ImageView logo = (ImageView) findViewById(R.id.logoImageView);
 
+        animateImageView(logo);
 
 
     }
+
+    public void animateImageView(final ImageView v) {
+        final int newColor = getResources().getColor(android.R.color.white);
+        final ValueAnimator colorAnim = ObjectAnimator.ofFloat(0f, 1f);
+        colorAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float mul = (Float) animation.getAnimatedValue();
+                int alphaWhite = adjustAlpha(newColor, mul);
+                v.setColorFilter(alphaWhite, PorterDuff.Mode.SRC_ATOP);
+                if (mul == 0.0) {
+                    v.setColorFilter(null);
+                }
+            }
+        });
+        colorAnim.setDuration(1000);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.setRepeatCount(-1);
+        colorAnim.start();
+
+    }
+
+    public int adjustAlpha(int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
+    }
+
 }
