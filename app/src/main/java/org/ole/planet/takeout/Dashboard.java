186f22632fea898dc52e90c87d7aa1bcbcf04341
Leonard Maximus Irenous Mensah <leomaxi@outlook.com>
2018-05-16 19:09:17 +0000

    open custom fragments ( fixes #14 )

--- app/src/main/java/org/ole/planet/takeout/Dashboard.java
+++ app/src/main/java/org/ole/planet/takeout/Dashboard.java
@@ -1,66 +1,59 @@
 package org.ole.planet.takeout;
 
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
-import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
-
-import org.ole.planet.takeout.R;
-
 public class Dashboard extends AppCompatActivity {
     private Drawer result = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setBackgroundColor(Color.BLACK);
-        setSupportActionBar(toolbar);
         AccountHeader headerResult = getAccountHeader();
-        createDrawer(savedInstanceState, toolbar, headerResult);
-        result.setSelection(1, true);
-        result.addStickyFooterItem(new PrimaryDrawerItem().withName("Logout"));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+        createDrawer(headerResult);
+        //result.setSelection(1, true);
+        //result.addStickyFooterItem(new PrimaryDrawerItem().withName("Logout"));
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (Build.VERSION.SDK_INT >= 19) {
             result.getDrawerLayout().setFitsSystemWindows(false);
         }
+
+        initElements();
+        openCallFragment(new DashboardFragment());
+
+    }
+
+    private void initElements() {
+        ImageButton imgbtnHamburger = findViewById(R.id.imgbtnHamburgger);
+        imgbtnHamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.openDrawer();
+            }
+        });
     }
 
     private AccountHeader getAccountHeader() {
         //Create User profile header
         return new AccountHeaderBuilder()
                 .withActivity(this)
-                .addProfiles(
-                        new ProfileDrawerItem()
-                                .withName("Leonard Mensah")
-                                .withEmail("Learner")
-                                .withIcon(getResources().getDrawable(R.drawable.profile))
-                )
                 .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
                     @Override
                     public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
@@ -70,14 +63,12 @@ public class Dashboard extends AppCompatActivity {
                 .build();
     }
 
-    private void createDrawer(Bundle savedInstanceState, Toolbar toolbar, AccountHeader headerResult) {
-        //Create the drawer
+    private void createDrawer(AccountHeader headerResult) {
         result = new DrawerBuilder()
                 .withActivity(this)
                 .withFullscreen(true)
                 .withSliderBackgroundColor(getResources().getColor(R.color.colorPrimary))
-                .withToolbar(toolbar)
-                .withAccountHeader(headerResult)
+    //            .withAccountHeader(headerResult)
                 .addDrawerItems(
                         getDrawerItems()
                 )
@@ -86,22 +77,48 @@ public class Dashboard extends AppCompatActivity {
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         if (drawerItem != null) {
                             if (drawerItem instanceof Nameable) {
-                                Toast.makeText(Dashboard.this, ((Nameable) drawerItem).getName().getText(Dashboard.this), Toast.LENGTH_SHORT).show();
+                                menuAction(((Nameable) drawerItem).getName().getTextRes());
                             }
                         }
                         return false;
                     }
                 })
+                .withDrawerWidthDp(200)
                 .build();
     }
+
+    private void menuAction(int selectedMenuId) {
+        switch (selectedMenuId){
+            case R.string.menu_home:
+                openCallFragment(new DashboardFragment());
+                break;
+            case R.string.menu_library:
+                break;
+            case R.string.menu_meetups:
+                break;
+            case R.string.menu_surveys:
+                break;
+            case R.string.menu_courses:
+                break;
+        }
+    }
+
+    public void openCallFragment(Fragment newfragment){
+        newfragment = new DashboardFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, newfragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
     @NonNull
     private IDrawerItem[] getDrawerItems() {
         return new IDrawerItem[]{
-                new PrimaryDrawerItem().withName("Home").withIcon(getResources().getDrawable(R.drawable.home)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
-                new PrimaryDrawerItem().withName("Library").withIcon(getResources().getDrawable(R.drawable.library)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
-                new PrimaryDrawerItem().withName("Courses").withIcon(getResources().getDrawable(R.drawable.courses)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
-                new PrimaryDrawerItem().withName("Meetups").withIcon(getResources().getDrawable(R.drawable.meetups)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
-                new PrimaryDrawerItem().withName("Surveys").withIcon(getResources().getDrawable(R.drawable.survey)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
+                new PrimaryDrawerItem().withName(R.string.menu_home).withIcon(getResources().getDrawable(R.drawable.home)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
+                new PrimaryDrawerItem().withName(R.string.menu_library).withIcon(getResources().getDrawable(R.drawable.library)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
+                new PrimaryDrawerItem().withName(R.string.menu_courses).withIcon(getResources().getDrawable(R.drawable.courses)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
+                new PrimaryDrawerItem().withName(R.string.menu_meetups).withIcon(getResources().getDrawable(R.drawable.meetups)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
+                new PrimaryDrawerItem().withName(R.string.menu_surveys).withIcon(getResources().getDrawable(R.drawable.survey)).withTextColor(getResources().getColor(R.color.textColorPrimary)),
         };
     }
     @Override
--- app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
+++ app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
@@ -6,8 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.ole.planet.takeout.R;
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -15,7 +13,6 @@ public class DashboardFragment extends Fragment {
 
     public DashboardFragment() {
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
