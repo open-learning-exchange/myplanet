d8b1cbb85eb336492ab766abb5558c9ba0083f6f
amjedcha@gmail.com <19535323+onlyserenity@users.noreply.github.com>
2018-05-31 23:58:38 -0400

    Adds sync settings on login activity (fixes #49)

--- app/src/main/java/org/ole/planet/takeout/LoginActivity.java
+++ app/src/main/java/org/ole/planet/takeout/LoginActivity.java
@@ -16,10 +16,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.URLUtil;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
@@ -29,6 +34,8 @@ import com.github.kittinunf.fuel.core.FuelError;
 import com.github.kittinunf.fuel.core.Handler;
 import com.github.kittinunf.fuel.core.Request;
 import com.github.kittinunf.fuel.core.Response;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,8 +47,11 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout inputLayoutName, inputLayoutPassword;
     private Button btnSignIn;
     private ImageButton imgBtnSetting;
+    private TextView syncOption, syncDate, intervalLabel;
+    private Spinner spinner;
+    private Switch syncSwitch;
     Context context;
-    private View positiveAction;
+    private View positiveAction, positiveSync, negativeSync;
     boolean connectionResult;
     dbSetup dbsetup =  new dbSetup();
 
@@ -70,6 +80,13 @@ public class LoginActivity extends AppCompatActivity {
                 settingDialog();
             }
         });
+        syncOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                feedbackDialog();
+            }
+        });
+
         dbsetup.Setup_db(this.context);
 
     }
@@ -84,7 +101,8 @@ public class LoginActivity extends AppCompatActivity {
         //buttons
         btnSignIn = findViewById(R.id.btn_signin);
         imgBtnSetting = findViewById(R.id.imgBtnSetting);
-
+        // textviews
+        syncOption = findViewById(R.id.syncOption);
     }
 
     public int adjustAlpha(int color, float factor) {
@@ -178,6 +196,99 @@ public class LoginActivity extends AppCompatActivity {
         });
         positiveAction.setEnabled(false);
         dialog.show();
+    }
+
+
+    // Server feedback dialog
+    public void  feedbackDialog(){
+
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title(R.string.title_sync_settings)
+                .customView(R.layout.dialog_sync_feedback, true)
+                .positiveText(R.string.btn_sync)
+                .negativeText(R.string.btn_sync_cancel)
+                .neutralText(R.string.btn_sync_save).onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(MaterialDialog dialog, DialogAction which) {
+                        Log.e("MD: ", "Clicked Positive (Sync now)");
+                        Toast.makeText(LoginActivity.this, "Syncing now...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(MaterialDialog dialog, DialogAction which) {
+                        Log.e("MD: ", "Clicked Negative (Cancel)");
+                    }
+                })
+                .onNeutral(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Log.e("MD: ", "Clicked Netural (Save)");
+                        Toast.makeText(LoginActivity.this, "Saving sync settings...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .build();
+
+        // Check Autosync switch (Toggler)
+        syncSwitch = (Switch) dialog.findViewById(R.id.syncSwitch);
+        intervalLabel = (TextView) dialog.findViewById(R.id.intervalLabel);
+
+
+        syncSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.e("MD: ", "Autosync is On");
+                    intervalLabel.setVisibility(View.VISIBLE);
+                    spinner.setVisibility(View.VISIBLE);
+                } else {
+                    Log.e("MD: ", "Autosync is Off");
+                    spinner.setVisibility(View.GONE);
+                    intervalLabel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        int convertedDate = convertDate();
+
+        // Check if the user never synced
+        if (convertedDate == 0){
+            syncDate = (TextView) dialog.findViewById(R.id.lastDateSynced);
+            syncDate.setText("Last Sync Date: Never");
+        }
+        else {
+            syncDate = (TextView) dialog.findViewById(R.id.lastDateSynced);
+            syncDate.setText("Last Sync Date: " + convertedDate);
+        }
+
+        // Init spinner dropdown items
+        spinner = (Spinner) dialog.findViewById(R.id.intervalDropper);
+        syncDropdownAdd();
+
+        dialog.show();
+    }
+
+    // Converts OS date to human date
+    private int convertDate(){
+        // Context goes here
+
+        return 0; // <=== modify this when implementing this method
+    }
+    
+    // Create items in the spinner
+    public void syncDropdownAdd(){
+        List<String> list = new ArrayList<>();
+
+        list.add("15 Minutes");
+        list.add("30 Minutes");
+        list.add("1 Hour");
+        list.add("3 Hours");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
     public boolean isServerReachable(String url) {
