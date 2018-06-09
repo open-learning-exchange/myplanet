ae5fd9d2362c9f3fc5f461d4024549682d9fd211
amjedcha@gmail.com <19535323+onlyserenity@users.noreply.github.com>
2018-06-09 19:05:10 -0400

    changed sync btn and icons bigger (fixes #64)
    :wq

--- app/src/main/java/org/ole/planet/takeout/LoginActivity.java
+++ app/src/main/java/org/ole/planet/takeout/LoginActivity.java
@@ -40,8 +40,7 @@ public class LoginActivity extends SyncActivity {
     private EditText inputName, inputPassword;
     private TextInputLayout inputLayoutName, inputLayoutPassword;
     private Button btnSignIn;
-    private ImageButton imgBtnSetting;
-    private TextView syncOption;
+    private ImageButton imgBtnSetting, syncOption;
     Context context;
     private View positiveAction;
     boolean connectionResult;
