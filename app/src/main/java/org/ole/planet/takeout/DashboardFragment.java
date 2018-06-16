233b7c904a7e287b967f7930db815f6f7559270d
Ak <akshayadagale075@gmail.com>
2018-06-16 18:16:27 -0500

    Update DashboardFragment.java

--- app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
+++ app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
@@ -14,51 +14,6 @@ import android.widget.ImageButton;
  */
 public class DashboardFragment extends Fragment {
     private ImageButton myLibraryImage;
-
-    public DashboardFragment() {
-        //init dashboard
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
-        declareElements(view);
-
-        myLibraryImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e("DF: ", "Clicked myLibrary");
-                Intent intent = new Intent(getActivity() , PDFReaderActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        return view;
-    }
-
-    private void declareElements(View view) {
-        // Imagebuttons
-        myLibraryImage = (ImageButton) view.findViewById(R.id.myLibrary);
-    }
-}
-package org.ole.planet.takeout;
-
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-/**
- * A placeholder fragment containing a simple view.
- */
-public class DashboardFragment extends Fragment {
-    private ImageButton myLibraryImage;
     private ImageButton myCoursesImage;
     private ImageButton myMeetUpsImage;
     private ImageButton myTeamsImage;
