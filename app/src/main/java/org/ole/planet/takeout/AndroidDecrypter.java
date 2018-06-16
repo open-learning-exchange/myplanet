4aee52399f659241018eb16f25e671378af42f66
Yibo Xu <7483137+xyb994@users.noreply.github.com>
2018-06-16 00:33:21 -0400

    format code

--- app/src/main/java/org/ole/planet/takeout/AndroidDecrypter.java
+++ app/src/main/java/org/ole/planet/takeout/AndroidDecrypter.java
@@ -9,19 +9,19 @@ import de.rtner.security.auth.spi.PBKDF2Parameters;
 
 public class AndroidDecrypter {
 
-    Boolean AndroidDecrypter(String usr_ID,String usr_rawPswd, String db_PswdkeyValue,String db_Salt){
+    Boolean AndroidDecrypter(String usr_ID, String usr_rawPswd, String db_PswdkeyValue, String db_Salt) {
         try {
 
-                //SecureRandom.getInstance("HmacSHA1").nextBytes(salt);
-                PBKDF2Parameters p = new PBKDF2Parameters("HmacSHA1", "utf-8", db_Salt.getBytes(), 10);
-                ///byte[] dk = new PBKDF2Engine(p).deriveKey("password", 20);
-                byte[] dk = new PBKDF2Engine(p).deriveKey(usr_rawPswd, 20);
-                System.out.println( usr_ID +" Value "+BinTools.bin2hex(dk).toLowerCase());
-                if(db_PswdkeyValue.equals(BinTools.bin2hex(dk).toLowerCase())){
-                    return true;
-                }else{
-                    return false;
-                }
+            //SecureRandom.getInstance("HmacSHA1").nextBytes(salt);
+            PBKDF2Parameters p = new PBKDF2Parameters("HmacSHA1", "utf-8", db_Salt.getBytes(), 10);
+            ///byte[] dk = new PBKDF2Engine(p).deriveKey("password", 20);
+            byte[] dk = new PBKDF2Engine(p).deriveKey(usr_rawPswd, 20);
+            System.out.println(usr_ID + " Value " + BinTools.bin2hex(dk).toLowerCase());
+            if (db_PswdkeyValue.equals(BinTools.bin2hex(dk).toLowerCase())) {
+                return true;
+            } else {
+                return false;
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
