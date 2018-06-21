bba3e2a3a68d86dcbca9fd15e4cc78f55000809a
anuya <aakarnik8@gmail.com>
2018-06-21 12:59:08 -0400

    added header xml

--- app/src/main/java/org/ole/planet/takeout/Dashboard.java
+++ app/src/main/java/org/ole/planet/takeout/Dashboard.java
@@ -74,7 +74,7 @@ public class Dashboard extends AppCompatActivity {
         return new AccountHeaderBuilder()
                 .withActivity(Dashboard.this)
                 .withTextColor(getResources().getColor(R.color.bg_white))
-                .withHeaderBackground(R.drawable.header)
+                .withHeaderBackground(R.drawable.header_image)
                 .withHeaderBackgroundScaleType(ImageView.ScaleType.FIT_XY)
                 .build();
 
