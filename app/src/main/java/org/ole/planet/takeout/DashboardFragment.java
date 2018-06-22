d842d27363696e5d4434f6c2200cce81489590fa
unknown <akshayadagale075@gmail.com>
2018-06-22 13:19:44 -0400

    made all the imagebutton clickable (Fixes #88)

--- app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
+++ app/src/main/java/org/ole/planet/takeout/DashboardFragment.java
@@ -13,7 +13,14 @@ import android.widget.ImageButton;
  * A placeholder fragment containing a simple view.
  */
 public class DashboardFragment extends Fragment {
+
+    //ImageButtons
     private ImageButton myLibraryImage;
+    private ImageButton myCourseImage;
+    private ImageButton myMeetUpsImage;
+    private ImageButton myTeamsImage;
+
+    //TextViews
 
     public DashboardFragment() {
         //init dashboard
@@ -35,11 +42,43 @@ public class DashboardFragment extends Fragment {
             }
         });
 
+        myCourseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("DF: ", "Clicked myLibrary");
+                Intent intent = new Intent(getActivity() , PDFReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myMeetUpsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("DF: ", "Clicked myLibrary");
+                Intent intent = new Intent(getActivity() , PDFReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myTeamsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("DF: ", "Clicked myLibrary");
+                Intent intent = new Intent(getActivity() , PDFReaderActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
     private void declareElements(View view) {
+
         // Imagebuttons
-        myLibraryImage = (ImageButton) view.findViewById(R.id.myLibrary);
+        myLibraryImage = (ImageButton) view.findViewById(R.id.myLibraryImageButton);
+        myCourseImage = (ImageButton) view.findViewById(R.id.myCoursesImageButton);
+        myMeetUpsImage = (ImageButton) view.findViewById(R.id.myMeetUpsImageButton);
+        myTeamsImage = (ImageButton) view.findViewById(R.id.myTeamsImageButton);
+
+        //TextViews
     }
 }
