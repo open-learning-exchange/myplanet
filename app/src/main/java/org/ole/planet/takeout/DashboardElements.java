86505431ee62486047c5840eaf7143424c56d545
anuya <aakarnik8@gmail.com>
2018-07-13 14:28:06 -0400

    changes in code

--- app/src/main/java/org/ole/planet/takeout/DashboardElements.java
+++ app/src/main/java/org/ole/planet/takeout/DashboardElements.java
@@ -17,7 +17,6 @@ import com.afollestad.materialdialogs.MaterialDialog;
 public class DashboardElements extends AppCompatActivity {
 
     private EditText feedbackText;
-    private RadioGroup choice1, choice2;
 
     /**
      * Disables the submit button until the feedback form is complete
@@ -26,24 +25,19 @@ public class DashboardElements extends AppCompatActivity {
         final View submitButton = dialog.getActionButton(DialogAction.POSITIVE);
         submitButton.setEnabled(false);
         feedbackText = dialog.getCustomView().findViewById(R.id.user_feedback);
-        choice1 = dialog.getCustomView().findViewById(R.id.choice1);
-        choice2 = dialog.getCustomView().findViewById(R.id.choice2);
         feedbackText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().length() != 0) submitButton.setEnabled(true);
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.toString().length() != 0) {
-                    if (choice1.getCheckedRadioButtonId() != -1 && choice2.getCheckedRadioButtonId() != -1) {
-                        submitButton.setEnabled(true);
-                    }
-                }
-
+                if (s.toString().length() == 0) submitButton.setEnabled(false);
             }
         });
     }
