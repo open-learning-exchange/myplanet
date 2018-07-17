ad54ca2d2b690a9e32e8a5773a0f11ec2f25da49
Roshan Rijal <rijalroshan94@gmail.com>
2018-07-17 06:15:50 +0545

    Change resource offline status after the file is downloaded (#121)
    * show progress bar while downloading..
    
    * code climate issue fixed
    
    * fixed issue #118
    
    * code climate issue fix
    
    * changed resource offline status after download is complete
    
    * clean up and added stop download option
    
    * code climate issue fixed

--- app/src/main/java/org/ole/planet/takeout/Data/Download.java
+++ app/src/main/java/org/ole/planet/takeout/Data/Download.java
@@ -3,15 +3,26 @@ package org.ole.planet.takeout.Data;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Download  implements Parcelable{
+public class Download implements Parcelable {
 
-    public Download(){
+    public Download() {
 
     }
 
+    private String fileName;
     private int progress;
     private int currentFileSize;
     private int totalFileSize;
+    private boolean completeAll;
+    private boolean failed;
+
+    public boolean isCompleteAll() {
+        return completeAll;
+    }
+
+    public void setCompleteAll(boolean completeAll) {
+        this.completeAll = completeAll;
+    }
 
     public int getProgress() {
         return progress;
@@ -37,24 +48,47 @@ public class Download  implements Parcelable{
         this.totalFileSize = totalFileSize;
     }
 
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
     @Override
     public int describeContents() {
         return 0;
     }
 
+    public boolean isFailed() {
+        return failed;
+    }
+
+    public void setFailed(boolean failed) {
+        this.failed = failed;
+    }
+
     @Override
     public void writeToParcel(Parcel dest, int flags) {
 
+        dest.writeString(fileName);
         dest.writeInt(progress);
         dest.writeInt(currentFileSize);
         dest.writeInt(totalFileSize);
+        dest.writeByte((byte) (completeAll ? 1 : 0));
+        dest.writeByte((byte) (failed ? 1 : 0));
     }
 
-    private Download(Parcel in) {
 
+    private Download(Parcel in) {
+        fileName = in.readString();
         progress = in.readInt();
         currentFileSize = in.readInt();
         totalFileSize = in.readInt();
+        completeAll = in.readByte() != 0;
+        failed = in.readByte() != 0;
     }
 
     public static final Parcelable.Creator<Download> CREATOR = new Parcelable.Creator<Download>() {
--- app/src/main/java/org/ole/planet/takeout/utilities/Utilities.java
+++ app/src/main/java/org/ole/planet/takeout/utilities/Utilities.java
@@ -14,7 +14,9 @@ import org.ole.planet.takeout.R;
 import org.ole.planet.takeout.datamanager.MyDownloadService;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class Utilities {
@@ -23,7 +25,7 @@ public class Utilities {
         Log.d("OLE ", "log: " + message);
     }
 
-    private static String getFileNameFromUrl(String url) {
+    public static String getFileNameFromUrl(String url) {
         try {
             return url.substring(url.lastIndexOf("/") + 1);
         } catch (Exception e) {
@@ -38,6 +40,13 @@ public class Utilities {
                 settings.getInt("url_Port", 0)
                 + "/resources/" + library.getResourceId() + "/" + library.getResourceLocalAddress();
 
+    }
+
+    public static String currentDate() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy");
+        String datetime = dateformat.format(c.getTime());
+        return datetime;
     }
 
     public static void openDownloadService(Context context, ArrayList urls) {
