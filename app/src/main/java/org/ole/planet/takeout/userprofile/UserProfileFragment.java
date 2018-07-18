23c366c4845c3d5478e7244c9dfbcbd5dff32232
Roshan Rijal <rijalroshan94@gmail.com>
2018-07-18 07:39:00 +0545

    added user profile fragment

---
+++ app/src/main/java/org/ole/planet/takeout/userprofile/UserProfileFragment.java
@@ -0,0 +1,26 @@
+package org.ole.planet.takeout.userprofile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ole.planet.takeaway.R;
+import org.ole.planet.takeout.R;
+
+public class UserProfileFragment extends Fragment {
+
+
+    public UserProfileFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+
+}
