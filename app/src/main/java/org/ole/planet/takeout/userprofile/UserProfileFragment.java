b31f961dd428d8d9874633d542e81848504076a6
Roshan Rijal <rijalroshan94@gmail.com>
2018-07-19 22:13:29 +0545

    replace layout with recycler view

--- app/src/main/java/org/ole/planet/takeout/userprofile/UserProfileFragment.java
+++ app/src/main/java/org/ole/planet/takeout/userprofile/UserProfileFragment.java
@@ -3,17 +3,28 @@ package org.ole.planet.takeout.userprofile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.ole.planet.takeout.Data.realm_UserModel;
 import org.ole.planet.takeout.R;
 import org.ole.planet.takeout.datamanager.RealmService;
 import org.ole.planet.takeout.utilities.Utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 import io.realm.Realm;
 
@@ -48,11 +59,57 @@ public class UserProfileFragment extends Fragment {
         ((TextView) v.findViewById(R.id.txt_name)).setText(String.format("%s %s %s", model.getFirstName(), model.getMiddleName(), model.getLastName()));
         ((TextView) v.findViewById(R.id.txt_email)).setText(Utilities.checkNA(model.getEmail()));
         ((TextView) v.findViewById(R.id.txt_dob)).setText(Utilities.checkNA(model.getDob()));
-        ((TextView) v.findViewById(R.id.txt_community_name)).setText(Utilities.checkNA(model.getCommunityName()));
-        ((TextView) v.findViewById(R.id.txt_last_visit)).setText(String.format("Last Login : %s", Utilities.getRelativeTime(handler.getLastVisit())));
-        ((TextView) v.findViewById(R.id.txt_visit_count)).setText(String.format("Total Visits : %d", handler.getOfflineVisits()));
-        ((TextView) v.findViewById(R.id.txt_max_opened_resource)).setText(Utilities.checkNA(handler.getMaxOpenedResource()));
-        ((TextView) v.findViewById(R.id.txt_number_resource_open)).setText(Utilities.checkNA(handler.getNumberOfResourceOpen()));
+
+        final LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        map.put("Community Name", Utilities.checkNA(model.getCommunityName()));
+        map.put("Last Login : ", Utilities.getRelativeTime(handler.getLastVisit()));
+        map.put("Total Visits : ", handler.getOfflineVisits() + "");
+        map.put("Maximum opened Resource : ", Utilities.checkNA(handler.getMaxOpenedResource()));
+        map.put("Number of Resource open : ", Utilities.checkNA(handler.getNumberOfResourceOpen()));
+
+        setUpRecyclerView(map, v);
     }
 
+    public void setUpRecyclerView(final HashMap<String, String> map, View v) {
+        RecyclerView rvStat = v.findViewById(R.id.rv_stat);
+        final LinkedList<String> keys = new LinkedList<>(map.keySet());
+        rvStat.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvStat.setNestedScrollingEnabled(false);
+        rvStat.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View v = getLayoutInflater().inflate(R.layout.row_stat, parent, false);
+                return new ViewHolderStat(v);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                if (holder instanceof ViewHolderStat) {
+                    ((ViewHolderStat) holder).key.setText(keys.get(position));
+                    ((ViewHolderStat) holder).value.setText(map.get(keys.get(position)));
+                    if (position %2 ==0){
+                        holder.itemView.setBackgroundColor(getResources().getColor(R.color.bg_white));
+                        holder.itemView.setBackgroundColor(getResources().getColor(R.color.md_grey_300));
+                    }
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return keys.size();
+            }
+        });
+    }
+
+
+    class ViewHolderStat extends RecyclerView.ViewHolder {
+        TextView key, value;
+
+        public ViewHolderStat(View itemView) {
+            super(itemView);
+            key = itemView.findViewById(R.id.key);
+            value = itemView.findViewById(R.id.value);
+        }
+    }
 }
