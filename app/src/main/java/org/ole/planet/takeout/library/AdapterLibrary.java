adb3190dc91304e305b0a5b2074145c9c620e3b9
Leonard <leomaxi@outlook.com>
2018-08-06 10:21:36 -0400

    library

--- app/src/main/java/org/ole/planet/takeout/library/AdapterLibrary.java
+++ app/src/main/java/org/ole/planet/takeout/library/AdapterLibrary.java
@@ -11,7 +11,6 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
-import org.ole.planet.takeout.Data.realm_myLibrary;
 import org.ole.planet.takeout.Data.realm_resources;
 import org.ole.planet.takeout.R;
 import org.ole.planet.takeout.callback.OnLibraryItemSelected;
@@ -85,7 +84,7 @@ public class AdapterLibrary extends RecyclerView.Adapter<RecyclerView.ViewHolder
             checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    if (listener != null){
+                    if (listener != null) {
                         Utilities.handleCheck(b, getAdapterPosition(), (ArrayList) selectedItems, libraryList);
                         listener.onSelectedListChange(selectedItems);
                     }
