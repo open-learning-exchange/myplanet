c3ecae06cf8ce9e5601324a2b8a853769b36bfbe
Roshan Rijal <rijalroshan94@gmail.com>
2018-12-14 19:02:43 +0545

    Open resource on dashboard item clicked and more button to open detail (Fixes #411) (#424)

--- app/src/main/java/org/ole/planet/myplanet/utilities/FileUtils.java
+++ app/src/main/java/org/ole/planet/myplanet/utilities/FileUtils.java
@@ -25,10 +25,10 @@ public class FileUtils {
     }
 
     public static String getFileExtension(String address) {
-
+        if (TextUtils.isEmpty(address))
+            return "";
         String filenameArray[] = address.split("\\.");
-        String extension = filenameArray[filenameArray.length - 1];
-        return extension;
+        return filenameArray[filenameArray.length - 1];
     }
 
 }
