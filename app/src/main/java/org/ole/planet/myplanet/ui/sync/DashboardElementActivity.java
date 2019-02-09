9f0e13e2dd2cd4fa40d95fa76e85ab7d97d87771
Leonard <leomaxi@outlook.com>
2019-02-09 12:42:13 -0500

    Dashboard look

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/DashboardElementActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/DashboardElementActivity.java
@@ -1,21 +1,30 @@
 package org.ole.planet.myplanet.ui.sync;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.ole.planet.myplanet.R;
 import org.ole.planet.myplanet.callback.OnRatingChangeListener;
 import org.ole.planet.myplanet.service.UserProfileDbHandler;
 import org.ole.planet.myplanet.ui.SettingActivity;
 import org.ole.planet.myplanet.ui.rating.RatingFragment;
-import org.ole.planet.myplanet.utilities.Utilities;
 
-import java.io.File;
+import static org.ole.planet.myplanet.ui.dashboard.DashboardFragment.PREFS_NAME;
 
 /**
  * Extra class for excess methods in DashboardActivity activities
@@ -24,14 +33,15 @@ import java.io.File;
 public abstract class DashboardElementActivity extends AppCompatActivity {
 
     public UserProfileDbHandler profileDbHandler;
+    private SharedPreferences settings;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         profileDbHandler = new UserProfileDbHandler(this);
+        settings = getApplicationContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 
     }
-
 
 
     @Override
@@ -45,16 +55,48 @@ public abstract class DashboardElementActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.menu_profile) {
             return true;
+        }
+        if (id == R.id.menu_goOnline) {
+            wifiStatusSwitch();
+            return true;
         } else if (id == R.id.menu_logout) {
             logout();
         } else if (id == R.id.action_setting) {
             startActivity(new Intent(this, SettingActivity.class));
-        }else if(id == R.id.action_sync){
-            startActivity(new Intent(this, LoginActivity.class).putExtra("forceSync", true).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        } else if (id == R.id.action_sync) {
+            startActivity(new Intent(this, LoginActivity.class).putExtra("forceSync", true).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
             finish();
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void wifiStatusSwitch() {
+        ActionMenuItemView goOnline = findViewById(R.id.menu_goOnline);
+        Drawable resIcon = getResources().getDrawable(R.drawable.goonline);
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWifi.isConnected()) {
+            wifi.setWifiEnabled(false);
+            resIcon.mutate().setColorFilter(getApplicationContext().getResources().getColor(R.color.green), PorterDuff.Mode.SRC_ATOP);
+            goOnline.setIcon(resIcon);
+            Toast.makeText(this, "Wifi is turned Off. Saving battery power", Toast.LENGTH_LONG).show();
+
+        } else {
+            wifi.setWifiEnabled(true);
+            resIcon.mutate().setColorFilter(getApplicationContext().getResources().getColor(R.color.accent), PorterDuff.Mode.SRC_ATOP);
+            goOnline.setIcon(resIcon);
+            Toast.makeText(this, "Wifi is turned On. Turn off later to save battery power", Toast.LENGTH_LONG).show();
+            connectToWifi();
+        }
+    }
+
+    private void connectToWifi() {
+
+        //Todo Connnect to Wifi with the SSID
+        Log.e("Wifi", "First Sync Wifi name is " + settings.getString("LastWifiSSID", ""));
+
     }
 
 
