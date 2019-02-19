ec3eed65727d692e3ff8bfbaba151ff39daa7d3f
Roshan Rijal <rijalroshan94@gmail.com>
2019-02-19 09:40:02 +0545

    added edit achievement fragment

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/userprofile/EditAchievementFragment.java
@@ -0,0 +1,30 @@
+package org.ole.planet.myplanet.ui.userprofile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ole.planet.myplanet.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditAchievementFragment extends Fragment {
+
+
+    public EditAchievementFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_achievement, container, false);
+    }
+
+}
