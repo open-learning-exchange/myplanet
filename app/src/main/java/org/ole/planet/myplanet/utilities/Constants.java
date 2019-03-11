216fbce375ebd0cf39a8a2598eb0c84d3792857d
Leonard Maximus Irenous Mensah <leomaxi@outlook.com>
2019-03-11 07:19:33 +0000

    Beta function switches updated (#635)

--- app/src/main/java/org/ole/planet/myplanet/utilities/Constants.java
+++ app/src/main/java/org/ole/planet/myplanet/utilities/Constants.java
@@ -15,14 +15,14 @@ import java.util.List;
 public class Constants {
     public static List<ShelfData> shelfDataList;
     public static List<String> betaList;
-    public static final String KEY_RATING = "rating";
-    public static final String KEY_EXAM = "exam";
-    public static final String KEY_SYNC = "sync";
-    public static final String KEY_SURVEY = "survey";
-    public static final String KEY_MEETUPS = "meetup";
-    public static final String KEY_TEAMS = "teams";
-    public static final String KEY_DELETE = "delete";
-    public static final String KEY_ACHIEVEMENT = "achievement";
+    public static final String KEY_RATING = "beta_rating";
+    public static final String KEY_EXAM = "beta_course";
+    public static final String KEY_SYNC = "beta_wifi_switch";
+    public static final String KEY_SURVEY = "beta_survey";
+    public static final String KEY_MEETUPS = "key_meetup";
+    public static final String KEY_TEAMS = "key_teams";
+    public static final String KEY_DELETE = "key_delete";
+    public static final String KEY_ACHIEVEMENT = "beta_achievement";
 
     static {
         shelfDataList = new ArrayList<>();
@@ -30,16 +30,16 @@ public class Constants {
         shelfDataList.add(new ShelfData("meetupIds", "meetups", "meetupId", RealmMeetup.class));
         shelfDataList.add(new ShelfData("courseIds", "courses", "courseId", RealmMyCourse.class));
         shelfDataList.add(new ShelfData("myTeamIds", "teams", "teamId", RealmMyTeam.class));
-
-        betaList = new ArrayList<>();
-        betaList.add(KEY_RATING);
-        betaList.add(KEY_EXAM);
-        betaList.add(KEY_TEAMS);
-        betaList.add(KEY_MEETUPS);
-        betaList.add(KEY_SURVEY);
-        betaList.add(KEY_DELETE);
-        betaList.add(KEY_SYNC);
-        betaList.add(KEY_ACHIEVEMENT);
+//
+//        betaList = new ArrayList<>();
+//        betaList.add(KEY_RATING);
+//        betaList.add(KEY_EXAM);
+//        betaList.add(KEY_TEAMS);
+//        betaList.add(KEY_MEETUPS);
+//        betaList.add(KEY_SURVEY);
+//        betaList.add(KEY_DELETE);
+//        betaList.add(KEY_SYNC);
+//        betaList.add(KEY_ACHIEVEMENT);
     }
 
     public static class ShelfData {
@@ -59,11 +59,14 @@ public class Constants {
 
     public static boolean showBetaFeature(String s, Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        if (betaList.contains(s)) {
-            Utilities.log("return " + preferences.getBoolean("beta_function", false));
-            return preferences.getBoolean("beta_function", false);
-        }
-        return true;
+        //if (betaList.contains(s)) {
+        Utilities.log(s + " beta");
+        Utilities.log(preferences.getBoolean("beta_function", false) + " beta");
+        Utilities.log(preferences.getBoolean(s,false) + " beta");
+        Utilities.log((preferences.getBoolean("beta_function", false) &&  preferences.getBoolean(s,false)) + "");
+            return preferences.getBoolean("beta_function", false) &&  preferences.getBoolean(s,false);
+      //  }
+//        return true;
     }
 
 
