b50573a2bab14d5d4d4a1aba53f8a2d06b6973c1
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-03-13 19:58:49 +0545

    Password field visibility icon toggle(connects #536) (#648)

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
