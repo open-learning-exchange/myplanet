b733f16a95bb10fe2b8e1843be111bdd611015d3
Roshan Rijal <rijalroshan94@gmail.com>
2019-05-18 22:09:22 +0545

    search feature in user list

--- app/src/main/java/org/ole/planet/myplanet/ui/team/AdapterTeam.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/AdapterTeam.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,14 +62,33 @@ public class AdapterTeam extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             });
         }
     }
-
+    List<RealmUserModel> users;
     private void showUserList(RealmMyTeam realmMyTeam) {
-        View view = LayoutInflater.from(context).inflate(R.layout.fragment_user_list,null);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_user_list,null);
         EditText etSearch = view.findViewById(R.id.et_search);
         ListView lv = view.findViewById(R.id.list_user);
-        List<RealmUserModel> users = mRealm.where(RealmUserModel.class).in("id", realmMyTeam.getUserId().toArray(new String[0])).findAll();
+        users = mRealm.where(RealmUserModel.class).in("id", realmMyTeam.getUserId().toArray(new String[0])).findAll();
         ArrayAdapter<RealmUserModel> adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, users);
         lv.setAdapter(adapter);
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                users = mRealm.where(RealmUserModel.class).in("id", realmMyTeam.getUserId().toArray(new String[0])).contains("name", charSequence.toString()).findAll();
+                ArrayAdapter<RealmUserModel> adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, users);
+                lv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         lv.setOnItemClickListener((adapterView, view1, i, l) -> {
             if (listener != null)
                 listener.onSelectedUser(users.get(i));
