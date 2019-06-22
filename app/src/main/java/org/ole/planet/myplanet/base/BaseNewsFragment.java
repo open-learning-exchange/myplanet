5a7f40c38883e331c8da2e39f5661302854ef289
Roshan Rijal <rijalroshan94@gmail.com>
2019-06-22 22:22:55 +0545

    reply issues fix

--- app/src/main/java/org/ole/planet/myplanet/base/BaseNewsFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseNewsFragment.java
@@ -1,6 +1,7 @@
 package org.ole.planet.myplanet.base;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import org.ole.planet.myplanet.callback.OnHomeItemClickListener;
 import org.ole.planet.myplanet.model.RealmNews;
 import org.ole.planet.myplanet.ui.news.AdapterNews;
+import org.ole.planet.myplanet.ui.news.ReplyActivity;
 
 import java.util.List;
 
@@ -18,7 +20,7 @@ import io.realm.Sort;
 public abstract class BaseNewsFragment extends Fragment implements AdapterNews.OnNewsItemClickListener {
 
     public Realm mRealm;
-    public Button btnShowMain;
+//    public Button btnShowMain;
     public OnHomeItemClickListener homeItemClickListener;
 
     @Override
@@ -31,11 +33,12 @@ public abstract class BaseNewsFragment extends Fragment implements AdapterNews.O
 
     @Override
     public void showReply(RealmNews news) {
-        List<RealmNews> list = mRealm.where(RealmNews.class).sort("time", Sort.DESCENDING)
-                .equalTo("replyTo", news.getId(), Case.INSENSITIVE)
-                .findAll();
-        setData(list);
-        btnShowMain.setVisibility(View.VISIBLE);
+//        List<RealmNews> list = mRealm.where(RealmNews.class).sort("time", Sort.DESCENDING)
+//                .equalTo("replyTo", news.getId(), Case.INSENSITIVE)
+//                .findAll();
+//        setData(list);
+//        btnShowMain.setVisibility(View.VISIBLE);
+        startActivity(new Intent(getActivity(), ReplyActivity.class).putExtra("id", news.getId()));
     }
 
     public abstract void setData(List<RealmNews> list);
---
+++ app/src/main/java/org/ole/planet/myplanet/ui/news/ReplyActivity.java
@@ -0,0 +1,71 @@
+package org.ole.planet.myplanet.ui.news;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import org.ole.planet.myplanet.R;
+import org.ole.planet.myplanet.datamanager.DatabaseService;
+import org.ole.planet.myplanet.model.RealmNews;
+import org.ole.planet.myplanet.model.RealmUserModel;
+import org.ole.planet.myplanet.service.UserProfileDbHandler;
+import org.ole.planet.myplanet.utilities.Utilities;
+
+import java.util.List;
+
+import io.realm.Case;
+import io.realm.Realm;
+import io.realm.Sort;
+
+public class ReplyActivity extends AppCompatActivity implements AdapterNews.OnNewsItemClickListener {
+
+    Realm mRealm;
+    String id;
+    AdapterNews newsAdapter;
+    RealmUserModel user;
+    RecyclerView rvReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mRealm = new DatabaseService(this).getRealmInstance();
+        setTitle("Reply");
+        id = getIntent().getStringExtra("id");
+        user = new UserProfileDbHandler(this).getUserModel();
+        rvReply = findViewById(R.id.rv_reply);
+        rvReply.setLayoutManager(new LinearLayoutManager(this));
+        rvReply.setNestedScrollingEnabled(false);
+        showData(id);
+    }
+
+    private void showData(String id) {
+        RealmNews news = mRealm.where(RealmNews.class).equalTo("id", id).findFirst();
+
+        List<RealmNews> list = mRealm.where(RealmNews.class).sort("time", Sort.DESCENDING)
+                .equalTo("replyTo", id, Case.INSENSITIVE)
+                .findAll();
+        Utilities.log("List "  + list.size());
+        newsAdapter = new AdapterNews(this, list, user, news);
+        newsAdapter.setListener(this);
+        newsAdapter.setmRealm(mRealm);
+        rvReply.setAdapter(newsAdapter);
+    }
+
+    @Override
+    public void showReply(RealmNews news) {
+        startActivity(new Intent(this, ReplyActivity.class).putExtra("id", news.getId()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
