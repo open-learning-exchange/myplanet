bf7ab28bcef9e21cf7f862a27d59e8285f0bdb35
jeevast <jeevast58@gmail.com>
2019-07-15 15:11:57 -0400

    deletion-factory-method

--- app/src/main/java/org/ole/planet/myplanet/ui/MyLife/LifeFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/MyLife/LifeFragment.java
@@ -16,18 +16,14 @@ import org.ole.planet.myplanet.base.BaseRecyclerFragment;
  * Activities that contain this fragment must implement the
  * {@link LifeFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LifeFragment#newInstance} factory method to
- * create an instance of this fragment.
+
  */
 public class LifeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,31 +31,11 @@ public class LifeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LifeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LifeFragment newInstance(String param1, String param2) {
-        LifeFragment fragment = new LifeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -69,12 +45,7 @@ public class LifeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_life, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
 
     @Override
     public void onAttach(Context context) {
