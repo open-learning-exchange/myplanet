59ee99c833e96f247a6ff75669042c5537cc053b
Roshan Rijal <rijalroshan94@gmail.com>
2019-07-19 22:31:49 +0545

    clean up

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
@@ -313,26 +313,14 @@ public class LoginActivity extends SyncActivity implements Service.CheckVersionC
     }
 
     private void protocol_semantics() {
-//        Uri uri = Uri.parse(serverUrl.getText().toString());
-//        String protocol = serverUrlProtocol.getText().toString();
-//        if (TextUtils.equals("https://", protocol)) {
-//            protocol_checkin.check(R.id.radio_https);
-//        } else if (TextUtils.equals("http://", protocol)) {
-//            protocol_checkin.check(R.id.radio_http);
-//        } else {
-//            protocol_checkin.check(R.id.radio_http);
-//            serverUrlProtocol.setText(getString(R.string.http_protocol));
-//        }
         settings.edit().putString("serverProtocol", serverUrlProtocol.getText().toString()).commit();
         protocol_checkin.setOnCheckedChangeListener((radioGroup, i) -> {
             switch (i) {
                 case R.id.radio_http:
-//                  serverUrl.setText((!serverUrl.getText().toString().equals("")) ? serverUrl.getText().toString().replaceFirst(getString(R.string.https_protocol), getString(R.string.http_protocol)) : getString(R.string.http_protocol));
                     serverUrlProtocol.setText(getString(R.string.http_protocol));
                     break;
 
                 case R.id.radio_https:
-//                  serverUrl.setText((!serverUrl.getText().toString().equals("")) ? serverUrl.getText().toString().replaceFirst(getString(R.string.http_protocol), getString(R.string.https_protocol)) : getString(R.string.https_protocol));
                     serverUrlProtocol.setText(getString(R.string.https_protocol));
                     break;
             }
