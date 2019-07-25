5e0d60f4abe8eb397c87cb0a1b5ee47e21cc1430
Roshan Rijal <rijalroshan94@gmail.com>
2019-07-25 22:12:40 +0545

    added my personal

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/myPersonals/AdapterMyPersonal.java
@@ -0,0 +1,59 @@
+package org.ole.planet.myplanet.ui.myPersonals;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.ole.planet.myplanet.R;
+import org.ole.planet.myplanet.model.RealmMyPersonal;
+import org.ole.planet.myplanet.ui.userprofile.AdapterOtherInfo;
+import org.ole.planet.myplanet.utilities.TimeUtils;
+import org.ole.planet.myplanet.utilities.Utilities;
+
+import java.util.List;
+
+public class AdapterMyPersonal extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private List<RealmMyPersonal> list ;
+
+    public AdapterMyPersonal(Context context, List<RealmMyPersonal> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.row_my_personal, parent, false);
+        return new ViewHolderMyPersonal(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolderMyPersonal){
+            ((ViewHolderMyPersonal) holder).title.setText(list.get(position).getTitle());
+            ((ViewHolderMyPersonal) holder).description.setText(list.get(position).getDescription());
+            ((ViewHolderMyPersonal) holder).date.setText(TimeUtils.getFormatedDate(list.get(position).getDate()));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ViewHolderMyPersonal extends RecyclerView.ViewHolder{
+        TextView title, description, date;
+        public ViewHolderMyPersonal(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            date = itemView.findViewById(R.id.date);
+        }
+    }
+}
--- app/src/main/java/org/ole/planet/myplanet/ui/myPersonals/MyPersonalsFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/myPersonals/MyPersonalsFragment.java
@@ -2,18 +2,32 @@ package org.ole.planet.myplanet.ui.myPersonals;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.ole.planet.myplanet.R;
+import org.ole.planet.myplanet.datamanager.DatabaseService;
+import org.ole.planet.myplanet.model.RealmMyPersonal;
+import org.ole.planet.myplanet.model.RealmUserModel;
+import org.ole.planet.myplanet.service.UserProfileDbHandler;
+import org.ole.planet.myplanet.ui.library.AddResourceFragment;
+
+import java.util.List;
+
+import io.realm.Realm;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyPersonalsFragment extends Fragment {
 
+    RecyclerView rvMyPersonal;
+    Realm mRealm;
 
     public MyPersonalsFragment() {
         // Required empty public constructor
@@ -24,9 +38,33 @@ public class MyPersonalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_my_personals, container, false);
+        View v = inflater.inflate(R.layout.fragment_my_personals, container, false);
+        mRealm = new DatabaseService(getActivity()).getRealmInstance();
+        rvMyPersonal = v.findViewById(R.id.rv_mypersonal);
+        rvMyPersonal.setLayoutManager(new LinearLayoutManager(getActivity()));
+        v.findViewById(R.id.add_my_personal).setOnClickListener(vi->{
+            AddResourceFragment f = new AddResourceFragment();
+            Bundle b = new Bundle();
+            b.putInt("type", 1);
+            f.setArguments(b);
+            f.show(getChildFragmentManager(), "Add Resource");
 
+        });
         return v;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        RealmUserModel model = new UserProfileDbHandler(getActivity()).getUserModel();
+        List<RealmMyPersonal> realmMyPersonals = mRealm.where(RealmMyPersonal.class).equalTo("userId", model.getId()).findAll();
+        rvMyPersonal.setAdapter(new AdapterMyPersonal(getActivity(), realmMyPersonals));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mRealm != null && !mRealm.isClosed())
+            mRealm.close();
+    }
 }
