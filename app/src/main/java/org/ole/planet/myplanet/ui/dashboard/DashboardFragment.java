598b147d11be47e574d93d1cfb894de77d453328
Leonard <leomaxi@outlook.com>
2019-08-19 09:40:20 +0000

    Dashboard code clean

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.java
@@ -1,53 +1,26 @@
 package org.ole.planet.myplanet.ui.dashboard;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.flexbox.FlexDirection;
-import com.google.android.flexbox.FlexboxLayout;
-
 import org.ole.planet.myplanet.R;
-import org.ole.planet.myplanet.base.BaseContainerFragment;
 import org.ole.planet.myplanet.datamanager.DatabaseService;
-import org.ole.planet.myplanet.model.RealmMeetup;
-import org.ole.planet.myplanet.model.RealmMyCourse;
-import org.ole.planet.myplanet.model.RealmMyLibrary;
-import org.ole.planet.myplanet.model.RealmMyTeam;
 import org.ole.planet.myplanet.model.RealmSubmission;
-import org.ole.planet.myplanet.model.RealmUserModel;
 import org.ole.planet.myplanet.service.UserProfileDbHandler;
-import org.ole.planet.myplanet.ui.course.TakeCourseFragment;
-import org.ole.planet.myplanet.ui.library.AddResourceActivity;
 import org.ole.planet.myplanet.ui.library.AddResourceFragment;
-import org.ole.planet.myplanet.ui.mymeetup.MyMeetupDetailFragment;
 import org.ole.planet.myplanet.ui.news.NewsFragment;
 import org.ole.planet.myplanet.ui.submission.MySubmissionFragment;
-import org.ole.planet.myplanet.ui.team.MyTeamsDetailFragment;
 import org.ole.planet.myplanet.ui.userprofile.AchievementFragment;
-import org.ole.planet.myplanet.ui.userprofile.UserProfileFragment;
 import org.ole.planet.myplanet.utilities.Constants;
 import org.ole.planet.myplanet.utilities.TimeUtils;
-import org.ole.planet.myplanet.utilities.Utilities;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.realm.Case;
 import io.realm.Realm;
-import io.realm.RealmObject;
-import io.realm.RealmResults;
 
 /**
  * A placeholder fragment containing a simple view.
