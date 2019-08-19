0ae827f20bdb213287b9832ff05d54a2c69c516b
Leonard <leomaxi@outlook.com>
2019-08-19 09:42:32 +0000

    code cleanup for  InactiveDashboard

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/InactiveDashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/InactiveDashboardFragment.java
@@ -25,9 +25,9 @@ public class InactiveDashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_in_active_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_in_active_dashboard, container, false);
 
-       v.findViewById(R.id.btn_feedback).setOnClickListener(vi -> {
+        v.findViewById(R.id.btn_feedback).setOnClickListener(vi -> {
             new FeedbackFragment().show(getChildFragmentManager(), "");
         });
         return v;
