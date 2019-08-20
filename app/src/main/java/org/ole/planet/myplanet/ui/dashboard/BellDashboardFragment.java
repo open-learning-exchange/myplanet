24c1f892899cec21fb555e2a9629152453854220
Leonard Maximus Irenous Mensah <leomaxi@outlook.com>
2019-08-20 09:06:17 +0000

    remove comments

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
@@ -50,28 +50,13 @@ public class BellDashboardFragment extends BaseDashboardFragment {
     private void declareElements(View view) {
         tvDate = view.findViewById(R.id.txt_date);
         tvCommunityName = view.findViewById(R.id.txt_community_name);
-//        view.findViewById(R.id.iv_setting).setOnClickListener(V->{
-//            startActivity(new Intent(getActivity(), SettingActivity.class));
-//        });
         initView(view);
-        // view.findViewById(R.id.ll_achievement).setOnClickListener(v -> homeItemClickListener.openCallFragment(new AchievementFragment()));
-        // view.findViewById(R.id.ll_news).setOnClickListener(v -> homeItemClickListener.openCallFragment(new NewsFragment()));
         View.OnClickListener showToast = view1 -> Toast.makeText(getContext(), "Feature Not Available", Toast.LENGTH_LONG).show();
         view.findViewById(R.id.ll_home_team).setOnClickListener(v -> homeItemClickListener.openCallFragment(new TeamFragment()));
-//        view.findViewById(R.id.ll_messages).setOnClickListener(showToast);
-        // view.findViewById(R.id.ll_calendar).setOnClickListener(v -> homeItemClickListener.openCallFragment(new CalendarFragment()));
-        // view.findViewById(R.id.ll_contacts).setOnClickListener(showToast);
-        //    view.findViewById(R.id.ll_references).setOnClickListener(view12 -> homeItemClickListener.openCallFragment(new ReferenceFragment()));
-        // view.findViewById(R.id.ll_help_wanted).setOnClickListener(showToast);
         view.findViewById(R.id.myLibraryImageButton).setOnClickListener(v -> openHelperFragment(new LibraryFragment()));
         view.findViewById(R.id.myCoursesImageButton).setOnClickListener(v -> openHelperFragment(new CourseFragment()));
-
-        // view.findViewById(R.id.ll_mySubmissions).setOnClickListener(v -> homeItemClickListener.openCallFragment(new MySubmissionFragment()));
-        // view.findViewById(R.id.ll_myHealth).setOnClickListener(showToast);
         view.findViewById(R.id.myLifeImageButton).setOnClickListener(v -> homeItemClickListener.openCallFragment(new LifeFragment()));
-
-        //   view.findViewById(R.id.ll_myPersonals).setOnClickListener(v->homeItemClickListener.openCallFragment(new MyPersonalsFragment()));
-    }
+     }
 
     private void openHelperFragment(Fragment f) {
         Fragment temp = f;
