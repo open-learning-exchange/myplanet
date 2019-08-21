03ebd0205cdd4affe5f0b76e37f6678a6e9e9d9c
chiragawale <chiragawale777@gmail.com>
2019-08-21 10:21:51 -0500

    clean up

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
@@ -8,26 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.ole.planet.myplanet.R;
-import org.ole.planet.myplanet.ui.calendar.CalendarFragment;
 import org.ole.planet.myplanet.ui.course.CourseFragment;
-import org.ole.planet.myplanet.ui.library.LibraryFragment;
 import org.ole.planet.myplanet.ui.library.AddResourceFragment;
+import org.ole.planet.myplanet.ui.library.LibraryFragment;
 import org.ole.planet.myplanet.ui.mylife.LifeFragment;
-import org.ole.planet.myplanet.ui.myPersonals.MyPersonalsFragment;
-import org.ole.planet.myplanet.ui.news.NewsFragment;
-import org.ole.planet.myplanet.ui.references.ReferenceFragment;
 import org.ole.planet.myplanet.ui.team.TeamFragment;
-import org.ole.planet.myplanet.ui.submission.MySubmissionFragment;
-import org.ole.planet.myplanet.ui.userprofile.AchievementFragment;
 import org.ole.planet.myplanet.utilities.TimeUtils;
-
 import java.util.Date;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 public class BellDashboardFragment extends BaseDashboardFragment {
 
     public static final String PREFS_NAME = "OLE_PLANET";
@@ -56,27 +46,12 @@ public class BellDashboardFragment extends BaseDashboardFragment {
     private void declareElements(View view) {
         tvDate = view.findViewById(R.id.txt_date);
         tvCommunityName = view.findViewById(R.id.txt_community_name);
-//        view.findViewById(R.id.iv_setting).setOnClickListener(V->{
-//            startActivity(new Intent(getActivity(), SettingActivity.class));
-//        });
         initView(view);
-       // view.findViewById(R.id.ll_achievement).setOnClickListener(v -> homeItemClickListener.openCallFragment(new AchievementFragment()));
-       // view.findViewById(R.id.ll_news).setOnClickListener(v -> homeItemClickListener.openCallFragment(new NewsFragment()));
         View.OnClickListener showToast = view1 -> Toast.makeText(getContext(), "Feature Not Available", Toast.LENGTH_LONG).show();
-        view.findViewById(R.id.ll_home_team).setOnClickListener(v->homeItemClickListener.openCallFragment(new TeamFragment()));
-//        view.findViewById(R.id.ll_messages).setOnClickListener(showToast);
-       // view.findViewById(R.id.ll_calendar).setOnClickListener(v -> homeItemClickListener.openCallFragment(new CalendarFragment()));
-       // view.findViewById(R.id.ll_contacts).setOnClickListener(showToast);
-    //    view.findViewById(R.id.ll_references).setOnClickListener(view12 -> homeItemClickListener.openCallFragment(new ReferenceFragment()));
-       // view.findViewById(R.id.ll_help_wanted).setOnClickListener(showToast);
+        view.findViewById(R.id.ll_home_team).setOnClickListener(v -> homeItemClickListener.openCallFragment(new TeamFragment()));
         view.findViewById(R.id.myLibraryImageButton).setOnClickListener(v -> openHelperFragment(new LibraryFragment()));
         view.findViewById(R.id.myCoursesImageButton).setOnClickListener(v -> openHelperFragment(new CourseFragment()));
-
-       // view.findViewById(R.id.ll_mySubmissions).setOnClickListener(v -> homeItemClickListener.openCallFragment(new MySubmissionFragment()));
-       // view.findViewById(R.id.ll_myHealth).setOnClickListener(showToast);
         view.findViewById(R.id.myLifeImageButton).setOnClickListener(v -> homeItemClickListener.openCallFragment(new LifeFragment()));
-
-     //   view.findViewById(R.id.ll_myPersonals).setOnClickListener(v->homeItemClickListener.openCallFragment(new MyPersonalsFragment()));
     }
 
     private void openHelperFragment(Fragment f) {
