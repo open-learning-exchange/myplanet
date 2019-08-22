19c7653b7ab04f7a8cf2e62e4a575f70fdb6b94e
Roshan Rijal <rijalroshan94@gmail.com>
2019-08-22 22:16:44 +0545

    code climate fix

--- app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/MembersFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/MembersFragment.java
@@ -1,23 +1,13 @@
 package org.ole.planet.myplanet.ui.team.teamMember;
 
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import org.ole.planet.myplanet.R;
-
-import android.support.annotation.Nullable;
 
 import org.ole.planet.myplanet.base.BaseMemberFragment;
 import org.ole.planet.myplanet.model.RealmMyTeam;
 import org.ole.planet.myplanet.model.RealmUserModel;
-import org.ole.planet.myplanet.ui.team.BaseTeamFragment;
 
 import java.util.List;
 
