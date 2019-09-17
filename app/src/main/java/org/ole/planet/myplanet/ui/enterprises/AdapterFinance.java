a2dc83d0a5ed5b92cfb260bb3362bad53eaff3c3
Leonard <leomaxi@outlook.com>
2019-09-17 10:34:57 +0000

    Alternate BG color

--- app/src/main/java/org/ole/planet/myplanet/ui/enterprises/AdapterFinance.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/enterprises/AdapterFinance.java
@@ -1,12 +1,15 @@
 package org.ole.planet.myplanet.ui.enterprises;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.ole.planet.myplanet.R;
@@ -31,6 +34,8 @@ public class AdapterFinance extends RecyclerView.Adapter<RecyclerView.ViewHolder
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.row_finance, parent, false);
+        //LinearLayout layout = v.findViewById(R.id.llayout);
+        //layout.setBackgroundColor(context.getResources().getColor((R.color.accent)));
         return new ViewHolderFinance(v);
     }
 
@@ -42,13 +47,15 @@ public class AdapterFinance extends RecyclerView.Adapter<RecyclerView.ViewHolder
             Utilities.log("Type " + list.get(position).getType());
             if (TextUtils.equals(list.get(position).getType().toLowerCase(), "debit")) {
                 ((ViewHolderFinance) holder).debit.setText(list.get(position).getAmount() + "");
-                ((ViewHolderFinance) holder).credit.setText("");
+                ((ViewHolderFinance) holder).credit.setText(" -");
+                ((ViewHolderFinance) holder).credit.setTextColor(Color.BLACK);;
             } else {
                 ((ViewHolderFinance) holder).credit.setText(list.get(position).getAmount() + "");
-                ((ViewHolderFinance) holder).debit.setText("");
+                ((ViewHolderFinance) holder).debit.setText(" -");
+                ((ViewHolderFinance) holder).debit.setTextColor(Color.BLACK);
             }
             ((ViewHolderFinance) holder).balance.setText(getBalance(position) + "");
-
+            updateBackgroundColor(((ViewHolderFinance) holder).row,position);
         }
     }
 
@@ -73,14 +80,20 @@ public class AdapterFinance extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     class ViewHolderFinance extends RecyclerView.ViewHolder {
         TextView date, note, credit, debit, balance;
-
+        LinearLayout row ;
         public ViewHolderFinance(View itemView) {
             super(itemView);
+            row= itemView.findViewById(R.id.llayout);
             date = itemView.findViewById(R.id.date);
             note = itemView.findViewById(R.id.note);
             credit = itemView.findViewById(R.id.credit);
             debit = itemView.findViewById(R.id.debit);
             balance = itemView.findViewById(R.id.balance);
+        }
+    }
+    public void updateBackgroundColor(LinearLayout layout,int position) {
+        if (position % 2 < 1) {
+            layout.setBackgroundColor(context.getResources().getColor((R.color.cardview_light_background)));
         }
     }
 }
