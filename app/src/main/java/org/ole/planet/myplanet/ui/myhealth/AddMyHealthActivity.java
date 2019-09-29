8f0ded3af416f85695bd7d524618daf4415453ef
Roshan Rijal <rijalroshan94@gmail.com>
2019-09-29 21:52:49 +0545

    add myhealth

--- app/src/main/java/org/ole/planet/myplanet/ui/myhealth/AddMyHealthActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/myhealth/AddMyHealthActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import org.ole.planet.myplanet.R;
+import org.ole.planet.myplanet.datamanager.DatabaseService;
+
+import io.realm.Realm;
 
 public class AddMyHealthActivity extends AppCompatActivity {
-
+    Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_my_health);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+        realm = new DatabaseService(this).getRealmInstance();
     }
+
+
 
 
     @Override
@@ -22,5 +28,12 @@ public class AddMyHealthActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home)
             finish();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (realm!=null)
+            realm.close();
     }
 }
