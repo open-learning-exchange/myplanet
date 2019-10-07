708f7c970b83e197ca274c7753707a5b2c54be6b
Chirag Awale <chiragawale777@gmail.com>
2019-10-07 05:53:26 -0500

    update health record (#1225)

--- app/src/main/java/org/ole/planet/myplanet/ui/myhealth/AddMyHealthActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/myhealth/AddMyHealthActivity.java
@@ -3,6 +3,7 @@ package org.ole.planet.myplanet.ui.myhealth;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
@@ -21,7 +22,8 @@ public class AddMyHealthActivity extends AppCompatActivity {
     Realm realm;
     TextInputLayout fname, mname, lname, email, phone, birthplace, birthdate, emergencyNumber, contact, specialNeed, otherNeed;
     Spinner contactType;
-    RealmUserModel userModel;
+    RealmMyHealth userModel;
+    RealmUserModel userModelB;
     String userId;
 
     @Override
@@ -32,21 +34,22 @@ public class AddMyHealthActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         realm = new DatabaseService(this).getRealmInstance();
         userId = getIntent().getStringExtra("userId");
-        userModel = realm.where(RealmUserModel.class).equalTo("id", userId).findFirst();
+        userModel = realm.where(RealmMyHealth.class).equalTo("userId", userId).findFirst();
+        if(userModel == null){
+            userModelB = realm.where(RealmUserModel.class).equalTo("id", userId).findFirst();
+        }
         initViews();
-        findViewById(R.id.btn_submit).setOnClickListener(view -> realm.executeTransactionAsync(this::createExamination, () -> {
-            Utilities.toast(AddMyHealthActivity.this, "My health saved successfully");
-            finish();
-        }));
+        findViewById(R.id.btn_submit).setOnClickListener(view -> realm.executeTransactionAsync(realm -> createExamination(realm), () -> Utilities.toast(AddMyHealthActivity.this, "My health saved successfully")));
     }
 
     private void createExamination(Realm realm) {
-        RealmMyHealth health = realm.createObject(RealmMyHealth.class, UUID.randomUUID().toString());
+        RealmMyHealth health = realm.where(RealmMyHealth.class).equalTo("userId",userId).findFirst();
+        if(health == null) health = realm.createObject(RealmMyHealth.class, UUID.randomUUID().toString());
         health.setFirstName(fname.getEditText().getText().toString());
         health.setMiddleName(mname.getEditText().getText().toString());
         health.setLastName(lname.getEditText().getText().toString());
         health.setEmail(email.getEditText().getText().toString());
-        health.setBirthdate(birthdate.getEditText().getText().toString());
+        health.setBirthDate(birthdate.getEditText().getText().toString());
         health.setBirthPlace(birthplace.getEditText().getText().toString());
         health.setEmergency(emergencyNumber.getEditText().getText().toString());
         health.setContact(contact.getEditText().getText().toString());
@@ -54,6 +57,8 @@ public class AddMyHealthActivity extends AppCompatActivity {
         health.setSpecialNeeds(specialNeed.getEditText().getText().toString());
         health.setOtherNeeds(otherNeed.getEditText().getText().toString());
         health.setUserId(userId);
+//        Log.e("TAG", "Created with "+ userId + " health " + health.getId());
+        finish();
     }
 
     private void initViews() {
@@ -69,14 +74,29 @@ public class AddMyHealthActivity extends AppCompatActivity {
         specialNeed = findViewById(R.id.et_special_need);
         otherNeed = findViewById(R.id.et_other_need);
         contactType = findViewById(R.id.spn_contact_type);
+        populate();
+    }
+
+    public void populate(){
         if (userModel != null) {
             fname.getEditText().setText(userModel.getFirstName());
             mname.getEditText().setText(userModel.getMiddleName());
-            mname.getEditText().setText(userModel.getLastName());
+            lname.getEditText().setText(userModel.getLastName());
             email.getEditText().setText(userModel.getEmail());
-            phone.getEditText().setText(userModel.getPhoneNumber());
-            birthdate.getEditText().setText(userModel.getDob());
+            phone.getEditText().setText(userModel.getPhone());
+            emergencyNumber.getEditText().setText(userModel.getEmergency());
+            birthdate.getEditText().setText(userModel.getBirthDate());
             birthplace.getEditText().setText(userModel.getBirthPlace());
+            specialNeed.getEditText().setText(userModel.getSpecialNeeds());
+            otherNeed.getEditText().setText(userModel.getOtherNeeds());
+        }else if (userModelB != null){
+            fname.getEditText().setText(userModelB.getFirstName());
+            mname.getEditText().setText(userModelB.getMiddleName());
+            lname.getEditText().setText(userModelB.getLastName());
+            email.getEditText().setText(userModelB.getEmail());
+            phone.getEditText().setText(userModelB.getPhoneNumber());
+            birthdate.getEditText().setText(userModelB.getDob());
+            birthplace.getEditText().setText(userModelB.getBirthPlace());
         }
     }
 
