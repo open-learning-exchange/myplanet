0bef8dd0886f43582bceb85d48e50030ffd86cd2
Roshan Rijal <rijalroshan94@gmail.com>
2019-12-18 21:57:37 +0545

    fixed server not found issue

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.java
@@ -14,6 +14,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import org.ole.planet.myplanet.R;
@@ -125,10 +127,8 @@ public abstract class SyncActivity extends ProcessUserDataActivity implements Sy
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
                     progressDialog.dismiss();
-                    Utilities.log("RES " + response.body().string() + " \n\n\n" + processedUrl);
-                    List<String> myList = Arrays.asList(response.body().string().split(","));
-                    Utilities.log("List size "  + myList.size());
-                    if (myList.size() < 8) {
+                    JsonArray arr = new Gson().fromJson(response.body().string(), JsonArray.class);
+                    if (arr.size() < 8) {
                         alertDialogOkay("Check the server address again. What i connected to wasn't the Planet Server");
                     } else {
                         startSync();
