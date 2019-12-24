72de2819c7400e3ea9cf80ff70e8aa47a26b351b
roshan rijal <rijalroshan94@gmail.com>
2019-12-24 21:30:58 +0545

    add image in discussion

--- app/src/main/java/org/ole/planet/myplanet/ui/team/teamDiscussion/DiscussionListFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamDiscussion/DiscussionListFragment.java
@@ -1,6 +1,9 @@
 package org.ole.planet.myplanet.ui.team.teamDiscussion;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -9,11 +12,16 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.ole.planet.myplanet.R;
@@ -21,13 +29,17 @@ import org.ole.planet.myplanet.model.RealmNews;
 import org.ole.planet.myplanet.model.RealmTeamNotification;
 import org.ole.planet.myplanet.ui.news.AdapterNews;
 import org.ole.planet.myplanet.ui.team.BaseTeamFragment;
+import org.ole.planet.myplanet.utilities.FileUtils;
 import org.ole.planet.myplanet.utilities.Utilities;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
 import io.realm.Realm;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,6 +48,9 @@ public class DiscussionListFragment extends BaseTeamFragment {
 
     RecyclerView rvDiscussion;
     TextView tvNodata;
+    String imageName = "", imageUrl = "";
+
+    ImageView thumb;
 
     public DiscussionListFragment() {
     }
@@ -58,7 +73,7 @@ public class DiscussionListFragment extends BaseTeamFragment {
         int count = realmNewsList.size();
         mRealm.executeTransactionAsync(realm -> {
             RealmTeamNotification notification = realm.where(RealmTeamNotification.class).equalTo("type", "chat").equalTo("parentId", teamId).findFirst();
-            if (notification == null){
+            if (notification == null) {
                 notification = realm.createObject(RealmTeamNotification.class, UUID.randomUUID().toString());
                 notification.setParentId(teamId);
                 notification.setType("chat");
@@ -81,6 +96,8 @@ public class DiscussionListFragment extends BaseTeamFragment {
     private void showAddMessage() {
         View v = getLayoutInflater().inflate(R.layout.alert_input, null);
         TextInputLayout layout = v.findViewById(R.id.tl_input);
+        thumb = v.findViewById(R.id.thumb);
+        v.findViewById(R.id.add_news_image).setOnClickListener(vi -> FileUtils.openOleFolder(this));
         layout.setHint(getString(R.string.enter_message));
         new AlertDialog.Builder(getActivity())
                 .setView(v)
@@ -97,6 +114,8 @@ public class DiscussionListFragment extends BaseTeamFragment {
                     map.put("message", msg);
                     map.put("messageType", team.getTeamType());
                     map.put("messagePlanetCode", team.getTeamPlanetCode());
+                    map.put("imageUrl", imageUrl);
+                    map.put("imageName", imageName);
                     RealmNews.createNews(map, mRealm, user);
                     Utilities.log("discussion created");
                     rvDiscussion.getAdapter().notifyDataSetChanged();
@@ -106,5 +125,47 @@ public class DiscussionListFragment extends BaseTeamFragment {
     @Override
     public void setData(List<RealmNews> list) {
         showRecyclerView(list);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Uri url = null;
+            String path = "";
+            url = data.getData();
+            path = FileUtils.getRealPathFromURI(getActivity(), url);
+            if (TextUtils.isEmpty(path)) {
+                path = getImagePath(url);
+            }
+            imageUrl = path;
+            imageName = FileUtils.getFileNameFromUrl(path);
+            try {
+                thumb.setVisibility(View.VISIBLE);
+                Glide.with(getActivity())
+                        .load(new File(imageUrl))
+                        .into(thumb);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public String getImagePath(Uri uri) {
+        Cursor cursor = getContext().getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContext().getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
     }
 }
