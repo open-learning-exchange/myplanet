8cade88df3c4bdfdd9043c88022092da30ac5a75
roshan rijal <rijalroshan94@gmail.com>
2020-01-03 23:44:34 +0545

    show health worker name

--- app/src/main/java/org/ole/planet/myplanet/ui/myhealth/MyHealthFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/myhealth/MyHealthFragment.java
@@ -194,7 +194,9 @@ public class MyHealthFragment extends Fragment {
             List<RealmExamination> list = mm.getEvents();
             rvRecord.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
             rvRecord.setNestedScrollingEnabled(false);
-            rvRecord.setAdapter(new AdapterHealthExamination(getActivity(), list,mh, userModel ));
+            AdapterHealthExamination adapter = new AdapterHealthExamination(getActivity(), list,mh, userModel );
+            adapter.setmRealm(mRealm);
+            rvRecord.setAdapter(adapter);
             List<RealmExamination> finalList = list;
             rvRecord.post(() -> rvRecord.scrollToPosition(finalList.size() - 1));
         } else {
