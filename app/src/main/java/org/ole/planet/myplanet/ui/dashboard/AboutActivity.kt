70a72126b59df4f91b96c9c516e75eb51482ea0f
roshan rijal <rijalroshan94@gmail.com>
2020-01-10 22:35:19 +0545

    about page

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/AboutActivity.kt
@@ -0,0 +1,12 @@
+package org.ole.planet.myplanet.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
