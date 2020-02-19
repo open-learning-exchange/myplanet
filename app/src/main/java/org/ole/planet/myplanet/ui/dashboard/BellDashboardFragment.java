f3d793247a0e880934374b2a9a0fc597ae3dffde
roshan rijal <rijalroshan94@gmail.com>
2020-02-19 22:04:38 +0545

    added toggle group

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.java
@@ -75,7 +75,7 @@ public class BellDashboardFragment extends BaseDashboardFragment {
         view.findViewById(R.id.myLibraryImageButton).setOnClickListener(v -> openHelperFragment(new LibraryFragment()));
         view.findViewById(R.id.myCoursesImageButton).setOnClickListener(v -> openHelperFragment(new CourseFragment()));
         view.findViewById(R.id.fab_my_progress).setOnClickListener(v -> openHelperFragment(new MyProgressFragment()));
-        view.findViewById(R.id.fab_my_activity).setOnClickListener(v -> openHelperFragment(new MyProgressFragment()));
+        view.findViewById(R.id.fab_my_activity).setOnClickListener(v -> openHelperFragment(new MyActivityFragment()));
         view.findViewById(R.id.fab_survey).setOnClickListener(v -> openHelperFragment(new SurveyFragment()));
         view.findViewById(R.id.fab_feedback).setOnClickListener(v -> openHelperFragment(new FeedbackListFragment()));
         view.findViewById(R.id.myLifeImageButton).setOnClickListener(v -> homeItemClickListener.openCallFragment(new LifeFragment()));
--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/MyActivityFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/MyActivityFragment.kt
@@ -6,17 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
+import io.realm.Realm
 import kotlinx.android.synthetic.main.fragment_my_activity.*
 import org.ole.planet.myplanet.R
+import org.ole.planet.myplanet.datamanager.DatabaseService
+import org.ole.planet.myplanet.model.RealmResourceActivity
+import org.ole.planet.myplanet.service.UserProfileDbHandler
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class MyActivityFragment : Fragment() {
-
+    lateinit var realm : Realm;
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_my_activity, container, false)
@@ -24,8 +26,14 @@ class MyActivityFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        var entries = ArrayList<Entry>();
-        
+        var userModel = UserProfileDbHandler(activity!!).userModel
+        realm = DatabaseService(activity!!).realmInstance
+        var resourceActivity = realm.where(RealmResourceActivity::class.java).equalTo("user", userModel.name).findAll()
+
+
+        resourceActivity.forEach {
+
+        }
     }
 
 }
