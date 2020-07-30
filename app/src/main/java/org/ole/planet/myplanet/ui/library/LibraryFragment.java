c6c48432557fdc8d9e0278ba10f8ee8d0a8c284b
Leonard Mensah <leomaxi@outlook.com>
2020-07-30 06:39:05 +0000

    Version update

--- app/src/main/java/org/ole/planet/myplanet/ui/library/LibraryFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/library/LibraryFragment.java
@@ -116,7 +116,7 @@ public class LibraryFragment extends BaseRecyclerFragment<RealmMyLibrary> implem
                 confirmation = createAlertDialog();
                 confirmation.show();
                 addToMyList();
-                clearSelectedItems();
+                selectedItems.clear();
                 tvAddToLib.setEnabled( false );  // After clearing selectedItems size is always 0
             }
         });
