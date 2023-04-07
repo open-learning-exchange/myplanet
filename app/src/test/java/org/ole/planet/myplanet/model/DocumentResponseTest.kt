ba76d23aa9f9f07dc7e3191146ce0b4f7214c4cc
Darkknight123 <philipkerry57@gmail.com>
2023-04-07 19:42:48 +0300

    test case

---
+++ app/src/test/java/org/ole/planet/myplanet/model/DocumentResponseTest.kt
@@ -0,0 +1,30 @@
+package org.ole.planet.myplanet.model
+
+import android.provider.DocumentsContract
+import junit.framework.TestCase
+import org.w3c.dom.Document
+
+class DocumentResponseTest : TestCase() {
+    private lateinit var response: DocumentResponse
+
+
+    public override fun setUp() {
+        super.setUp()
+        // Create a new DocumentResponse object for testing
+        response = DocumentResponse()
+        response.total_rows = 10.toString()
+        response.offset = 5.toString()
+
+    }
+
+    fun testGetTotal_rows() {
+        // Test that the getTotal_rows() method returns the correct value
+        assertEquals(10, response.total_rows.toInt())
+    }
+
+    fun testGetOffset() {
+        // Test that the getOffset() method returns the correct value
+        assertEquals(5, response.offset.toInt())
+    }
+
+}
