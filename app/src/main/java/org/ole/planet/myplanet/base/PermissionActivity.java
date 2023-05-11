fd81578aadf15489f321c5e9af4669ad553c00aa
Gideon Okuro <gideonollonde@gmail.com>
2023-05-11 20:51:10 +0300

    enhanced permission for storage access (fixes #2122) (#2121)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/base/PermissionActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/base/PermissionActivity.java
@@ -5,14 +5,20 @@ import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.Settings;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.ole.planet.myplanet.utilities.Utilities;
+
+import java.util.ArrayList;
 
 public abstract class PermissionActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE_FILE = 111;
@@ -23,8 +29,7 @@ public abstract class PermissionActivity extends AppCompatActivity {
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
-
-    public void checkUsagesPermission(){
+    public void checkUsagesPermission() {
         if (!getUsagesPermission(this)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 Utilities.toast(this, "Please allow usages permission to myPlanet app.");
@@ -32,16 +37,17 @@ public abstract class PermissionActivity extends AppCompatActivity {
             }
         }
     }
+
     public void requestPermission(String strPermission, int perCode) {
-        ActivityCompat.requestPermissions(this, new String[]{strPermission}, perCode);
+        if (!checkPermission(strPermission)) {
+            ActivityCompat.requestPermissions(this, new String[]{strPermission}, perCode);
+        }
     }
 
     public boolean getUsagesPermission(Context context) {
         boolean granted = false;
-        AppOpsManager appOps = (AppOpsManager) context
-                .getSystemService(Context.APP_OPS_SERVICE);
-        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(), context.getPackageName());
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.getPackageName());
 
         if (mode == AppOpsManager.MODE_DEFAULT) {
             granted = (context.checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
@@ -51,15 +57,54 @@ public abstract class PermissionActivity extends AppCompatActivity {
         return granted;
     }
 
-    public void requestPermission() {
-        if (!checkPermission(Manifest.permission.RECORD_AUDIO) || !checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) || !checkPermission(Manifest.permission.CAMERA) || !checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION) || !checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.RECORD_AUDIO,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            }, PERMISSION_REQUEST_CODE_FILE);
+    public void requestAllPermissions() {
+        ArrayList<String> permissions = new ArrayList<>();
+
+        if (!checkPermission(Manifest.permission.RECORD_AUDIO)) {
+            permissions.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (!checkPermission(Manifest.permission.CAMERA)) {
+            permissions.add(Manifest.permission.CAMERA);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, PERMISSION_REQUEST_CODE_FILE);
+        } else {
+            if (!checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+        }
+
+        if (!checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (!checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (!permissions.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissions.toArray(new String[0]), PERMISSION_REQUEST_CODE_FILE);
+        }
+
+        if (!permissions.isEmpty()) {
+            String[] permissionsArray = permissions.toArray(new String[0]);
+            ActivityCompat.requestPermissions(this, permissionsArray, PERMISSION_REQUEST_CODE_FILE);
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE_FILE || requestCode == PERMISSION_REQUEST_CODE_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permissions Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
