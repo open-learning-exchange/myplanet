f9e755e0849c3ed59151d4711664a8592d7b673f
Gideon Okuro <gideonollonde@gmail.com>
2023-06-20 14:37:33 +0300

    call wifi status switch in dashboardActivity

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardActivity.java
@@ -48,6 +48,7 @@ import org.ole.planet.myplanet.ui.survey.SurveyFragment;
 import org.ole.planet.myplanet.ui.sync.DashboardElementActivity;
 import org.ole.planet.myplanet.ui.team.TeamFragment;
 import org.ole.planet.myplanet.utilities.BottomNavigationViewHelper;
+import org.ole.planet.myplanet.utilities.Constants;
 import org.ole.planet.myplanet.utilities.KeyboardUtils;
 import org.ole.planet.myplanet.utilities.LocaleHelper;
 import org.ole.planet.myplanet.utilities.Utilities;
@@ -160,6 +161,9 @@ public class DashboardActivity extends DashboardElementActivity implements OnHom
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
+                    case R.id.menu_goOnline:
+                        wifiStatusSwitch();
+                        break;
                     case R.id.action_sync:
                         syncNow();
                         break;
@@ -432,6 +436,7 @@ public class DashboardActivity extends DashboardElementActivity implements OnHom
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_bell_dashboard, menu);
+        menu.findItem(R.id.menu_goOnline).setVisible(Constants.showBetaFeature(Constants.KEY_SYNC, this));
         return super.onCreateOptionsMenu(menu);
     }
 
