0bdf97dfdb3123cddb3dcb8d871f4c7331dabb27
Gideon Okuro <gideonollonde@gmail.com>
2023-07-14 11:47:36 +0300

    change title filter to switch between alphabetic ascending and descending order

--- app/src/main/java/org/ole/planet/myplanet/ui/course/AdapterCourses.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/course/AdapterCourses.java
@@ -55,6 +55,7 @@ public class AdapterCourses extends RecyclerView.Adapter<RecyclerView.ViewHolder
     private ChipCloudConfig config;
     private Markwon markwon;
     private boolean isAscending = true;
+    private boolean isTitleAscending = true;
     public AdapterCourses(Context context, List<RealmMyCourse> courseList, HashMap<String, JsonObject> map) {
         this.map = map;
         this.context = context;
@@ -89,7 +90,18 @@ public class AdapterCourses extends RecyclerView.Adapter<RecyclerView.ViewHolder
     public void setCourseList(List<RealmMyCourse> courseList) {
         this.courseList = courseList;
         sortCourseList();
+        sortCourseListByTitle();
         notifyDataSetChanged();
+    }
+
+    private void sortCourseListByTitle() {
+        Collections.sort(courseList, (course1, course2) -> {
+            if (isTitleAscending) {
+                return course1.getCourseTitle().compareToIgnoreCase(course2.getCourseTitle());
+            } else {
+                return course2.getCourseTitle().compareToIgnoreCase(course1.getCourseTitle());
+            }
+        });
     }
 
     private void sortCourseList() {
@@ -103,6 +115,12 @@ public class AdapterCourses extends RecyclerView.Adapter<RecyclerView.ViewHolder
                 }
             }
         });
+    }
+
+    public void toggleTitleSortOrder() {
+        isTitleAscending = !isTitleAscending;
+        sortCourseListByTitle();
+        notifyDataSetChanged();
     }
 
     public void toggleSortOrder() {
