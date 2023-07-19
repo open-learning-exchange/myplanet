3470cec59daff9c7ebdf85b06d7df4981dd0e2ee
Gideon Okuro <gideonollonde@gmail.com>
2023-07-19 16:55:56 +0300

    add chat fragment layout

--- app/src/main/java/org/ole/planet/myplanet/ui/chat/ChatFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/chat/ChatFragment.kt
@@ -5,56 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 import org.ole.planet.myplanet.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var sendMessage: ImageView
+    private lateinit var chatMessage: EditText
+    private lateinit var imageLoading: ImageView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_chat, container, false)
+        sendMessage = rootView.findViewById(R.id.button_gchat_send)
+        chatMessage = rootView.findViewById(R.id.edit_gchat_message)
+        recyclerView = rootView.findViewById(R.id.recycler_gchat)
+        imageLoading = rootView.findViewById(R.id.image_gchat_loading)
+
+        sendMessage.setOnClickListener {
+
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return rootView
     }
 }
