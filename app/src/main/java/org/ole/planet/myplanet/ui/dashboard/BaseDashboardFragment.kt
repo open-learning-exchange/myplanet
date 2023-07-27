6c63d66052bcb5fc5e06b5711b3bc29cb930fbfc
dogi <dogi@users.noreply.github.com>
2023-07-27 12:12:14 -0400

    Update BaseDashboardFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BaseDashboardFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BaseDashboardFragment.kt
@@ -6,7 +6,6 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Typeface
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
