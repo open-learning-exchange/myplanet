5354e2a5171608bc5792a13ae05b88b4517a1526
Gideon Okuro <gideonollonde@gmail.com>
2023-08-17 19:34:02 +0300

    logs: remove gps fragments (fixes #2309) (#2310)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/model/MyPlanet.java
+++ app/src/main/java/org/ole/planet/myplanet/model/MyPlanet.java
@@ -145,10 +145,6 @@ public class MyPlanet implements Serializable {
         postJSON.addProperty("deviceName", NetworkUtils.getDeviceName());
         postJSON.addProperty("time", new Date().getTime());
         postJSON.addProperty("type", "sync");
-        JsonObject gps = new JsonObject();
-        gps.addProperty("latitude", pref.getString("last_lat", ""));
-        gps.addProperty("longitude", pref.getString("last_lng", ""));
-        postJSON.add("gps", gps);
         return postJSON;
     }
 
@@ -182,10 +178,6 @@ public class MyPlanet implements Serializable {
             object.addProperty("customDeviceName", NetworkUtils.getCustomDeviceName(context));
             object.addProperty("deviceName", NetworkUtils.getDeviceName());
             object.addProperty("time", new Date().getTime());
-            JsonObject gps = new JsonObject();
-            gps.addProperty("latitude", pref.getString("last_lat", ""));
-            gps.addProperty("longitude", pref.getString("last_lng", ""));
-            object.add("gps", gps);
             arr.add(object);
         }
     }
