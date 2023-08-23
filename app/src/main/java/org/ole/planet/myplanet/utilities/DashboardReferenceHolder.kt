6350f3a7858fb4049b3e5a1155be98fe3e5a7b58
Gideon Okuro <gideonollonde@gmail.com>
2023-08-23 11:26:48 +0300

    create a dashboard reference holder

---
+++ app/src/main/java/org/ole/planet/myplanet/utilities/DashboardReferenceHolder.kt
@@ -0,0 +1,16 @@
+package org.ole.planet.myplanet.utilities
+
+import android.view.View
+import android.widget.ImageView
+import com.google.android.material.tabs.TabLayout
+
+class DashboardReferenceHolder {
+    var begin: View? = null
+    var img: ImageView? = null
+    var menuh: TabLayout.Tab? = null
+    var menul: TabLayout.Tab? = null
+    var menuc: TabLayout.Tab? = null
+    var menue: TabLayout.Tab? = null
+    var menuco: TabLayout.Tab? = null
+    var menut: TabLayout.Tab? = null
+}
