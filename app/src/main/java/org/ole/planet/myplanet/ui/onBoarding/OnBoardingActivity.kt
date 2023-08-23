d9219711667e7a42b944837722363f9a8401b465
Gideon Okuro <gideonollonde@gmail.com>
2023-08-23 14:48:34 +0300

    add on boarding activity layout

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/onBoarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package org.ole.planet.myplanet.ui.onBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.ole.planet.myplanet.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
