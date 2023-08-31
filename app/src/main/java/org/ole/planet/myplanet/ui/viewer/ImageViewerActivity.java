84869240cf1e1e8573206acb1b7c732aa7ec3be2
Gideon Okuro <gideonollonde@gmail.com>
2023-08-31 14:38:56 +0300

    implement viewBinding in imageViewerActivity

--- app/src/main/java/org/ole/planet/myplanet/ui/viewer/ImageViewerActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/ImageViewerActivity.java
@@ -3,34 +3,25 @@ package org.ole.planet.myplanet.ui.viewer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
-import org.ole.planet.myplanet.R;
-import org.ole.planet.myplanet.utilities.Utilities;
+import org.ole.planet.myplanet.databinding.ActivityImageViewerBinding;
 
 import java.io.File;
 
 public class ImageViewerActivity extends AppCompatActivity {
-    private TextView mImageFileNameTitle;
-    private ImageView mImageViewer;
+    private ActivityImageViewerBinding activityImageViewerBinding;
     private boolean isFullPath = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_viewer);
-        declareElements();
+        activityImageViewerBinding = ActivityImageViewerBinding.inflate(getLayoutInflater());
+        setContentView(activityImageViewerBinding.getRoot());
         renderImageFile();
-    }
-
-    private void declareElements() {
-        mImageFileNameTitle = (TextView) findViewById(R.id.imageFileName);
-        mImageViewer = (ImageView) findViewById(R.id.imageViewer);
     }
 
     private void renderImageFile() {
@@ -38,8 +29,8 @@ public class ImageViewerActivity extends AppCompatActivity {
         Intent imageOpenIntent = getIntent();
         String fileName = imageOpenIntent.getStringExtra("TOUCHED_FILE");
         if (fileName != null && !fileName.isEmpty()) {
-            mImageFileNameTitle.setText(fileName);
-            mImageFileNameTitle.setVisibility(View.VISIBLE);
+            activityImageViewerBinding.imageFileName.setText(fileName);
+            activityImageViewerBinding.imageFileName.setVisibility(View.VISIBLE);
         }
 
         try {
@@ -50,7 +41,7 @@ public class ImageViewerActivity extends AppCompatActivity {
                 File basePath = getExternalFilesDir(null);
                 imageFile = new File(basePath, "ole/" + fileName);
             }
-            Glide.with(getApplicationContext()).load(imageFile).into(mImageViewer);
+            Glide.with(getApplicationContext()).load(imageFile).into(activityImageViewerBinding.imageViewer);
         } catch (Exception e) {
             e.printStackTrace();
         }
