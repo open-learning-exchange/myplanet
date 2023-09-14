e67301c07667376b0b92cb63302af9ac353c2f76
Gideon Okuro <gideonollonde@gmail.com>
2023-09-14 20:31:14 +0300

    resources: refactor viewer modules (fixes #2390) (#2394)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/viewer/MarkdownViewerActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/MarkdownViewerActivity.java
@@ -3,34 +3,25 @@ package org.ole.planet.myplanet.ui.viewer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.ole.planet.myplanet.R;
-import org.ole.planet.myplanet.utilities.Utilities;
+import org.ole.planet.myplanet.databinding.ActivityMarkdownViewerBinding;
 
 import java.io.File;
 
-import br.tiagohm.markdownview.MarkdownView;
-
 public class MarkdownViewerActivity extends AppCompatActivity {
-    private TextView mMarkdownNameTitle;
-    private MarkdownView mMarkdownContent;
+    private ActivityMarkdownViewerBinding activityMarkdownViewerBinding;
     private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_markdown_viewer);
-        declareElements();
+        activityMarkdownViewerBinding = ActivityMarkdownViewerBinding.inflate(getLayoutInflater());
+        setContentView(activityMarkdownViewerBinding.getRoot());
         renderMarkdownFile();
-    }
-
-    private void declareElements() {
-        mMarkdownNameTitle = (TextView) findViewById(R.id.markdownFileName);
-        mMarkdownContent = (MarkdownView) findViewById(R.id.markdown_view);
     }
 
     private void renderMarkdownFile() {
@@ -38,8 +29,8 @@ public class MarkdownViewerActivity extends AppCompatActivity {
         String fileName = markdownOpenIntent.getStringExtra("TOUCHED_FILE");
 
         if (fileName != null && !fileName.isEmpty()) {
-            mMarkdownNameTitle.setText(fileName);
-            mMarkdownNameTitle.setVisibility(View.VISIBLE);
+            activityMarkdownViewerBinding.markdownFileName.setText(fileName);
+            activityMarkdownViewerBinding.markdownFileName.setVisibility(View.VISIBLE);
         }
 
         try {
@@ -47,7 +38,7 @@ public class MarkdownViewerActivity extends AppCompatActivity {
             File markdownFile = new File(basePath, "ole/" + fileName);
 
             if (markdownFile.exists()) {
-                mMarkdownContent.loadMarkdownFromFile(markdownFile);
+                activityMarkdownViewerBinding.markdownView.loadMarkdownFromFile(markdownFile);
             } else {
                 Toast.makeText(this, getString(R.string.unable_to_load) + fileName, Toast.LENGTH_LONG).show();
             }
