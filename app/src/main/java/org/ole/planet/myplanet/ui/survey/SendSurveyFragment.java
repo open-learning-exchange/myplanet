9864b9baa00045070107af6c5d0702f601b93a75
Gideon Okuro <gideonollonde@gmail.com>
2023-09-29 20:27:02 +0300

    survey: refactor towards viewbinding (fixes #2533) (#2538)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/survey/SendSurveyFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/survey/SendSurveyFragment.java
@@ -8,13 +8,15 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+
 import org.ole.planet.myplanet.R;
 import org.ole.planet.myplanet.base.BaseDialogFragment;
+import org.ole.planet.myplanet.databinding.FragmentSendSurveyBinding;
 import org.ole.planet.myplanet.datamanager.DatabaseService;
 import org.ole.planet.myplanet.model.RealmStepExam;
 import org.ole.planet.myplanet.model.RealmSubmission;
 import org.ole.planet.myplanet.model.RealmUserModel;
-import org.ole.planet.myplanet.utilities.CheckboxListView;
 import org.ole.planet.myplanet.utilities.Utilities;
 
 import java.util.Date;
@@ -24,7 +26,7 @@ import io.realm.Realm;
 import io.realm.Sort;
 
 public class SendSurveyFragment extends BaseDialogFragment {
-    CheckboxListView listView;
+    private FragmentSendSurveyBinding fragmentSendSurveyBinding;
     Realm mRealm;
     DatabaseService dbService;
 
@@ -32,18 +34,17 @@ public class SendSurveyFragment extends BaseDialogFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_send_survey, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        fragmentSendSurveyBinding = FragmentSendSurveyBinding.inflate(inflater, container, false);
 
-        listView = v.findViewById(R.id.list_users);
         dbService = new DatabaseService(getActivity());
         mRealm = dbService.getRealmInstance();
         if (TextUtils.isEmpty(id)) {
             dismiss();
-            return v;
+            return fragmentSendSurveyBinding.getRoot();
         }
-        v.findViewById(R.id.btn_cancel).setOnClickListener(view -> dismiss());
-        return v;
+        fragmentSendSurveyBinding.btnCancel.setOnClickListener(view -> dismiss());
+        return fragmentSendSurveyBinding.getRoot();
     }
 
     private void createSurveySubmission(String userId) {
@@ -65,8 +66,8 @@ public class SendSurveyFragment extends BaseDialogFragment {
         super.onActivityCreated(savedInstanceState);
         List<RealmUserModel> users = mRealm.where(RealmUserModel.class).findAll();
         initListView(users);
-        getView().findViewById(R.id.send_survey).setOnClickListener(view -> {
-            for (int i = 0; i < listView.getSelectedItemsList().size(); i++) {
+        fragmentSendSurveyBinding.sendSurvey.setOnClickListener(view -> {
+            for (int i = 0; i < fragmentSendSurveyBinding.listUsers.getSelectedItemsList().size(); i++) {
                 RealmUserModel u = users.get(i);
                 createSurveySubmission(u.getId());
             }
@@ -77,8 +78,8 @@ public class SendSurveyFragment extends BaseDialogFragment {
 
     private void initListView(List<RealmUserModel> users) {
         ArrayAdapter<RealmUserModel> adapter = new ArrayAdapter<RealmUserModel>(getActivity(), R.layout.rowlayout, R.id.checkBoxRowLayout, users);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listView.setAdapter(adapter);
+        fragmentSendSurveyBinding.listUsers.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        fragmentSendSurveyBinding.listUsers.setAdapter(adapter);
     }
 
     @Override
