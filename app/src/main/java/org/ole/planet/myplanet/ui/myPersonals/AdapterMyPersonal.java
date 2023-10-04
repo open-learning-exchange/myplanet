e73ae224250ab9955d1f37d2b2f022e3c586fdfc
Gideon Okuro <gideonollonde@gmail.com>
2023-10-04 20:29:05 +0300

    addons: viewbinding in myPersonals module (fixes #2564) (#2566)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/myPersonals/AdapterMyPersonal.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/myPersonals/AdapterMyPersonal.java
@@ -18,7 +18,11 @@ import android.widget.TextView;
 
 import org.ole.planet.myplanet.R;
 import org.ole.planet.myplanet.callback.OnSelectedMyPersonal;
+import org.ole.planet.myplanet.databinding.AlertMyPersonalBinding;
+import org.ole.planet.myplanet.databinding.RowFeedbackBinding;
+import org.ole.planet.myplanet.databinding.RowMyPersonalBinding;
 import org.ole.planet.myplanet.model.RealmMyPersonal;
+import org.ole.planet.myplanet.ui.feedback.AdapterFeedback;
 import org.ole.planet.myplanet.ui.viewer.ImageViewerActivity;
 import org.ole.planet.myplanet.ui.viewer.PDFReaderActivity;
 import org.ole.planet.myplanet.ui.viewer.VideoPlayerActivity;
@@ -31,7 +35,8 @@ import java.util.List;
 
 import io.realm.Realm;
 
-public class AdapterMyPersonal extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class AdapterMyPersonal extends RecyclerView.Adapter<AdapterMyPersonal.ViewHolderMyPersonal> {
+    private RowMyPersonalBinding rowMyPersonalBinding;
     private Context context;
     private Realm realm;
     private List<RealmMyPersonal> list;
@@ -52,36 +57,34 @@ public class AdapterMyPersonal extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.row_my_personal, parent, false);
-        return new ViewHolderMyPersonal(v);
+    public ViewHolderMyPersonal onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        rowMyPersonalBinding = RowMyPersonalBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new ViewHolderMyPersonal(rowMyPersonalBinding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof ViewHolderMyPersonal) {
-            ((ViewHolderMyPersonal) holder).title.setText(list.get(position).getTitle());
-            ((ViewHolderMyPersonal) holder).description.setText(list.get(position).getDescription());
-            ((ViewHolderMyPersonal) holder).date.setText(TimeUtils.getFormatedDate(list.get(position).getDate()));
-            ((ViewHolderMyPersonal) holder).ivDelete.setOnClickListener(view -> new AlertDialog.Builder(context).setMessage(R.string.delete_record).setPositiveButton(R.string.ok, (dialogInterface, i) -> {
-                if (!realm.isInTransaction()) realm.beginTransaction();
-                RealmMyPersonal personal = realm.where(RealmMyPersonal.class).equalTo("_id", list.get(position).get_id()).findFirst();
-                personal.deleteFromRealm();
-                realm.commitTransaction();
-                notifyDataSetChanged();
-                listener.onAddedResource();
-            }).setNegativeButton(R.string.cancel, null).show());
+    public void onBindViewHolder(@NonNull ViewHolderMyPersonal holder, int position) {
+        rowMyPersonalBinding.title.setText(list.get(position).getTitle());
+        rowMyPersonalBinding.description.setText(list.get(position).getDescription());
+        rowMyPersonalBinding.date.setText(TimeUtils.getFormatedDate(list.get(position).getDate()));
+        rowMyPersonalBinding.imgDelete.setOnClickListener(view -> new AlertDialog.Builder(context).setMessage(R.string.delete_record).setPositiveButton(R.string.ok, (dialogInterface, i) -> {
+            if (!realm.isInTransaction()) realm.beginTransaction();
+            RealmMyPersonal personal = realm.where(RealmMyPersonal.class).equalTo("_id", list.get(position).get_id()).findFirst();
+            personal.deleteFromRealm();
+            realm.commitTransaction();
+            notifyDataSetChanged();
+            listener.onAddedResource();
+        }).setNegativeButton(R.string.cancel, null).show());
 
-            ((ViewHolderMyPersonal) holder).ivEdit.setOnClickListener(view -> {
-                editPersonal(list.get(position));
-            });
-            holder.itemView.setOnClickListener(view -> {
-                openResource(list.get(position).getPath());
-            });
-            ((ViewHolderMyPersonal) holder).ivUpload.setOnClickListener(v -> {
-                if (listener != null) listener.onUpload(list.get(position));
-            });
-        }
+        rowMyPersonalBinding.imgEdit.setOnClickListener(view -> {
+            editPersonal(list.get(position));
+        });
+        holder.itemView.setOnClickListener(view -> {
+            openResource(list.get(position).getPath());
+        });
+        rowMyPersonalBinding.imgUpload.setOnClickListener(v -> {
+            if (listener != null) listener.onUpload(list.get(position));
+        });
     }
 
     private void openResource(String path) {
@@ -121,44 +124,47 @@ public class AdapterMyPersonal extends RecyclerView.Adapter<RecyclerView.ViewHol
     }
 
     private void editPersonal(RealmMyPersonal personal) {
-        View v = LayoutInflater.from(context).inflate(R.layout.alert_my_personal, null);
-        EditText etTitle = v.findViewById(R.id.et_title);
-        EditText etDesc = v.findViewById(R.id.et_description);
-        etDesc.setText(personal.getDescription());
-        etTitle.setText(personal.getTitle());
-        new AlertDialog.Builder(context).setTitle(R.string.edit_personal).setIcon(R.drawable.ic_edit).setView(v).setPositiveButton(R.string.button_submit, (dialogInterface, i) -> {
-            String title = etTitle.getText().toString().trim();
-            String desc = etDesc.getText().toString().trim();
-            if (title.isEmpty()) {
-                Utilities.toast(context, String.valueOf(R.string.please_enter_title));
-                return;
-            }
-            if (!realm.isInTransaction()) realm.beginTransaction();
-            personal.setDescription(desc);
-            personal.setTitle(title);
-            realm.commitTransaction();
-            notifyDataSetChanged();
-            listener.onAddedResource();
-        }).setNegativeButton(R.string.cancel, null).show();
+        AlertMyPersonalBinding alertMyPersonalBinding = AlertMyPersonalBinding.inflate(LayoutInflater.from(context));
+        alertMyPersonalBinding.etDescription.setText(personal.getDescription());
+        alertMyPersonalBinding.etTitle.setText(personal.getTitle());
+
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.edit_personal)
+                .setIcon(R.drawable.ic_edit)
+                .setView(alertMyPersonalBinding.getRoot())
+                .setPositiveButton(R.string.button_submit, (dialogInterface, i) -> {
+                    String title = alertMyPersonalBinding.etDescription.getText().toString().trim();
+                    String desc = alertMyPersonalBinding.etTitle.getText().toString().trim();
+
+                    if (title.isEmpty()) {
+                        Utilities.toast(context, String.valueOf(R.string.please_enter_title));
+                        return;
+                    }
+
+                    if (!realm.isInTransaction()) realm.beginTransaction();
+                    personal.setDescription(desc);
+                    personal.setTitle(title);
+                    realm.commitTransaction();
+
+                    notifyDataSetChanged();
+                    listener.onAddedResource();
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
+
 
     @Override
     public int getItemCount() {
         return list.size();
     }
 
-    class ViewHolderMyPersonal extends RecyclerView.ViewHolder {
-        TextView title, description, date;
-        ImageView ivEdit, ivDelete, ivUpload;
+    static class ViewHolderMyPersonal extends RecyclerView.ViewHolder {
+        RowMyPersonalBinding rowMyPersonalBinding;
 
-        public ViewHolderMyPersonal(View itemView) {
-            super(itemView);
-            title = itemView.findViewById(R.id.title);
-            description = itemView.findViewById(R.id.description);
-            date = itemView.findViewById(R.id.date);
-            ivDelete = itemView.findViewById(R.id.img_delete);
-            ivEdit = itemView.findViewById(R.id.img_edit);
-            ivUpload = itemView.findViewById(R.id.img_upload);
+        public ViewHolderMyPersonal(RowMyPersonalBinding rowMyPersonalBinding) {
+            super(rowMyPersonalBinding.getRoot());
+            this.rowMyPersonalBinding = rowMyPersonalBinding;
         }
     }
 }
