7c64e37e8ff1b18b28361bcc8a7b0662a763a48d
Gideon Okuro <gideonollonde@gmail.com>
2023-10-06 20:16:12 +0300

    teams: viewbinding refactor (fixes #2576) (#2578)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/team/teamCourse/TeamCourseFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamCourse/TeamCourseFragment.java
@@ -1,40 +1,30 @@
 package org.ole.planet.myplanet.ui.team.teamCourse;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import org.ole.planet.myplanet.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import org.ole.planet.myplanet.databinding.FragmentTeamCourseBinding;
 import org.ole.planet.myplanet.model.RealmMyCourse;
 import org.ole.planet.myplanet.ui.team.BaseTeamFragment;
 
 import io.realm.RealmResults;
 
 public class TeamCourseFragment extends BaseTeamFragment {
-    RecyclerView rvCourse;
-    TextView tvNodata;
+    private FragmentTeamCourseBinding fragmentTeamCourseBinding;
 
     public TeamCourseFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_team_course, container, false);
-        initView(v);
-        return v;
-    }
-
-    private void initView(View v) {
-        rvCourse = v.findViewById(R.id.rv_course);
-        tvNodata = v.findViewById(R.id.tv_nodata);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        fragmentTeamCourseBinding = FragmentTeamCourseBinding.inflate(inflater, container, false);
+        return fragmentTeamCourseBinding.getRoot();
     }
 
     @Override
@@ -42,8 +32,8 @@ public class TeamCourseFragment extends BaseTeamFragment {
         super.onActivityCreated(savedInstanceState);
         RealmResults<RealmMyCourse> courses = mRealm.where(RealmMyCourse.class).in("id", team.getCourses().toArray(new String[0])).findAll();
         AdapterTeamCourse adapterTeamCourse = new AdapterTeamCourse(getActivity(), courses, mRealm, teamId, settings);
-        rvCourse.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rvCourse.setAdapter(adapterTeamCourse);
-        showNoData(tvNodata, adapterTeamCourse.getItemCount());
+        fragmentTeamCourseBinding.rvCourse.setLayoutManager(new LinearLayoutManager(getActivity()));
+        fragmentTeamCourseBinding.rvCourse.setAdapter(adapterTeamCourse);
+        showNoData(fragmentTeamCourseBinding.tvNodata, adapterTeamCourse.getItemCount());
     }
 }
