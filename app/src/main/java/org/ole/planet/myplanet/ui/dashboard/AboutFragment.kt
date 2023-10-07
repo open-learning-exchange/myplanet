08c18280dfe4e971f8fc15667216d4d46a9135e3
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2023-10-07 23:08:50 +0800

    add version number to about page

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/AboutFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/AboutFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
+import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.databinding.FragmentAboutBinding
 import org.ole.planet.myplanet.utilities.Constants
 
@@ -18,6 +19,8 @@ class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentAboutBinding = FragmentAboutBinding.inflate(inflater, container, false)
         fragmentAboutBinding.tvDisclaimer.text = Html.fromHtml(getString(Constants.ABOUT), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        fragmentAboutBinding.tvVersion.text = resources.getText(R.string.version)
+            .toString() + " " + resources.getText(R.string.app_version)
         return fragmentAboutBinding.root
     }
 }
