a7e3c3cfef9914219909f7de4a5750e51527e6b8
Gideon Okuro <gideonollonde@gmail.com>
2023-10-23 17:40:39 +0300

    ui boilerplate code

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/userprofile/UsersLoginActivity.kt
@@ -0,0 +1,12 @@
+package org.ole.planet.myplanet.ui.userprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.ole.planet.myplanet.R
+
+class UsersLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users_login)
+    }
+}
