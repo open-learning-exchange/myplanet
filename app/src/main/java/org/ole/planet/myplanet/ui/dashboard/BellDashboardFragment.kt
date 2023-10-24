06fc220d952f771ad89c60cbc7d5ee6917fd9cf1
Gideon Okuro <gideonollonde@gmail.com>
2023-10-24 14:45:22 +0300

    implement viewbinding in dashboard

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/BellDashboardFragment.kt
@@ -1,22 +1,8 @@
 package org.ole.planet.myplanet.ui.dashboard
 
-//import kotlinx.android.synthetic.main.card_profile_bell.ll_badges
-//import kotlinx.android.synthetic.main.card_profile_bell.txt_community_name
-//import kotlinx.android.synthetic.main.card_profile_bell.txt_date
-//import kotlinx.android.synthetic.main.card_profile_bell.view.fab_feedback
-//import kotlinx.android.synthetic.main.fragment_home_bell.add_resource
-//import kotlinx.android.synthetic.main.fragment_home_bell.view.fab_my_activity
-//import kotlinx.android.synthetic.main.fragment_home_bell.view.fab_my_progress
-//import kotlinx.android.synthetic.main.fragment_home_bell.view.fab_notification
-//import kotlinx.android.synthetic.main.fragment_home_bell.view.fab_survey
-//import kotlinx.android.synthetic.main.home_card_courses.view.myCoursesImageButton
-//import kotlinx.android.synthetic.main.home_card_library.view.myLibraryImageButton
-//import kotlinx.android.synthetic.main.home_card_mylife.view.myLifeImageButton
-//import kotlinx.android.synthetic.main.home_card_teams.view.ll_home_team
 import android.content.DialogInterface
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,13 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import org.ole.planet.myplanet.MainApplication
 import org.ole.planet.myplanet.R
-import org.ole.planet.myplanet.base.BaseResourceFragment
-import org.ole.planet.myplanet.databinding.CardProfileBellBinding
 import org.ole.planet.myplanet.databinding.FragmentHomeBellBinding
-import org.ole.planet.myplanet.databinding.HomeCardCoursesBinding
-import org.ole.planet.myplanet.databinding.HomeCardLibraryBinding
-import org.ole.planet.myplanet.databinding.HomeCardMylifeBinding
-import org.ole.planet.myplanet.databinding.HomeCardTeamsBinding
 import org.ole.planet.myplanet.model.RealmCertification
 import org.ole.planet.myplanet.model.RealmCourseProgress
 import org.ole.planet.myplanet.model.RealmExamQuestion
@@ -49,31 +29,21 @@ import java.util.Date
 
 class BellDashboardFragment : BaseDashboardFragment() {
     private lateinit var fragmentHomeBellBinding: FragmentHomeBellBinding
-    private lateinit var cardProfileBellBinding: CardProfileBellBinding
-    private lateinit var homeCardTeamsBinding: HomeCardTeamsBinding
-    private lateinit var homeCardCoursesBinding: HomeCardCoursesBinding
-    private lateinit var homeCardLibraryBinding: HomeCardLibraryBinding
-    private lateinit var homeCardMylifeBinding: HomeCardMylifeBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentHomeBellBinding = FragmentHomeBellBinding.inflate(inflater, container, false)
-        cardProfileBellBinding = CardProfileBellBinding.inflate(layoutInflater)
-//        homeCardTeamsBinding = HomeCardTeamsBinding.inflate(layoutInflater)
-        homeCardCoursesBinding = HomeCardCoursesBinding.inflate(layoutInflater)
-        homeCardLibraryBinding = HomeCardLibraryBinding.inflate(layoutInflater)
-        homeCardMylifeBinding = HomeCardMylifeBinding.inflate(layoutInflater)
 
         val view = fragmentHomeBellBinding.root
-
-        declareElements(view)
+        initView(view)
+        declareElements()
         onLoaded(view)
         return fragmentHomeBellBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        cardProfileBellBinding.txtDate.text = TimeUtils.formatDate(Date().time)
-        cardProfileBellBinding.txtCommunityName.text = model.planetCode
+        fragmentHomeBellBinding.cardProfileBell.txtDate.text = TimeUtils.formatDate(Date().time)
+        fragmentHomeBellBinding.cardProfileBell.txtCommunityName.text = model.planetCode
         (activity as DashboardActivity?)?.supportActionBar?.hide()
         fragmentHomeBellBinding.addResource.setOnClickListener {
             AddResourceFragment().show(
@@ -92,10 +62,8 @@ class BellDashboardFragment : BaseDashboardFragment() {
     }
 
     private fun showBadges() {
-        cardProfileBellBinding.llBadges.removeAllViews()
-        val list = RealmCourseProgress.getPassedCourses(
-            mRealm, BaseResourceFragment.settings.getString("userId", "")
-        )
+        fragmentHomeBellBinding.cardProfileBell.llBadges.removeAllViews()
+        val list = RealmCourseProgress.getPassedCourses(mRealm, settings.getString("userId", ""))
         for (sub in list) {
             val star =
                 LayoutInflater.from(activity).inflate(R.layout.image_start, null) as ImageView
@@ -106,7 +74,7 @@ class BellDashboardFragment : BaseDashboardFragment() {
             val questions =
                 mRealm.where(RealmExamQuestion::class.java).equalTo("examId", examId).count()
             setColor(questions, courseId, star)
-            cardProfileBellBinding.llBadges.addView(star)
+            fragmentHomeBellBinding.cardProfileBell.llBadges.addView(star)
         }
     }
 
@@ -117,26 +85,15 @@ class BellDashboardFragment : BaseDashboardFragment() {
             star.setColorFilter(ContextCompat.getColor(requireContext(), R.color.md_blue_grey_300))
         }
 
-    private fun declareElements(view: View) {
-        initView(view)
-        val cardProfileView: View = cardProfileBellBinding.root
-        homeCardTeamsBinding = HomeCardTeamsBinding.inflate(layoutInflater)
-        val homeCardTeamsView: View = homeCardTeamsBinding.root
-        homeCardTeamsView.findViewById<View>(R.id.ll_home_team).setOnClickListener {
-            Log.d("clicked", "clicked")
-            homeItemClickListener.openCallFragment(TeamFragment())
-        }
-        homeCardLibraryBinding.myLibraryImageButton.setOnClickListener { openHelperFragment(LibraryFragment()) }
-        homeCardCoursesBinding.myCoursesImageButton.setOnClickListener { openHelperFragment(CourseFragment()) }
+    private fun declareElements() {
+        fragmentHomeBellBinding.homeCardTeams.llHomeTeam.setOnClickListener { homeItemClickListener.openCallFragment(TeamFragment()) }
+        fragmentHomeBellBinding.homeCardLibrary.myLibraryImageButton.setOnClickListener { openHelperFragment(LibraryFragment()) }
+        fragmentHomeBellBinding.homeCardCourses.myCoursesImageButton.setOnClickListener { openHelperFragment(CourseFragment()) }
         fragmentHomeBellBinding.fabMyProgress.setOnClickListener { openHelperFragment(MyProgressFragment()) }
         fragmentHomeBellBinding.fabMyActivity.setOnClickListener { openHelperFragment(MyActivityFragment()) }
         fragmentHomeBellBinding.fabSurvey.setOnClickListener { openHelperFragment(SurveyFragment()) }
-        view.findViewById<View>(R.id.fab_feedback).setOnClickListener { openHelperFragment(FeedbackListFragment()) }
-        homeCardMylifeBinding.myLifeImageButton.setOnClickListener {
-            homeItemClickListener.openCallFragment(
-                LifeFragment()
-            )
-        }
+        fragmentHomeBellBinding.cardProfileBell.fabFeedback.setOnClickListener { openHelperFragment(FeedbackListFragment()) }
+        fragmentHomeBellBinding.homeCardMyLife.myLifeImageButton.setOnClickListener { homeItemClickListener.openCallFragment(LifeFragment()) }
         fragmentHomeBellBinding.fabNotification.setOnClickListener { showNotificationFragment() }
     }
 
--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.java
@@ -1,24 +1,18 @@
 package org.ole.planet.myplanet.ui.dashboard;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import org.ole.planet.myplanet.R;
-import org.ole.planet.myplanet.databinding.CardProfileBinding;
-import org.ole.planet.myplanet.databinding.FragmentFinanceBinding;
 import org.ole.planet.myplanet.databinding.FragmentHomeBinding;
 import org.ole.planet.myplanet.datamanager.DatabaseService;
 import org.ole.planet.myplanet.model.RealmSubmission;
-import org.ole.planet.myplanet.service.UserProfileDbHandler;
 import org.ole.planet.myplanet.ui.library.AddResourceFragment;
 import org.ole.planet.myplanet.ui.news.NewsFragment;
 import org.ole.planet.myplanet.ui.submission.MySubmissionFragment;
@@ -30,13 +24,9 @@ import io.realm.Realm;
 
 public class DashboardFragment extends BaseDashboardFragment {
     private FragmentHomeBinding fragmentHomeBinding;
-    private CardProfileBinding cardProfileBinding;
     public static final String PREFS_NAME = "OLE_PLANET";
-    TextView txtFullName, txtVisits, tv_surveys, tv_submission, tv_achievement, txtRole, tv_news;
     Realm mRealm;
     DatabaseService dbService;
-    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(250, 100);
-    private UserProfileDbHandler profileDbHandler;
 
     public DashboardFragment() {
     }
@@ -45,15 +35,12 @@ public class DashboardFragment extends BaseDashboardFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = fragmentHomeBinding.getRoot();
-        cardProfileBinding = CardProfileBinding.inflate(getLayoutInflater());
-        View cardProfileView = cardProfileBinding.getRoot();
 
-//        declareElements(cardProfileView);
-        cardProfileView.findViewById(R.id.tv_surveys).setOnClickListener(view12 -> homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("survey")));
-        cardProfileView.findViewById(R.id.tv_news).setOnClickListener(view12 -> homeItemClickListener.openCallFragment(new NewsFragment()));
-        cardProfileView.findViewById(R.id.tv_submission).setOnClickListener(view1 -> homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("exam")));
-        cardProfileView.findViewById(R.id.tv_achievement).setVisibility(Constants.showBetaFeature(Constants.KEY_ACHIEVEMENT, getActivity()) ? View.VISIBLE : View.GONE);
-        cardProfileView.findViewById(R.id.tv_achievement).setOnClickListener(v -> homeItemClickListener.openCallFragment(new AchievementFragment()));
+        fragmentHomeBinding.cardProfile.tvSurveys.setOnClickListener(view12 -> homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("survey")));
+        fragmentHomeBinding.cardProfile.tvNews.setOnClickListener(view12 -> homeItemClickListener.openCallFragment(new NewsFragment()));
+        fragmentHomeBinding.cardProfile.tvSubmission.setOnClickListener(view1 -> homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("exam")));
+        fragmentHomeBinding.cardProfile.tvAchievement.setVisibility(Constants.showBetaFeature(Constants.KEY_ACHIEVEMENT, getActivity()) ? View.VISIBLE : View.GONE);
+        fragmentHomeBinding.cardProfile.tvAchievement.setOnClickListener(v -> homeItemClickListener.openCallFragment(new AchievementFragment()));
         dbService = new DatabaseService(getActivity());
         mRealm = dbService.getRealmInstance();
         onLoaded(view);
@@ -67,18 +54,9 @@ public class DashboardFragment extends BaseDashboardFragment {
         super.onActivityCreated(savedInstanceState);
         int noOfSurvey = RealmSubmission.getNoOfSurveySubmissionByUser(settings.getString("userId", "--"), mRealm);
 
-        cardProfileBinding.imgSurveyWarn.setVisibility(noOfSurvey == 0 ? View.VISIBLE : View.GONE);
+        fragmentHomeBinding.cardProfile.imgSurveyWarn.setVisibility(noOfSurvey == 0 ? View.VISIBLE : View.GONE);
         fragmentHomeBinding.addResource.setOnClickListener(v -> {
             new AddResourceFragment().show(getChildFragmentManager(),  getString(R.string.add_res));
         });
     }
-
-//    private void declareElements(View view) {
-//        tv_surveys = view.findViewById(R.id.tv_surveys);
-//        tv_submission = view.findViewById(R.id.tv_submission);
-//        tv_achievement = view.findViewById(R.id.tv_achievement);
-//        tv_news = view.findViewById(R.id.tv_news);
-
-//        initView(view);
-//    }
 }
