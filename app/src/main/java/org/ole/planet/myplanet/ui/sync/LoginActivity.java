03c40dd07a9f771e557ebc8a30476968378fa3fa
Gideon Okuro <gideonollonde@gmail.com>
2023-10-30 21:27:11 +0300

    settings: new default server `planet learning` (fixes #2643) (#2645)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.java
@@ -10,8 +10,10 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -733,6 +735,7 @@ public class LoginActivity extends SyncActivity implements Service.CheckVersionC
                 settings.edit().putBoolean("switchCloudUrl", b).commit();
                 dialogServerUrlBinding.spnCloud.setVisibility(b ? View.VISIBLE : View.GONE);
                 setUrlAndPin(dialogServerUrlBinding.switchServerUrl.isChecked());
+                Log.d("checked", String.valueOf(dialogServerUrlBinding.switchServerUrl.isChecked()));
             });
             serverUrl.addTextChangedListener(new MyTextWatcher(serverUrl));
             dialogServerUrlBinding.deviceName.setText(getCustomDeviceName());
@@ -760,7 +763,8 @@ public class LoginActivity extends SyncActivity implements Service.CheckVersionC
                 serverUrl.setText(selected.getLocalDomain());
                 protocol_checkin.check(R.id.radio_https);
                 settings.getString("serverProtocol", "https://");
-                serverPassword.setText(selected.getWeight() == 0 ? "0660" : "");
+                serverPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                serverPassword.setText(selected.getWeight() == 0 ? "1983" : "");
                 serverPassword.setEnabled(selected.getWeight() != 0);
             }
         } finally {
@@ -778,8 +782,10 @@ public class LoginActivity extends SyncActivity implements Service.CheckVersionC
             serverPassword.setText(settings.getString("serverPin", ""));
             protocol_checkin.check(TextUtils.equals(settings.getString("serverProtocol", ""), "http://") ? R.id.radio_http : R.id.radio_https);
             serverUrlProtocol.setText(settings.getString("serverProtocol", ""));
+            serverPassword.setTransformationMethod(null);
         }
         serverUrl.setEnabled(!checked);
+        serverPassword.setEnabled(!checked);
         serverPassword.clearFocus();
         serverUrl.clearFocus();
         protocol_checkin.setEnabled(!checked);
