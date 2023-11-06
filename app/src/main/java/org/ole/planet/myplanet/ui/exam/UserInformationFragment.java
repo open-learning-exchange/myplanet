941e1cbdc7d7c4e385400c6f96142cd05c3e8415
Gideon Okuro <gideonollonde@gmail.com>
2023-11-06 19:30:20 +0300

    Update UserInformationFragment.java

--- app/src/main/java/org/ole/planet/myplanet/ui/exam/UserInformationFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/exam/UserInformationFragment.java
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
