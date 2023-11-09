111b0b7ecf8dd4f50eef68c9b89f95f4c2927059
Gideon Okuro <gideonollonde@gmail.com>
2023-11-09 20:50:28 +0300

    Update MyHealthFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/myhealth/MyHealthFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/myhealth/MyHealthFragment.kt
@@ -9,12 +9,9 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatSpinner
 import androidx.fragment.app.Fragment
