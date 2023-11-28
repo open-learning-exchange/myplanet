6e2887c81b121196fa424ae887798ac850c2b998
Gideon Okuro <gideonollonde@gmail.com>
2023-11-28 17:28:15 +0300

    remove commented code

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/UserListAdapter.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/UserListAdapter.java
@@ -1,8 +1,6 @@
 package org.ole.planet.myplanet.ui.sync;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
