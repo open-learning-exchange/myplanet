8a78150609584fb9fa2e3f5ba20ad5fdd3dbfbac
Gideon Okuro <gideonollonde@gmail.com>
2023-11-29 14:12:29 +0300

    convert DocumentResponse to kotlin

--- app/src/main/java/org/ole/planet/myplanet/service/SyncManager.java
+++ app/src/main/java/org/ole/planet/myplanet/service/SyncManager.java
@@ -191,8 +191,8 @@ public class SyncManager {
         mRealm.executeTransaction(realm -> {
             try {
                 DocumentResponse res = apiInterface.getDocuments(Utilities.getHeader(), Utilities.getUrl() + "/shelf/_all_docs").execute().body();
-                for (int i = 0; i < res.getRows().size(); i++) {
-                    shelfDoc = res.getRows().get(i);
+                for (int i = 0; i < res.rows.size(); i++) {
+                    shelfDoc = res.rows.get(i);
                     populateShelfItems(apiInterface, realm);
                 }
             } catch (IOException e) {
