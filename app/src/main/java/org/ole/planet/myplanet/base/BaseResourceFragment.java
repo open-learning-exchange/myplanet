fb5c2eb04b213bcaf43716b88dd7a564c464a864
Gideon Okuro <gideonollonde@gmail.com>
2023-12-06 20:46:40 +0300

    actions: refactor `RealmMyCourse` model to kotlin (fixes #2797) (#2802)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/base/BaseResourceFragment.java
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseResourceFragment.java
@@ -264,9 +264,9 @@ public abstract class BaseResourceFragment extends Fragment {
             RealmRemovedLog.onRemove(mRealm, "resources", model.getId(), ((RealmMyLibrary) object).resourceId);
             Utilities.toast(getActivity(), getString(R.string.removed_from_mylibrary));
         } else {
-            RealmMyCourse myObject = RealmMyCourse.getMyCourse(mRealm, ((RealmMyCourse) object).getCourseId());
+            RealmMyCourse myObject = RealmMyCourse.getMyCourse(mRealm, ((RealmMyCourse) object).courseId);
             myObject.removeUserId(model.getId());
-            RealmRemovedLog.onRemove(mRealm, "courses", model.getId(), ((RealmMyCourse) object).getCourseId());
+            RealmRemovedLog.onRemove(mRealm, "courses", model.getId(), ((RealmMyCourse) object).courseId);
             Utilities.toast(getActivity(), getString(R.string.removed_from_mycourse));
         }
     }
