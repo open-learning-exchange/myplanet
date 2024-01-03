aa65be1c0bd60f304a6f958c29f2d722744f5a16
Gideon Okuro <gideonollonde@gmail.com>
2024-01-03 15:04:58 +0300

    ump com.google.android.exoplayer:exoplayer to 2.12.1

--- app/src/main/java/org/ole/planet/myplanet/ui/viewer/VideoPlayerActivity.java
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/VideoPlayerActivity.java
@@ -11,16 +11,13 @@ import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DataSpec;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.FileDataSource;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
@@ -77,17 +74,14 @@ public class VideoPlayerActivity extends AppCompatActivity implements AuthSessio
     }
 
     public void streamVideoFromUrl(String videoUrl, String auth) {
-
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        TrackSelector trackSelectorDef = new DefaultTrackSelector();
+        exoPlayer= ExoPlayerFactory.newSimpleInstance(this, trackSelectorDef);
 
         Uri videoUri = Uri.parse(videoUrl);
 
         HttpDataSource.Factory defaultHttpDataSourceFactory = new DefaultHttpDataSourceFactory("ExoPlayer", null);
         defaultHttpDataSourceFactory.setDefaultRequestProperty("Cookie", auth);
-        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-        MediaSource mediaSource = new ExtractorMediaSource(videoUri, defaultHttpDataSourceFactory, extractorsFactory, null, null);
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(videoUri);
 
         activityExoPlayerVideoBinding.exoPlayerSimple.setPlayer(exoPlayer);
         exoPlayer.prepare(mediaSource);
