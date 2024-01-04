b17c6b89fdb7a83cdddbda73c579fb9752a27c56
Gideon Okuro <gideonollonde@gmail.com>
2024-01-04 13:50:49 +0300

    migrate BaseMemberFragment to kotlin

---
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseMemberFragment.kt
@@ -0,0 +1,32 @@
+package org.ole.planet.myplanet.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.ole.planet.myplanet.R
+import org.ole.planet.myplanet.model.RealmUserModel
+import org.ole.planet.myplanet.ui.team.BaseTeamFragment
+
+abstract class BaseMemberFragment : BaseTeamFragment() {
+    abstract val list: List<RealmUserModel?>
+    abstract val adapter: RecyclerView.Adapter<*>?
+    abstract val layoutManager: RecyclerView.LayoutManager?
+    var rvMember: RecyclerView? = null
+    var tvNodata: TextView? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_members, container, false)
+        rvMember = v.findViewById(R.id.rv_member)
+        tvNodata = v.findViewById(R.id.tv_nodata)
+        return v
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        rvMember!!.layoutManager = layoutManager
+        rvMember!!.adapter = adapter
+        showNoData(tvNodata, list.size)
+    }
+}
