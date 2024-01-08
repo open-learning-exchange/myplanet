82608ff2605b4ba74b9b88b2ce69e4328d8ce4b0
Gideon Okuro <gideonollonde@gmail.com>
2024-01-08 23:35:10 +0300

    actions: refactor `DownloadUtils` utilities to kotlin (fixes #2935) (#2937)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/utilities/DownloadUtils.kt
+++ app/src/main/java/org/ole/planet/myplanet/utilities/DownloadUtils.kt
@@ -3,24 +3,22 @@ package org.ole.planet.myplanet.utilities
 import android.content.SharedPreferences
 import org.ole.planet.myplanet.model.RealmMyLibrary
 
-class DownloadUtils {
-    companion object {
-        @JvmStatic
-        fun downloadAllFiles(dbMyLibrary: List<RealmMyLibrary>, settings: SharedPreferences): ArrayList<String> {
-            val urls = ArrayList<String>()
-            for (i in dbMyLibrary.indices) {
-                urls.add(Utilities.getUrl(dbMyLibrary[i], settings))
-            }
-            return urls
+object DownloadUtils {
+    @JvmStatic
+    fun downloadAllFiles(db_myLibrary: List<RealmMyLibrary?>, settings: SharedPreferences?): ArrayList<String> {
+        val urls = ArrayList<String>()
+        for (i in db_myLibrary.indices) {
+            urls.add(Utilities.getUrl(db_myLibrary[i], settings))
         }
+        return urls
+    }
 
-        @JvmStatic
-        fun downloadFiles(dbMyLibrary: List<RealmMyLibrary>, selectedItems: ArrayList<Int>, settings: SharedPreferences): ArrayList<String> {
-            val urls = ArrayList<String>()
-            for (i in selectedItems) {
-                urls.add(Utilities.getUrl(dbMyLibrary[i], settings))
-            }
-            return urls
+    @JvmStatic
+    fun downloadFiles(db_myLibrary: List<RealmMyLibrary?>, selectedItems: ArrayList<Int?>, settings: SharedPreferences?): ArrayList<String> {
+        val urls = ArrayList<String>()
+        for (i in selectedItems.indices) {
+            urls.add(Utilities.getUrl(db_myLibrary[selectedItems[i]!!], settings))
         }
+        return urls
     }
 }
