29ab7766e02d78b0eaf724a913784255b69ee74e
Gideon Okuro <gideonollonde@gmail.com>
2024-01-11 17:17:21 +0300

    migrate MarkdownViewerActivity to kotlin

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/MarkdownViewerActivity.kt
@@ -0,0 +1,57 @@
+package org.ole.planet.myplanet.ui.viewer
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.ole.planet.myplanet.R
+import org.ole.planet.myplanet.databinding.ActivityMarkdownViewerBinding
+import org.ole.planet.myplanet.utilities.Markdown.setMarkdownText
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
+
+class MarkdownViewerActivity : AppCompatActivity() {
+    private var activityMarkdownViewerBinding: ActivityMarkdownViewerBinding? = null
+    private var fileName: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMarkdownViewerBinding = ActivityMarkdownViewerBinding.inflate(layoutInflater)
+        setContentView(activityMarkdownViewerBinding!!.root)
+        renderMarkdownFile()
+    }
+
+    private fun renderMarkdownFile() {
+        val markdownOpenIntent = intent
+        fileName = markdownOpenIntent.getStringExtra("TOUCHED_FILE")
+        if (!fileName.isNullOrEmpty()) {
+            activityMarkdownViewerBinding!!.markdownFileName.text = fileName
+            activityMarkdownViewerBinding!!.markdownFileName.visibility = View.VISIBLE
+        }
+        try {
+            val basePath = getExternalFilesDir(null)
+            val markdownFile = File(basePath, "ole/$fileName")
+            if (markdownFile.exists()) {
+                val markdownContent = readMarkdownFile(markdownFile)
+                setMarkdownText(activityMarkdownViewerBinding!!.markdownView, markdownContent)
+            } else {
+                Toast.makeText(this, getString(R.string.unable_to_load) + fileName, Toast.LENGTH_LONG).show()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun readMarkdownFile(file: File): String {
+        val reader = BufferedReader(FileReader(file))
+        val content = StringBuilder()
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            content.append(line).append("\n")
+        }
+        reader.close()
+        return content.toString()
+    }
+}
