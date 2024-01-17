f9b18100140aeacababd2a236de132f5081334a6
Gideon Okuro <gideonollonde@gmail.com>
2024-01-17 15:00:21 +0300

    migrate teamDiscussion to kotlin

---
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamDiscussion/DiscussionListFragment.kt
@@ -0,0 +1,120 @@
+package org.ole.planet.myplanet.ui.team.teamDiscussion
+
+import android.content.DialogInterface
+import android.content.res.Configuration
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import io.realm.Realm
+import io.realm.Sort
+import org.ole.planet.myplanet.R
+import org.ole.planet.myplanet.databinding.AlertInputBinding
+import org.ole.planet.myplanet.databinding.FragmentDiscussionListBinding
+import org.ole.planet.myplanet.model.RealmNews
+import org.ole.planet.myplanet.model.RealmNews.Companion.createNews
+import org.ole.planet.myplanet.model.RealmTeamNotification
+import org.ole.planet.myplanet.ui.news.AdapterNews
+import org.ole.planet.myplanet.ui.team.BaseTeamFragment
+import org.ole.planet.myplanet.utilities.Constants
+import org.ole.planet.myplanet.utilities.Constants.showBetaFeature
+import org.ole.planet.myplanet.utilities.FileUtils.openOleFolder
+import org.ole.planet.myplanet.utilities.Utilities
+import java.util.UUID
+
+class DiscussionListFragment : BaseTeamFragment() {
+    private lateinit var fragmentDiscussionListBinding: FragmentDiscussionListBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        fragmentDiscussionListBinding = FragmentDiscussionListBinding.inflate(inflater, container, false)
+        fragmentDiscussionListBinding.addMessage.setOnClickListener { showAddMessage() }
+        return fragmentDiscussionListBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val realmNewsList = news
+        val count = realmNewsList.size
+        mRealm.executeTransactionAsync { realm: Realm ->
+            var notification = realm.where(RealmTeamNotification::class.java).equalTo("type", "chat").equalTo("parentId", teamId).findFirst()
+            if (notification == null) {
+                notification = realm.createObject(RealmTeamNotification::class.java, UUID.randomUUID().toString())
+                notification.parentId = teamId
+                notification.type = "chat"
+            }
+            notification!!.lastCount = count
+        }
+        changeLayoutManager(resources.configuration.orientation, fragmentDiscussionListBinding.rvDiscussion)
+        showRecyclerView(realmNewsList)
+    }
+
+    private val news: List<RealmNews>
+        get() {
+            val realmNewsList: List<RealmNews> = mRealm.where(RealmNews::class.java).isEmpty("replyTo").sort("time", Sort.DESCENDING).findAll()
+            val list: MutableList<RealmNews> = ArrayList()
+            for (news in realmNewsList) {
+                if (!TextUtils.isEmpty(news.viewableBy) && news.viewableBy.equals("teams", ignoreCase = true) && news.viewableId.equals(team._id, ignoreCase = true)) {
+                    list.add(news)
+                } else if (!TextUtils.isEmpty(news.viewIn)) {
+                    val ar = Gson().fromJson(news.viewIn, JsonArray::class.java)
+                    for (e in ar) {
+                        val ob = e.asJsonObject
+                        if (ob["_id"].asString.equals(team._id, ignoreCase = true)) {
+                            list.add(news)
+                        }
+                    }
+                }
+            }
+            return list
+        }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        changeLayoutManager(newConfig.orientation, fragmentDiscussionListBinding.rvDiscussion)
+    }
+
+    private fun showRecyclerView(realmNewsList: List<RealmNews>) {
+        val adapterNews = AdapterNews(activity, realmNewsList, user, null, true)
+        adapterNews.setmRealm(mRealm)
+        adapterNews.setListener(this)
+        fragmentDiscussionListBinding.rvDiscussion.adapter = adapterNews
+        showNoData(fragmentDiscussionListBinding.tvNodata, adapterNews.itemCount)
+    }
+
+    private fun showAddMessage() {
+        val binding = AlertInputBinding.inflate(layoutInflater)
+        val layout = binding.tlInput
+        binding.addNewsImage.setOnClickListener { openOleFolder(this, 100) }
+        binding.llImage.visibility = if (showBetaFeature(Constants.KEY_NEWSADDIMAGE, activity)) View.VISIBLE else View.GONE
+        layout.hint = getString(R.string.enter_message)
+        AlertDialog.Builder(requireActivity())
+            .setView(binding.root)
+            .setTitle(getString(R.string.add_message))
+            .setPositiveButton(getString(R.string.save)) { _: DialogInterface?, _: Int ->
+                val msg = layout.editText!!.text.toString().trim { it <= ' ' }
+                if (msg.isEmpty()) {
+                    Utilities.toast(activity, getString(R.string.message_is_required))
+                    return@setPositiveButton
+                }
+                val map = HashMap<String?, String>()
+                map["viewInId"] = teamId
+                map["viewInSection"] = "teams"
+                map["message"] = msg
+                map["messageType"] = team.teamType!!
+                map["messagePlanetCode"] = team.teamPlanetCode!!
+                createNews(map, mRealm, user, imageList)
+                Utilities.log("discussion created")
+                fragmentDiscussionListBinding.rvDiscussion.adapter!!.notifyDataSetChanged()
+                setData(news)
+            }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .show()
+    }
+
+    override fun setData(list: List<RealmNews>) {
+        showRecyclerView(list)
+    }
+}
