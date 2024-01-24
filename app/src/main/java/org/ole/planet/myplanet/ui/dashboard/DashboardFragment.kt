ce05f2a1e36f58394c5730c94187ca1633f3505e
Gideon Okuro <gideonollonde@gmail.com>
2024-01-24 14:06:04 +0300

    actions: initialize realm property (fixes #3051) (#3052)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardFragment.kt
@@ -20,26 +20,26 @@ import org.ole.planet.myplanet.utilities.TimeUtils.currentDate
 
 class DashboardFragment : BaseDashboardFragment() {
     private lateinit var fragmentHomeBinding: FragmentHomeBinding
-    var mRealm: Realm? = null
+    private lateinit var dRealm: Realm
     private lateinit var databaseService: DatabaseService
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
         val view: View = fragmentHomeBinding.root
         fragmentHomeBinding.cardProfile.tvSurveys.setOnClickListener {
-            homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("survey"))
+            homeItemClickListener?.openCallFragment(MySubmissionFragment.newInstance("survey"))
         }
         fragmentHomeBinding.cardProfile.tvNews.setOnClickListener {
-            homeItemClickListener.openCallFragment(NewsFragment())
+            homeItemClickListener?.openCallFragment(NewsFragment())
         }
         fragmentHomeBinding.cardProfile.tvSubmission.setOnClickListener {
-            homeItemClickListener.openCallFragment(MySubmissionFragment.newInstance("exam"))
+            homeItemClickListener?.openCallFragment(MySubmissionFragment.newInstance("exam"))
         }
         fragmentHomeBinding.cardProfile.tvAchievement.visibility = if (showBetaFeature(Constants.KEY_ACHIEVEMENT, activity)) View.VISIBLE else View.GONE
         fragmentHomeBinding.cardProfile.tvAchievement.setOnClickListener {
-            homeItemClickListener.openCallFragment(AchievementFragment())
+            homeItemClickListener?.openCallFragment(AchievementFragment())
         }
         databaseService = DatabaseService(requireActivity())
-        mRealm = databaseService.realmInstance
+        dRealm = databaseService.realmInstance
         onLoaded(view)
         initView(view)
         (activity as AppCompatActivity?)!!.supportActionBar!!.subtitle = currentDate()
@@ -48,7 +48,7 @@ class DashboardFragment : BaseDashboardFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val noOfSurvey = getNoOfSurveySubmissionByUser(settings.getString("userId", "--"), mRealm)
+        val noOfSurvey = getNoOfSurveySubmissionByUser(settings?.getString("userId", "--"), dRealm)
         fragmentHomeBinding.cardProfile.imgSurveyWarn.visibility = if (noOfSurvey == 0) View.VISIBLE else View.GONE
         fragmentHomeBinding.addResource.setOnClickListener {
             AddResourceFragment().show(childFragmentManager, getString(R.string.add_res))
