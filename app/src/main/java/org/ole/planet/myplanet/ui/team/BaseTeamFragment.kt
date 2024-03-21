aed6d4cc557986320fcce8457984fb7aa8538094
Gideon Okuro <gideonollonde@gmail.com>
2024-03-21 15:37:32 +0300

    Update BaseTeamFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/team/BaseTeamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/BaseTeamFragment.kt
@@ -3,6 +3,7 @@ package org.ole.planet.myplanet.ui.team
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import org.ole.planet.myplanet.base.BaseNewsFragment
 import org.ole.planet.myplanet.datamanager.DatabaseService
 import org.ole.planet.myplanet.model.RealmMyTeam
@@ -19,18 +20,36 @@ abstract class BaseTeamFragment : BaseNewsFragment() {
     lateinit var team: RealmMyTeam
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (requireParentFragment().arguments != null) {
-            teamId = requireParentFragment().requireArguments().getString("id", "")
-        } else if (arguments != null) {
-            teamId = requireArguments().getString("id", "")
-        }
+        teamId = parentFragment?.arguments?.getString("id", "") ?: arguments?.getString("id", "") ?: ""
         dbService = DatabaseService(requireActivity())
         mRealm = dbService.realmInstance
         user = profileDbHandler.userModel?.let { mRealm.copyFromRealm(it) }
         Utilities.log("Team id $teamId")
-        team = mRealm.where(RealmMyTeam::class.java).equalTo("_id", teamId).findFirst() ?: throw IllegalArgumentException("Team not found for ID: $teamId")
+        val allTeams = mRealm.where(RealmMyTeam::class.java).findAll()
+        logLargeString("TEAM", "All teams $allTeams")
+
+        team = try {
+            mRealm.where(RealmMyTeam::class.java).equalTo("_id", teamId).findFirst() ?: throw IllegalArgumentException("Team not found for ID: $teamId")
+        } catch (e: IllegalArgumentException) {
+            try {
+                mRealm.where(RealmMyTeam::class.java).equalTo("teamId", teamId).findFirst() ?: throw IllegalArgumentException("Team not found for ID: $teamId")
+            } catch (e: IllegalArgumentException) {
+                throw e
+            }
+        }
+
         settings = requireActivity().getSharedPreferences(SyncActivity.PREFS_NAME, Context.MODE_PRIVATE)
     }
+
+    fun logLargeString(tag: String, content: String) {
+        if (content.length > 3000) {
+            Log.d(tag, content.substring(0, 3000))
+            logLargeString(tag, content.substring(3000))
+        } else {
+            Log.d(tag, content)
+        }
+    }
+
 
     override fun setData(list: List<RealmNews?>?) {}
 
