03fc14ae05815bd63b738e6cd21162a0f8deafc9
Gideon Okuro <gideonollonde@gmail.com>
2024-04-19 20:02:59 +0300

    Update BaseRecyclerFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/base/BaseRecyclerFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseRecyclerFragment.kt
@@ -4,7 +4,6 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
