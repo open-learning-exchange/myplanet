ea188b7a9419b81ab8edaa31a066985d0e4f8480
Gideon Okuro <gideonollonde@gmail.com>
2024-04-22 19:05:24 +0300

    initialize and default configuration in init and add realm log

--- app/src/main/java/org/ole/planet/myplanet/datamanager/DatabaseService.kt
+++ app/src/main/java/org/ole/planet/myplanet/datamanager/DatabaseService.kt
@@ -1,19 +1,24 @@
 package org.ole.planet.myplanet.datamanager
 
 import android.content.Context
-import io.realm.Realm
-import io.realm.RealmConfiguration
+import io.realm.*
+import io.realm.log.*
 
 class DatabaseService(context: Context) {
     init {
         Realm.init(context)
+        RealmLog.setLevel(LogLevel.DEBUG)
+        val config = RealmConfiguration.Builder()
+            .name(Realm.DEFAULT_REALM_NAME)
+            .deleteRealmIfMigrationNeeded()
+            .schemaVersion(4)
+            .allowWritesOnUiThread(true)
+            .build()
+        Realm.setDefaultConfiguration(config)
     }
 
     val realmInstance: Realm
         get() {
-            val config = RealmConfiguration.Builder().name(Realm.DEFAULT_REALM_NAME)
-                .deleteRealmIfMigrationNeeded().schemaVersion(4).allowWritesOnUiThread(true).build()
-            Realm.setDefaultConfiguration(config)
-            return Realm.getInstance(config)
+            return Realm.getDefaultInstance()
         }
 }
