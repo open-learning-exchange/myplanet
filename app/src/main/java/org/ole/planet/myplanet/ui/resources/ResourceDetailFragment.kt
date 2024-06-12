5c9eb83df9d950ff973c3925333f1d7a0d6ebf99
dogi <dogi@users.noreply.github.com>
2024-06-12 12:38:35 -0400

    Update ResourceDetailFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/resources/ResourceDetailFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/resources/ResourceDetailFragment.kt
@@ -2,7 +2,6 @@ package org.ole.planet.myplanet.ui.resources
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
