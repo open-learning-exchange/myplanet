b69c2390af10f9817dffc23879016f42c4e284af
Gideon Okuro <gideonollonde@gmail.com>
2024-06-19 18:43:19 +0300

    trim -lite from curentVersion

--- app/src/main/java/org/ole/planet/myplanet/datamanager/Service.kt
+++ app/src/main/java/org/ole/planet/myplanet/datamanager/Service.kt
@@ -295,9 +295,10 @@ class Service(private val context: Context) {
             override fun onResponse(call: Call<JsonObject?>, response: Response<JsonObject?>) {
                 if (response.isSuccessful) {
                     response.body()?.let { jsonObject ->
-                        val currentVersion = "${context.resources.getText(R.string.app_version)}"
+                        val rawCurrentVersion = "${context.resources.getText(R.string.app_version)}"
+                        val cleanCurrentVersion = rawCurrentVersion.replace("-lite", "")
                         val minApkVersion = jsonObject.get("minapk").asString
-                        if (isVersionAllowed(currentVersion, minApkVersion)) {
+                        if (isVersionAllowed(cleanCurrentVersion, minApkVersion)) {
                             customProgressDialog.setText(context.getString(R.string.checking_server))
                             val uri = Uri.parse(url)
                             val couchdbURL: String
