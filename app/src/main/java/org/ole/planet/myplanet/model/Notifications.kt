28b4ce5accdb8e38ef9645a969eac6642ba35587
Rayyan <52076121+rlam20@users.noreply.github.com>
2024-07-01 12:58:30 -0400

    progress on read history

--- app/src/main/java/org/ole/planet/myplanet/model/Notifications.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/Notifications.kt
@@ -1,11 +1,17 @@
 package org.ole.planet.myplanet.model
 
-data class Notifications(var icon: Int, var text: String) {
+data class Notifications(var id: Int, var icon: Int, var text: String) {
 
-    constructor(icon: Int, text: String, timestamp: String, isRead: Boolean) : this(icon, text) {
+    constructor(id: Int, icon: Int, text: String, timestamp: String, isRead: Boolean) : this(id, icon, text) {
         this.timestamp = timestamp
         this.isRead = isRead
     }
+
+    constructor(icon: Int, text: String) : this(0, icon, text){
+
+    }
+
+    constructor(id: String?, icon: Int, text: String) : this(0,0,text)
 
     var timestamp: String = ""
     var isRead: Boolean = false
--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/notification/SeeAllNotificationsFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/notification/SeeAllNotificationsFragment.kt
@@ -1,6 +1,8 @@
 package org.ole.planet.myplanet.ui.dashboard.notification
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -39,12 +41,19 @@ class SeeAllNotificationsFragment : Fragment() {
             .equalTo("assignee", model.id)
             .findAll()
 
+        val sharedPrefs = requireContext().getSharedPreferences("notifications_prefs", Context.MODE_PRIVATE)
+
         val notifications = tasks.map {
             Notifications(
+                id = it.id,
                 icon = R.drawable.task_pending,
                 text = "You were assigned a new task: " + "${it.title}"
             )
+        }.filterNot {
+            sharedPrefs.getBoolean("notification_${it.id}", false)
         }.toMutableList()
+
+        Log.d("Notifications", "Loaded notifications: $notifications")
 
         val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_notifications)
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -56,7 +65,7 @@ class SeeAllNotificationsFragment : Fragment() {
             override fun downloadDictionary() {}
             override fun showTaskListDialog() {}
             override fun syncKeyId() {}
-        }, showMarkAsReadButton = true, false)
+        }, showMarkAsReadButton = true, showImages = false)
 
         recyclerView.adapter = notificationsAdapter
 
