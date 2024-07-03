2f4c7dc963c6d29f77b7c70df6a390a28dd713e7
Elijah <ewhang3@gmail.com>
2024-07-03 13:03:23 -0500

    actions: less is more (fixes #3775)

--- app/src/main/java/org/ole/planet/myplanet/utilities/VersionUtils.kt
+++ app/src/main/java/org/ole/planet/myplanet/utilities/VersionUtils.kt
@@ -2,6 +2,7 @@ package org.ole.planet.myplanet.utilities
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.provider.Settings
 
 object VersionUtils {
@@ -9,7 +10,12 @@ object VersionUtils {
     fun getVersionCode(context: Context): Int {
         try {
             val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-            return pInfo.versionCode
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                return (pInfo.longVersionCode and 0xFFFFFFFF).toInt()
+            } else {
+                @Suppress("DEPRECATION")
+                pInfo.versionCode
+            }
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
