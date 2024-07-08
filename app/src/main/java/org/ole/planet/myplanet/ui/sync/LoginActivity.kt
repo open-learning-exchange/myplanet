7dc14a428c9b0a0eadddaa7fe86a6d61e799fb3e
Gideon Okuro <gideonollonde@gmail.com>
2024-07-08 20:11:11 +0300

    Update LoginActivity.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.drawable.AnimationDrawable
 import android.os.*
 import android.os.Build.VERSION_CODES.TIRAMISU
 import android.text.*
-import android.util.Log
 import android.view.*
 import android.view.inputmethod.EditorInfo
 import android.widget.*
