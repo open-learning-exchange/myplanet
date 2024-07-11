06df76c021931a084efe48b6fefbf94d4feaa2ce
Gideon Okuro <gideonollonde@gmail.com>
2024-07-11 16:37:12 +0300

    clear sharedPreference data

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
@@ -752,6 +752,9 @@ abstract class SyncActivity : ProcessUserDataActivity(), SyncListener, CheckVers
                 editor.putBoolean(key, value)
             }
             editor.commit()
+
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            preferences.edit().clear().commit()
         }
 
         fun restartApp() {
