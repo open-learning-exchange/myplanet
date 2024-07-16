a2fb36678cab41e95812efd53cbfae0018694a46
rlam20 <52076121+rlam20@users.noreply.github.com>
2024-07-16 15:00:18 -0400

    teams: better permissions (fixes #3813) (#3867)
    Co-authored-by: Gideon Okuro <gideonollonde@gmail.com>
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/MembersFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/MembersFragment.kt
@@ -1,18 +1,33 @@
 package org.ole.planet.myplanet.ui.team.teamMember
 
+import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.ole.planet.myplanet.base.BaseMemberFragment
+import org.ole.planet.myplanet.model.RealmMyTeam
 import org.ole.planet.myplanet.model.RealmMyTeam.Companion.getRequestedMember
 import org.ole.planet.myplanet.model.RealmUserModel
+import org.ole.planet.myplanet.service.UserProfileDbHandler
 
 class MembersFragment : BaseMemberFragment() {
+
+    private lateinit var currentUser: RealmUserModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        currentUser = UserProfileDbHandler(context).userModel!!
+    }
+
     override val list: List<RealmUserModel>
+        @RequiresApi(Build.VERSION_CODES.O)
         get() = getRequestedMember(teamId, mRealm)
 
     override val adapter: RecyclerView.Adapter<*>
-        get() = AdapterMemberRequest(requireActivity(), list.toMutableList(), mRealm).apply { setTeamId(teamId) }
+        @RequiresApi(Build.VERSION_CODES.O)
+        get() = AdapterMemberRequest(requireActivity(), list.toMutableList(), mRealm, isTeamLeader()).apply { setTeamId(teamId) }
 
     override val layoutManager: RecyclerView.LayoutManager
         get() {
@@ -24,4 +39,18 @@ class MembersFragment : BaseMemberFragment() {
             }
             return GridLayoutManager(activity, columns)
         }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun isTeamLeader(): Boolean {
+        val currentUserId = getCurrentUserId()
+        val team = mRealm.where(RealmMyTeam::class.java)
+            .equalTo("teamId", teamId)
+            .equalTo("userId", currentUserId)
+            .findFirst()
+        return team?.isLeader == true
+    }
+
+    private fun getCurrentUserId(): String? {
+        return currentUser.id
+    }
 }
