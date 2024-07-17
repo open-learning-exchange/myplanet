de3242a80c1e9600aed9d6ae6da1f55d288f907c
Gideon Okuro <gideonollonde@gmail.com>
2024-07-17 19:53:57 +0300

    Update BaseExamFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/exam/BaseExamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/exam/BaseExamFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.CompoundButton
 import android.widget.EditText
