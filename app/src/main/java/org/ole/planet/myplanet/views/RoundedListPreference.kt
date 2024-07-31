bd1bddbbbfd3c237ed52106144b234ce7e55164a
Rayyan <52076121+rlam20@users.noreply.github.com>
2024-07-31 12:18:25 -0400

    fix settings (fixes #3969)

--- app/src/main/java/org/ole/planet/myplanet/views/RoundedListPreference.kt
+++ app/src/main/java/org/ole/planet/myplanet/views/RoundedListPreference.kt
@@ -5,11 +5,12 @@ import android.content.Context
 import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.util.AttributeSet
 import android.view.ContextThemeWrapper
 import androidx.preference.ListPreference
 import org.ole.planet.myplanet.R
 
-class RoundedListPreference(context: Context) : ListPreference(context) {
+class RoundedListPreference(context: Context, attrs: AttributeSet) : ListPreference(context, attrs) {
 
     override fun onClick() {
         val currentIndex = findIndexOfValue(value)
@@ -23,8 +24,6 @@ class RoundedListPreference(context: Context) : ListPreference(context) {
             }
             .setNegativeButton(R.string.cancel) { dialog: DialogInterface, _: Int -> dialog.dismiss() }.create()
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         dialog.show()
     }
-
 }
