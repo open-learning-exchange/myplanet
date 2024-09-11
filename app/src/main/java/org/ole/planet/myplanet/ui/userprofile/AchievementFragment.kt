9e2d0d246218d7c8f683b46638a23c3c3be71c22
Gideon Okuro <gideonollonde@gmail.com>
2024-09-11 20:48:04 +0300

    Update AchievementFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/userprofile/AchievementFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/userprofile/AchievementFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.JsonArray
