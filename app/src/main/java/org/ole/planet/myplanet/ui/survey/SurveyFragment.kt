895aebab5b9cd49a91f50070156c1c4869353f26
Gideon Okuro <gideonollonde@gmail.com>
2024-09-26 20:13:25 +0300

    reduce  unnecessary adapter recreation

--- app/src/main/java/org/ole/planet/myplanet/ui/survey/SurveyFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/survey/SurveyFragment.kt
@@ -16,36 +16,50 @@ class SurveyFragment : BaseRecyclerFragment<RealmStepExam?>() {
     private lateinit var addNewSurvey: FloatingActionButton
     private lateinit var spn: CustomSpinner
     private var isTitleAscending = true
+    private lateinit var adapter: AdapterSurvey
+
     override fun getLayout(): Int {
         return R.layout.fragment_survey
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = AdapterSurvey(requireActivity(), mRealm, model?.id ?: "")
+    }
+
     override fun getAdapter(): RecyclerView.Adapter<*> {
-        return model?.id?.let {
-            AdapterSurvey(requireActivity(), safeCastList(getList(RealmStepExam::class.java, "name", Sort.ASCENDING), RealmStepExam::class.java), mRealm, it)
-        }!!
+        return adapter
     }
 
     @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        spn = requireView().findViewById(R.id.spn_sort)
-        addNewSurvey = requireView().findViewById(R.id.fab_add_new_survey)
-        addNewSurvey.setOnClickListener { }
-        if (getAdapter().itemCount == 0) {
-            spn.visibility = View.GONE
-        }
-        showNoData(tvMessage, getAdapter().itemCount, "survey")
+
+        initializeViews(view)
+        setupRecyclerView()
+        setupListeners()
+        updateAdapterData()
+    }
+
+    private fun initializeViews(view: View) {
+        spn = view.findViewById(R.id.spn_sort)
+        addNewSurvey = view.findViewById(R.id.fab_add_new_survey)
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupListeners() {
+        addNewSurvey.setOnClickListener {}
+
         spn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, i: Int, l: Long) {
-                if (view != null) {
-                    if (i == 0) {
-                        recyclerView.adapter = activity?.let { act -> model?.id?.let { id -> AdapterSurvey(act, safeCastList(getList(RealmStepExam::class.java, "createdDate", Sort.ASCENDING), RealmStepExam::class.java), mRealm, id) } }
-                    } else if (i == 1){
-                        recyclerView.adapter = activity?.let { act -> model?.id?.let { id -> AdapterSurvey(act, safeCastList(getList(RealmStepExam::class.java, "createdDate", Sort.DESCENDING), RealmStepExam::class.java), mRealm, id) } }
-                    } else {
-                        toggleTitleSortOrder()
-                    }
+                when (i) {
+                    0 -> updateAdapterData(Sort.ASCENDING, "createdDate")
+                    1 -> updateAdapterData(Sort.DESCENDING, "createdDate")
+                    2 -> toggleTitleSortOrder()
                 }
             }
 
@@ -63,13 +77,36 @@ class SurveyFragment : BaseRecyclerFragment<RealmStepExam?>() {
         })
     }
 
-    fun <T> safeCastList(items: List<Any?>, clazz: Class<T>): List<T> {
-        return items.mapNotNull { it?.takeIf(clazz::isInstance)?.let(clazz::cast) }
+    private fun updateAdapterData(sort: Sort = Sort.ASCENDING, field: String = "name") {
+        val newList = getList(RealmStepExam::class.java, field, sort)
+        adapter.updateData(safeCastList(newList, RealmStepExam::class.java))
+        updateUIState()
+    }
+
+    private fun updateUIState() {
+        val itemCount = adapter.itemCount
+        spn.visibility = if (itemCount == 0) View.GONE else View.VISIBLE
+        showNoData(tvMessage, itemCount, "survey")
     }
 
     fun toggleTitleSortOrder() {
         isTitleAscending = !isTitleAscending
         val sort = if (isTitleAscending) Sort.ASCENDING else Sort.DESCENDING
-        recyclerView.adapter = AdapterSurvey(requireActivity(), safeCastList(getList(RealmStepExam::class.java, "name", sort), RealmStepExam::class.java), mRealm, model?.id!!)
+        updateAdapterData(sort, "name")
+    }
+
+    private fun <T> safeCastList(items: List<Any?>, clazz: Class<T>): List<T> {
+        return items.mapNotNull { it?.takeIf(clazz::isInstance)?.let(clazz::cast) }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRealm.close()
+    }
+
+    companion object {
+        fun newInstance(): SurveyFragment {
+            return SurveyFragment()
+        }
     }
 }
