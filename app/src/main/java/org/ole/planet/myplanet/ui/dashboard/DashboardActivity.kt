4e5e0e1ca71088ae5423458d84d25a830d5bdbe0
dogi <stefan@unterhauser.name>
2024-10-08 07:27:27 -0400

    less whitespaces is more

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardActivity.kt
@@ -253,18 +253,18 @@ class DashboardActivity : DashboardElementActivity(), OnHomeItemClickListener, N
     private fun challengeDialog(remainingDays: Int) {
         val markdownContent = """
             ## myPlanet issues challenge
-            
+
             ![issues challenge](file:///android_asset/images/github-filter-issues.png)
-    
+
             get ready for virtual intern github issues challenge!
-    
+
             **duration:** $remainingDays days remaining
-    
+
             ## steps to participate:
             - Find an issue on myPlanet
             - Take a screenshot/record a video
             - create an issue on our github repository using link below
-            
+
             [new myPlanet issue](https://github.com/open-learning-exchange/myplanet/issues/new)
             """.trimIndent()
 
