aecaa3217e4b69606f70107c56989902fb247e32
Gideon Okuro <gideonollonde@gmail.com>
2024-10-22 18:28:18 +0300

    Update TakeCourseFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/courses/TakeCourseFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/courses/TakeCourseFragment.kt
@@ -2,7 +2,6 @@ package org.ole.planet.myplanet.ui.courses
 
 import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
