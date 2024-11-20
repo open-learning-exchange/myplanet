a00b70ba4a7e1d5493d1948064b5adc4ced1a770
Gideon Okuro <gideonollonde@gmail.com>
2024-11-20 15:08:58 +0300

    remove unused import

--- app/src/main/java/org/ole/planet/myplanet/ui/courses/TakeCourseFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/courses/TakeCourseFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
