bd69e86d10c48d883323ea8545d1f84c822cc1c8
Gideon Okuro <gideonollonde@gmail.com>
2024-11-25 14:30:56 +0300

    update file class name

--- app/src/main/java/org/ole/planet/myplanet/model/RealmDictionary.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/RealmDictionary.kt
@@ -3,7 +3,7 @@ package org.ole.planet.myplanet.model
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 
-class RealmCourseStep : RealmObject {
+class RealmDictionary : RealmObject {
     @PrimaryKey
     var id: String? = null
     var word: String? = null
