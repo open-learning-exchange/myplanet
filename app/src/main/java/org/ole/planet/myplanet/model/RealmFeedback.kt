dcee5de56e5f310210aed5d76a789a7ed27e3de9
Gideon Okuro <gideonollonde@gmail.com>
2024-12-06 17:22:46 +0300

    migrate realm feedback

--- app/src/main/java/org/ole/planet/myplanet/model/RealmFeedback.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/RealmFeedback.kt
@@ -1,23 +1,16 @@
 package org.ole.planet.myplanet.model
 
-import android.text.TextUtils
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import com.google.gson.stream.JsonReader
-import com.opencsv.CSVWriter
-import io.realm.Realm
-import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
-import org.ole.planet.myplanet.MainApplication.Companion.context
-import org.ole.planet.myplanet.utilities.JsonUtils
+import com.google.gson.*
+import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
-import java.io.StringReader
+import kotlin.collections.MutableList
 
-open class RealmFeedback : RealmObject() {
+class RealmFeedback : RealmObject {
     @PrimaryKey
     var id: String? = null
     var _id: String? = null
@@ -31,139 +24,132 @@ open class RealmFeedback : RealmObject() {
     var url: String? = null
     var isUploaded = false
     var _rev: String? = null
-    var messages: String? = null
-        private set
+    private var messages: String? = null
     var item: String? = null
     var parentCode: String? = null
     var state: String? = null
+
     fun setMessages(messages: JsonArray?) {
         this.messages = Gson().toJson(messages)
     }
 
     val messageList: List<FeedbackReply>?
-        get() {
-            if (TextUtils.isEmpty(messages)) return null
-            val feedbackReplies: MutableList<FeedbackReply> = ArrayList()
-
-            val stringReader = StringReader(messages)
-            val jsonReader = JsonReader(stringReader)
-
-            val e = JsonParser.parseReader(jsonReader)
-            val ar = e.asJsonArray
-            if (ar.size() > 0) {
-                for (i in 1 until ar.size()) {
-                    val ob = ar[i].asJsonObject
-                    feedbackReplies.add(
-                        FeedbackReply(
-                            ob["message"].asString,
-                            ob["user"].asString,
-                            ob["time"].asString
-                        )
+        get() = if (messages.isNullOrEmpty()) {
+            null
+        } else {
+            try {
+                val jsonArray = JsonParser.parseString(messages).asJsonArray
+                jsonArray.map {
+                    val jsonObject = it.asJsonObject
+                    FeedbackReply(
+                        jsonObject["message"].asString,
+                        jsonObject["user"].asString,
+                        jsonObject["time"].asString
                     )
                 }
+            } catch (e: Exception) {
+                null
             }
-            return feedbackReplies
         }
 
     val message: String
-        get() {
-            if (TextUtils.isEmpty(messages)) return ""
-
-            val stringReader = StringReader(messages)
-            val jsonReader = JsonReader(stringReader)
-
-            val e = JsonParser.parseReader(jsonReader)
-            val ar = e.asJsonArray
-            if (ar.size() > 0) {
-                val ob = ar[0].asJsonObject
-                return ob["message"].asString
+        get() = if (messages.isNullOrEmpty()) {
+            ""
+        } else {
+            try {
+                val jsonArray = JsonParser.parseString(messages).asJsonArray
+                if (jsonArray.size() > 0) {
+                    jsonArray[0].asJsonObject["message"].asString
+                } else {
+                    ""
+                }
+            } catch (e: Exception) {
+                ""
             }
-            return ""
         }
-
-    fun setMessages(messages: String?) {
-        this.messages = messages
-    }
 
     companion object {
-        val feedbacksDataList: MutableList<Array<String>> = mutableListOf()
-        @JvmStatic
-        fun serializeFeedback(feedback: RealmFeedback): JsonObject {
-            val `object` = JsonObject()
-            `object`.addProperty("title", feedback.title)
-            `object`.addProperty("source", feedback.source)
-            `object`.addProperty("status", feedback.status)
-            `object`.addProperty("priority", feedback.priority)
-            `object`.addProperty("owner", feedback.owner)
-            `object`.addProperty("openTime", feedback.openTime)
-            `object`.addProperty("type", feedback.type)
-            `object`.addProperty("url", feedback.url)
-            `object`.addProperty("parentCode", feedback.parentCode)
-            `object`.addProperty("state", feedback.state)
-            `object`.addProperty("item", feedback.item)
-            if (feedback._id != null) `object`.addProperty("_id", feedback._id)
-            if (feedback._rev != null) `object`.addProperty("_rev", feedback._rev)
+        private val feedbacksDataList: MutableList<Array<String>> = mutableListOf()
 
-            try {
-                `object`.add("messages", JsonParser.parseString(feedback.messages))
-            } catch (err: Exception) {
-                err.printStackTrace()
+        suspend fun insert(realm: Realm, act: JsonObject) {
+            realm.writeBlocking {
+                val feedback = query<RealmFeedback>("_id == $0", act["_id"].asString).first().find()
+                    ?: RealmFeedback().apply {
+                        _id = act["_id"].asString
+                    }.also { copyToRealm(it) }
+
+                feedback.apply {
+                    title = act["title"].asString
+                    source = act["source"].asString
+                    status = act["status"].asString
+                    priority = act["priority"].asString
+                    owner = act["owner"].asString
+                    openTime = act["openTime"].asLong
+                    type = act["type"].asString
+                    url = act["url"].asString
+                    parentCode = act["parentCode"].asString
+                    setMessages(act["messages"].asJsonArray)
+                    isUploaded = true
+                    item = act["item"].asString
+                    state = act["state"].asString
+                    _rev = act["_rev"].asString
+                }
+
+                feedbacksDataList.add(
+                    arrayOf(
+                        act["_id"].asString,
+                        act["title"].asString,
+                        act["source"].asString,
+                        act["status"].asString,
+                        act["priority"].asString,
+                        act["owner"].asString,
+                        act["openTime"].asString,
+                        act["type"].asString,
+                        act["url"].asString,
+                        act["parentCode"].asString,
+                        act["state"].asString,
+                        act["item"].asString,
+                        act["messages"].asJsonArray.toString()
+                    )
+                )
             }
-            return `object`
         }
 
-        @JvmStatic
-        fun insert(mRealm: Realm, act: JsonObject?) {
-            if (!mRealm.isInTransaction) {
-                mRealm.beginTransaction()
-            }
-            var feedback = mRealm.where(RealmFeedback::class.java).equalTo("_id", JsonUtils.getString("_id", act)).findFirst()
-            if (feedback == null) {
-                feedback = mRealm.createObject(RealmFeedback::class.java, JsonUtils.getString("_id", act))
-            }
-            feedback?._id = JsonUtils.getString("_id", act)
-            feedback?.title = JsonUtils.getString("title", act)
-            feedback?.source = JsonUtils.getString("source", act)
-            feedback?.status = JsonUtils.getString("status", act)
-            feedback?.priority = JsonUtils.getString("priority", act)
-            feedback?.owner = JsonUtils.getString("owner", act)
-            feedback?.openTime = JsonUtils.getLong("openTime", act)
-            feedback?.type = JsonUtils.getString("type", act)
-            feedback?.url = JsonUtils.getString("url", act)
-            feedback?.parentCode = JsonUtils.getString("parentCode", act)
-            feedback?.setMessages(Gson().toJson(JsonUtils.getJsonArray("messages", act)))
-            feedback?.isUploaded = true
-            feedback?.item = JsonUtils.getString("item", act)
-            feedback?.state = JsonUtils.getString("state", act)
-            feedback?._rev = JsonUtils.getString("_rev", act)
-            mRealm.commitTransaction()
+        fun serializeFeedback(feedback: RealmFeedback): JsonObject {
+            val jsonObject = JsonObject()
+            jsonObject.addProperty("title", feedback.title)
+            jsonObject.addProperty("source", feedback.source)
+            jsonObject.addProperty("status", feedback.status)
+            jsonObject.addProperty("priority", feedback.priority)
+            jsonObject.addProperty("owner", feedback.owner)
+            jsonObject.addProperty("openTime", feedback.openTime)
+            jsonObject.addProperty("type", feedback.type)
+            jsonObject.addProperty("url", feedback.url)
+            jsonObject.addProperty("parentCode", feedback.parentCode)
+            jsonObject.addProperty("state", feedback.state)
+            jsonObject.addProperty("item", feedback.item)
+            feedback._id?.let { jsonObject.addProperty("_id", it) }
+            feedback._rev?.let { jsonObject.addProperty("_rev", it) }
 
-            val csvRow = arrayOf(
-                JsonUtils.getString("_id", act),
-                JsonUtils.getString("title", act),
-                JsonUtils.getString("source", act),
-                JsonUtils.getString("status", act),
-                JsonUtils.getString("priority", act),
-                JsonUtils.getString("owner", act),
-                JsonUtils.getLong("openTime", act).toString(),
-                JsonUtils.getString("type", act),
-                JsonUtils.getString("url", act),
-                JsonUtils.getString("parentCode", act),
-                JsonUtils.getString("state", act),
-                JsonUtils.getString("item", act),
-                JsonUtils.getJsonArray("messages", act).toString()
-            )
-            feedbacksDataList.add(csvRow)
+            try {
+                feedback.messages?.let {
+                    jsonObject.add("messages", JsonParser.parseString(it))
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return jsonObject
         }
 
         fun writeCsv(filePath: String, data: List<Array<String>>) {
             try {
                 val file = File(filePath)
                 file.parentFile?.mkdirs()
-                val writer = CSVWriter(FileWriter(file))
-                writer.writeNext(arrayOf("feedbackId", "title", "source", "status", "priority", "owner", "openTime", "type", "url", "parentCode", "state", "item", "messages"))
-                for (row in data) {
-                    writer.writeNext(row)
+                val writer = FileWriter(file)
+                writer.write("feedbackId,title,source,status,priority,owner,openTime,type,url,parentCode,state,item,messages\n")
+                data.forEach { row ->
+                    writer.write(row.joinToString(","))
+                    writer.write("\n")
                 }
                 writer.close()
             } catch (e: IOException) {
@@ -171,8 +157,8 @@ open class RealmFeedback : RealmObject() {
             }
         }
 
-        fun feedbackWriteCsv() {
-            writeCsv("${context.getExternalFilesDir(null)}/ole/feedback.csv", feedbacksDataList)
+        fun feedbackWriteCsv(filePath: String) {
+            writeCsv(filePath, feedbacksDataList)
         }
     }
 }
