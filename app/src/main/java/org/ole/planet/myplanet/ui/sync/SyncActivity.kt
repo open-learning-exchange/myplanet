22906bb7a170fb12dabd322f8c09cadae88d57c9
Gideon Okuro <gideonollonde@gmail.com>
2024-12-17 20:46:45 +0300

    resolve log error

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.text.*
+import android.util.Log
 import android.view.*
 import android.webkit.URLUtil
 import android.widget.*
