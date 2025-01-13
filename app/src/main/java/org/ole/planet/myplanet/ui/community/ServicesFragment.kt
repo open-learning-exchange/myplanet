9034db2dee6b7cfc8c224360195a3d17294d49c1
Avinash Sharma <donotsmile9@gmail.com>
2025-01-13 22:14:15 +0530

    services: smoohter info (fixes #5006)

--- app/src/main/java/org/ole/planet/myplanet/ui/community/ServicesFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/community/ServicesFragment.kt
@@ -75,25 +75,37 @@ class ServicesFragment : BaseTeamFragment() {
 
     private fun setRecyclerView(links: RealmResults<RealmMyTeam>?) {
         fragmentServicesBinding.llServices.removeAllViews()
-        links?.forEach { team ->
-            val b: TextView = LayoutInflater.from(activity).inflate(R.layout.button_single, fragmentServicesBinding.llServices, false) as TextView
-            b.setPadding(8, 8, 8, 8)
-            b.text = team.title
-            b.setOnClickListener {
-                val route = team.route?.split("/")
-                if (route != null) {
-                    if (route.size >= 3) {
-                        val f = TeamDetailFragment()
-                        val c = Bundle()
-                        val teamObject = mRealm.where(RealmMyTeam::class.java)?.equalTo("_id", route[3])?.findFirst()
-                        c.putString("id", route[3])
-                        teamObject?.isMyTeam(user?.id, mRealm)?.let { it1 -> c.putBoolean("isMyTeam", it1) }
-                        f.arguments = c
-                        (context as OnHomeItemClickListener).openCallFragment(f)
+        if(links.isNullOrEmpty()){
+            val placeholderView = LayoutInflater.from(activity).inflate(R.layout.placeholder_services, fragmentServicesBinding.llServices, false)
+            fragmentServicesBinding.llServices.addView(placeholderView)
+        }else {
+            links?.forEach { team ->
+                val b: TextView = LayoutInflater.from(activity).inflate(
+                    R.layout.button_single,
+                    fragmentServicesBinding.llServices,
+                    false
+                ) as TextView
+                b.setPadding(8, 8, 8, 8)
+                b.text = team.title
+                b.setOnClickListener {
+                    val route = team.route?.split("/")
+                    if (route != null) {
+                        if (route.size >= 3) {
+                            val f = TeamDetailFragment()
+                            val c = Bundle()
+                            val teamObject =
+                                mRealm.where(RealmMyTeam::class.java)?.equalTo("_id", route[3])
+                                    ?.findFirst()
+                            c.putString("id", route[3])
+                            teamObject?.isMyTeam(user?.id, mRealm)
+                                ?.let { it1 -> c.putBoolean("isMyTeam", it1) }
+                            f.arguments = c
+                            (context as OnHomeItemClickListener).openCallFragment(f)
+                        }
                     }
                 }
+                fragmentServicesBinding.llServices.addView(b)
             }
-            fragmentServicesBinding.llServices.addView(b)
         }
     }
 }
