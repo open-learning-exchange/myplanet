07d3ecef7f99f382d402ebac3fdd47b6f4a3d546
Avinash Sharma <donotsmile9@gmail.com>
2025-01-14 00:49:33 +0530

    disclaimer: smoother link (fixes #5017)

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DisclaimerFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DisclaimerFragment.kt
@@ -1,20 +1,38 @@
 package org.ole.planet.myplanet.ui.dashboard
 
 import android.os.Bundle
-import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
+import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.databinding.FragmentDisclaimerBinding
-import org.ole.planet.myplanet.utilities.Constants
 
 class DisclaimerFragment : Fragment() {
-    private lateinit var fragmentDisclaimerBinding: FragmentDisclaimerBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        fragmentDisclaimerBinding = FragmentDisclaimerBinding.inflate(inflater, container, false)
-        fragmentDisclaimerBinding.tvDisclaimer.text = Html.fromHtml(getString(Constants.DISCLAIMER), HtmlCompat.FROM_HTML_MODE_LEGACY)
-        return fragmentDisclaimerBinding.root
+
+    private var _binding: FragmentDisclaimerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDisclaimerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val disclaimerText = getString(R.string.disclaimer)
+        binding.tvDisclaimer.text = HtmlCompat.fromHtml(disclaimerText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        binding.tvDisclaimer.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
