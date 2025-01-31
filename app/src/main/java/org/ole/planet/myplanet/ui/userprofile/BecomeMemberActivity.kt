c4d86b1a7f2cb3c536891e59c4014578a6cff6ad
Avinash Sharma <donotsmile9@gmail.com>
2025-01-31 23:43:39 +0530

    smoother: dark mode colors

--- app/src/main/java/org/ole/planet/myplanet/ui/userprofile/BecomeMemberActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/userprofile/BecomeMemberActivity.kt
@@ -271,9 +271,11 @@ class BecomeMemberActivity : BaseActivity() {
                 }
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
         })
     }
 }
