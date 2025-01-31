3e0e1ac8be3f4972553459f665d3866d8b4fc934
Gideon Okuro <gideonollonde@gmail.com>
2025-01-31 19:44:53 +0300

    Update AdapterLeader.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/community/AdapterLeader.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/community/AdapterLeader.kt
@@ -1,7 +1,6 @@
 package org.ole.planet.myplanet.ui.community
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
