8828fef2a458959ca8103ab50426370bd8c4dad5
Gideon Okuro <gideonollonde@gmail.com>
2025-02-05 21:20:35 +0300

    update adopt a survey button

--- app/src/main/java/org/ole/planet/myplanet/ui/survey/AdapterSurvey.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/survey/AdapterSurvey.kt
@@ -1,17 +1,20 @@
 package org.ole.planet.myplanet.ui.survey
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
+import org.json.JSONObject
 import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.callback.OnHomeItemClickListener
 import org.ole.planet.myplanet.databinding.RowSurveyBinding
 import org.ole.planet.myplanet.model.RealmExamQuestion
 import org.ole.planet.myplanet.model.RealmStepExam
+import org.ole.planet.myplanet.model.RealmSubmission
 import org.ole.planet.myplanet.model.RealmSubmission.Companion.getNoOfSubmissionByUser
 import org.ole.planet.myplanet.model.RealmSubmission.Companion.getRecentSubmissionDate
 import org.ole.planet.myplanet.ui.submission.AdapterMySubmission
@@ -55,11 +58,25 @@ class AdapterSurvey(private val context: Context, private val mRealm: Realm, pri
             }
         }
 
+        val filteredParentIds = mRealm.where(RealmSubmission::class.java)
+            .isNotNull("membershipDoc")
+            .findAll()
+            .mapNotNull { submission ->
+                val parentJson = JSONObject(submission.parent ?: "{}")
+                parentJson.optString("_id")
+            }
+            .filter { it.isNotEmpty() }
+            .toSet()
+
         fun bind(exam: RealmStepExam) {
             binding.apply {
                 tvTitle.text = exam.name
                 startSurvey.setOnClickListener {
-                    AdapterMySubmission.openSurvey(listener, exam.id, false, isTeam, teamId)
+                    when {
+                        exam.id !in filteredParentIds && exam.isTeamShareAllowed -> Log.d("okuro", "adopt survey")
+                        else -> AdapterMySubmission.openSurvey(listener, exam.id, false, isTeam, teamId)
+                    }
+
                 }
 
                 val questions = mRealm.where(RealmExamQuestion::class.java)
@@ -72,7 +89,7 @@ class AdapterSurvey(private val context: Context, private val mRealm: Realm, pri
                 }
 
                 startSurvey.text = when {
-                    exam.isTeamShareAllowed -> context.getString(R.string.adopt_survey)
+                    exam.id !in filteredParentIds && exam.isTeamShareAllowed -> context.getString(R.string.adopt_survey)
                     exam.isFromNation -> context.getString(R.string.take_survey)
                     else -> context.getString(R.string.record_survey)
                 }
