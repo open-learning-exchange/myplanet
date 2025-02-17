b6e95563e01574e905128c657123c118fa4743ab
Deep Vora <70479785+deeppp15@users.noreply.github.com>
2025-02-17 18:23:22 -0500

    cleanup logs

--- app/src/main/java/org/ole/planet/myplanet/base/BaseResourceFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseResourceFragment.kt
@@ -10,7 +10,6 @@ import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
