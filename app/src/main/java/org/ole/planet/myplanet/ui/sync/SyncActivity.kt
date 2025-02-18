36d5fb423b29b711f0da83049a7ad53df2a9e667
Gideon Okuro <gideonollonde@gmail.com>
2025-02-18 17:29:52 +0300

    smoothe file not found error handling

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/SyncActivity.kt
@@ -446,7 +446,7 @@ abstract class SyncActivity : ProcessUserDataActivity(), SyncListener, CheckVers
                 syncIconDrawable.selectDrawable(0)
                 syncIcon.invalidateDrawable(syncIconDrawable)
                 MainApplication.applicationScope.launch {
-                    createLog("synced successfully")
+                    createLog("synced successfully", "")
                 }
                 showSnack(findViewById(android.R.id.content), getString(R.string.sync_completed))
                 if (settings.getBoolean("isAlternativeUrl", false)) {
