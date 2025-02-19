c8f0fda920d34549873cb259fd4a7dfdc4b85f81
Avinash Sharma <donotsmile9@gmail.com>
2025-02-19 22:52:53 +0530

    fixed the issue

--- app/src/main/java/org/ole/planet/myplanet/ui/SettingActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/SettingActivity.kt
@@ -237,8 +237,6 @@ class SettingActivity : AppCompatActivity() {
             }
         }
 
-
-
         companion object {
             fun darkMode(context: Context) {
                 val options = arrayOf(context.getString(R.string.dark_mode_off), context.getString(R.string.dark_mode_on),context.getString(R.string.dark_mode_follow_system))
