d84e8c54c8b625a09b8c67c6d78e60db278ed324
Gideon Okuro <gideonollonde@gmail.com>
2025-03-03 23:19:09 +0300

    teams: smoother joining security (fixes #5381) (#5400)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/AdapterMemberRequest.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/teamMember/AdapterMemberRequest.kt
@@ -2,16 +2,19 @@ package org.ole.planet.myplanet.ui.team.teamMember
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
 import org.ole.planet.myplanet.databinding.RowMemberRequestBinding
 import org.ole.planet.myplanet.model.RealmMyTeam
 import org.ole.planet.myplanet.model.RealmUserModel
+import org.ole.planet.myplanet.service.UserProfileDbHandler
 
 class AdapterMemberRequest(private val context: Context, private val list: MutableList<RealmUserModel>, private val mRealm: Realm, private val isTeamLeader: Boolean) : RecyclerView.Adapter<AdapterMemberRequest.ViewHolderUser>() {
     private lateinit var rowMemberRequestBinding: RowMemberRequestBinding
     private var teamId: String? = null
+    private lateinit var team: RealmMyTeam
 
     fun setTeamId(teamId: String?) {
         this.teamId = teamId
@@ -25,27 +28,42 @@ class AdapterMemberRequest(private val context: Context, private val list: Mutab
     override fun onBindViewHolder(holder: ViewHolderUser, position: Int) {
         val currentItem = list.getOrNull(position) ?: return
         rowMemberRequestBinding.tvName.text = currentItem.name ?: currentItem.toString()
+        val user = UserProfileDbHandler(context).userModel
 
-        if (isTeamLeader) {
-            rowMemberRequestBinding.btnAccept.isEnabled = true
-            rowMemberRequestBinding.btnReject.isEnabled = true
-
-            rowMemberRequestBinding.btnAccept.setOnClickListener {
-                val adapterPosition = holder.bindingAdapterPosition
-                if (adapterPosition != RecyclerView.NO_POSITION && adapterPosition < list.size) {
-                    acceptReject(list[adapterPosition], true, adapterPosition)
-                }
+        team = try {
+            mRealm.where(RealmMyTeam::class.java).equalTo("_id", teamId).findFirst()
+                ?: throw IllegalArgumentException("Team not found for ID: $teamId")
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+            try {
+                mRealm.where(RealmMyTeam::class.java).equalTo("teamId", teamId).findFirst()
+                    ?: throw IllegalArgumentException("Team not found for ID: $teamId")
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                return
             }
+        }
 
-            rowMemberRequestBinding.btnReject.setOnClickListener {
-                val adapterPosition = holder.bindingAdapterPosition
-                if (adapterPosition != RecyclerView.NO_POSITION && adapterPosition < list.size) {
-                    acceptReject(list[adapterPosition], false, adapterPosition)
-                }
+        val isUserRequested = team.requested(user?.id, mRealm)
+        with(rowMemberRequestBinding) {
+            btnAccept.visibility = if (isUserRequested) View.GONE else View.VISIBLE
+            btnReject.visibility = if (isUserRequested) View.GONE else View.VISIBLE
+
+            val isEnabled = isTeamLeader && !isUserRequested
+            btnAccept.isEnabled = isEnabled
+            btnReject.isEnabled = isEnabled
+
+            if (isTeamLeader) {
+                btnAccept.setOnClickListener { handleClick(holder, true) }
+                btnReject.setOnClickListener { handleClick(holder, false) }
             }
-        } else {
-            rowMemberRequestBinding.btnAccept.isEnabled = false
-            rowMemberRequestBinding.btnReject.isEnabled = false
+        }
+    }
+
+    private fun handleClick(holder: RecyclerView.ViewHolder, isAccepted: Boolean) {
+        val adapterPosition = holder.bindingAdapterPosition
+        if (adapterPosition != RecyclerView.NO_POSITION && adapterPosition < list.size) {
+            acceptReject(list[adapterPosition], isAccepted, adapterPosition)
         }
     }
 
