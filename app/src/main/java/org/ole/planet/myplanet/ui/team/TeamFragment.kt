f44641dbbd907e738c82ea5e8511680fa36cef83
Pavneet Singh Cheema <90737218+pavi38@users.noreply.github.com>
2025-03-17 15:16:52 -0500

    teams: smoother search (fixes #5479) (#5495)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
@@ -199,6 +199,10 @@ class TeamFragment : Fragment(), AdapterTeamList.OnClickTeamItem {
         fragmentTeamBinding.etSearch.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                if (TextUtils.isEmpty(charSequence)) {
+                    updatedTeamList()
+                    return
+                }
                 val query = mRealm.where(RealmMyTeam::class.java).isEmpty("teamId")
                     .notEqualTo("status", "archived")
                     .contains("name", charSequence.toString(), Case.INSENSITIVE)
