03c3b6811d81f5a15d4d5b27e7b76c4f426dcb19
Gideon Okuro <gideonollonde@gmail.com>
2025-03-19 22:17:24 +0300

    prevent used username from creating new account

--- app/src/main/java/org/ole/planet/myplanet/ui/userprofile/BecomeMemberActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/userprofile/BecomeMemberActivity.kt
@@ -168,6 +168,8 @@ class BecomeMemberActivity : BaseActivity() {
                 activityBecomeMemberBinding.etUsername.error = getString(R.string.must_start_with_letter_or_number)
             } else if (hasInvalidCharacters || hasSpecialCharacters || hasDiacriticCharacters) {
                activityBecomeMemberBinding.etUsername.error = getString(R.string.only_letters_numbers_and_are_allowed)
+            } else if (RealmUserModel.isUserExists(mRealm, activityBecomeMemberBinding.etUsername.text.toString())) {
+                activityBecomeMemberBinding.etUsername.error = getString(R.string.username_taken)
             } else if (TextUtils.isEmpty(password)) {
                 activityBecomeMemberBinding.etPassword.error = getString(R.string.please_enter_a_password)
             } else if (password != rePassword) {
