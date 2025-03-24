cf2727fa90ebb3c532947f638fdac5a21c063a41
Avinash Sharma <133222313+Avinash-Codes@users.noreply.github.com>
2025-03-25 01:09:13 +0530

    teams: smoother voices editing (fixes #5471) (#5489)
    Co-authored-by: Gideon Okuro <gideonollonde@gmail.com>
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/news/AdapterNews.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/news/AdapterNews.kt
@@ -141,6 +141,11 @@ class AdapterNews(var context: Context, private val list: MutableList<RealmNews?
                     holder.rowNewsBinding.linearLayout51.visibility = View.GONE
                 }
                 holder.rowNewsBinding.tvDate.text = formatDate(news.time)
+                if (news.isEdited) {
+                    holder.rowNewsBinding.tvEdited.visibility = View.VISIBLE
+                } else {
+                    holder.rowNewsBinding.tvEdited.visibility = View.GONE
+                }
                 if (news.userId == currentUser?._id) {
                     holder.rowNewsBinding.imgDelete.setOnClickListener {
                         AlertDialog.Builder(context)
@@ -413,7 +418,7 @@ class AdapterNews(var context: Context, private val list: MutableList<RealmNews?
             return
         }
         if (!mRealm.isInTransaction) mRealm.beginTransaction()
-        news?.message = s
+        news?.updateMessage(s)
         mRealm.commitTransaction()
         notifyDataSetChanged()
         listener?.clearImages()
@@ -437,9 +442,11 @@ class AdapterNews(var context: Context, private val list: MutableList<RealmNews?
 
     private fun showHideButtons(userModel: RealmUserModel, holder: RecyclerView.ViewHolder) {
         val viewHolder = holder as ViewHolderNews
-        if (currentUser?.id == userModel.id) {
+        if (currentUser?.id == userModel.id && !fromLogin && !nonTeamMember) {
             viewHolder.rowNewsBinding.llEditDelete.visibility = View.VISIBLE
             viewHolder.rowNewsBinding.btnAddLabel.visibility = View.VISIBLE
+            viewHolder.rowNewsBinding.imgEdit.visibility = View.VISIBLE
+            viewHolder.rowNewsBinding.imgDelete.visibility = View.VISIBLE
         } else {
             viewHolder.rowNewsBinding.llEditDelete.visibility = View.GONE
             viewHolder.rowNewsBinding.btnAddLabel.visibility = View.GONE
