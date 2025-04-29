791df92eb288a0d401e6a3857bb757f94e4f0478
Avinash Sharma <donotsmile9@gmail.com>
2025-04-29 22:15:00 +0530

    fixed the issue

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamPagerAdapter.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamPagerAdapter.kt
@@ -5,8 +5,6 @@ import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import org.ole.planet.myplanet.MainApplication
-import org.ole.planet.myplanet.MainApplication.Companion.context
 import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.model.RealmMyTeam
 import org.ole.planet.myplanet.ui.enterprises.FinanceFragment
@@ -19,58 +17,73 @@ import org.ole.planet.myplanet.ui.team.teamMember.MembersFragment
 import org.ole.planet.myplanet.ui.team.teamResource.TeamResourceFragment
 import org.ole.planet.myplanet.ui.team.teamTask.TeamTaskFragment
 
-class TeamPagerAdapter(fm: FragmentActivity, team: RealmMyTeam?, isInMyTeam: Boolean) : FragmentStateAdapter(fm) {
+class TeamPagerAdapter(private val activity: FragmentActivity, private val team: RealmMyTeam?, private val isInMyTeam: Boolean) : FragmentStateAdapter(activity) {
     private val teamId: String? = team?._id
-    private val list: MutableList<String> = ArrayList()
     private val isEnterprise: Boolean = TextUtils.equals(team?.type, "enterprise")
 
-    init {
-        if (isInMyTeam || team?.isPublic == true) {
-            list.add(context.getString(R.string.chat))
-            list.add(context.getString(if (isEnterprise) R.string.mission else R.string.plan))
-            list.add(context.getString(if (isEnterprise) R.string.team else R.string.members))
-            list.add(context.getString(R.string.tasks))
-            list.add(context.getString(R.string.calendar))
-            list.add(context.getString(R.string.survey))
-            list.add(context.getString(if (isEnterprise) R.string.finances else R.string.courses))
-            if (isEnterprise) {
-                list.add(context.getString(R.string.reports))
-            }
-            list.add(context.getString(if (isEnterprise) R.string.documents else R.string.resources))
-            list.add(context.getString(if (isEnterprise) R.string.applicants else R.string.join_requests))
-        } else {
-            list.add(context.getString(if (isEnterprise) R.string.mission else R.string.plan))
-            list.add(context.getString(if (isEnterprise) R.string.team else R.string.members))
+    fun getPageTitle(position: Int): CharSequence {
+        val index = getPositionIndex(position)
+        return when (index) {
+            "chat" -> activity.getString(R.string.chat)
+            "plan" -> activity.getString(if (isEnterprise) R.string.mission else R.string.plan)
+            "members" -> activity.getString(if (isEnterprise) R.string.team else R.string.members)
+            "tasks" -> activity.getString(R.string.tasks)
+            "calendar" -> activity.getString(R.string.calendar)
+            "survey" -> activity.getString(R.string.survey)
+            "courses" -> activity.getString(if (isEnterprise) R.string.finances else R.string.courses)
+            "reports" -> activity.getString(R.string.reports)
+            "resources" -> activity.getString(if (isEnterprise) R.string.documents else R.string.resources)
+            "requests" -> activity.getString(if (isEnterprise) R.string.applicants else R.string.join_requests)
+            else -> ""
         }
     }
 
-    fun getPageTitle(position: Int): CharSequence {
-        return list[position]
+    private fun getPositionIndex(position: Int): String {
+        if (isInMyTeam || team?.isPublic == true) {
+            return when (position) {
+                0 -> "chat"
+                1 -> "plan"
+                2 -> "members"
+                3 -> "tasks"
+                4 -> "calendar"
+                5 -> "survey"
+                6 -> if (isEnterprise) "courses" else "finances"
+                7 -> if (isEnterprise) "reports" else "resources"
+                8 -> if (isEnterprise) "resources" else "requests"
+                9 -> if (isEnterprise) "requests" else ""
+                else -> ""
+            }
+        } else {
+            return when (position) {
+                0 -> "plan"
+                1 -> "members"
+                else -> ""
+            }
+        }
     }
 
     override fun createFragment(position: Int): Fragment {
-        if (position < 0 || position >= list.size) {
-            throw IllegalArgumentException("Invalid position: $position. List size: ${list.size}")
-        }
-        val fragment: Fragment = when (list[position]) {
-            context.getString(R.string.chat) -> DiscussionListFragment()
-            context.getString(R.string.plan), context.getString(R.string.mission) -> PlanFragment()
-            context.getString(R.string.members), context.getString(R.string.team) -> JoinedMemberFragment()
-            context.getString(R.string.tasks) -> TeamTaskFragment()
-            context.getString(R.string.calendar) -> TeamCalendarFragment()
-            context.getString(R.string.survey) -> SurveyFragment().apply {
+        val index = getPositionIndex(position)
+        val fragment: Fragment = when (index) {
+            "chat" -> DiscussionListFragment()
+            "plan" -> PlanFragment()
+            "members" -> JoinedMemberFragment()
+            "tasks" -> TeamTaskFragment()
+            "calendar" -> TeamCalendarFragment()
+            "survey" -> SurveyFragment().apply {
                 arguments = Bundle().apply {
                     putBoolean("isTeam", true)
                     putString("teamId", teamId)
                 }
             }
-            context.getString(R.string.courses) -> TeamCourseFragment()
-            context.getString(R.string.finances) -> FinanceFragment()
-            context.getString(R.string.reports) -> ReportsFragment()
-            context.getString(R.string.resources), context.getString(R.string.documents) -> TeamResourceFragment().apply { MainApplication.listener = this }
-            context.getString(R.string.join_requests), context.getString(R.string.applicants) -> MembersFragment()
+            "courses" -> TeamCourseFragment()
+            "finances" -> FinanceFragment()
+            "reports" -> ReportsFragment()
+            "resources" -> TeamResourceFragment()
+            "requests" -> MembersFragment()
             else -> throw IllegalArgumentException("Invalid fragment type for position: $position")
         }
+
         if (fragment.arguments == null) {
             fragment.arguments = Bundle().apply {
                 putString("id", teamId)
@@ -80,6 +93,10 @@ class TeamPagerAdapter(fm: FragmentActivity, team: RealmMyTeam?, isInMyTeam: Boo
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return if (isInMyTeam || team?.isPublic == true) {
+            if (isEnterprise) 10 else 9
+        } else {
+            2
+        }
     }
 }
