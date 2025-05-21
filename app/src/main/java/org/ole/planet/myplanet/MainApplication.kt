c757ee5ca16ce63c92b39efc5749b9a9d06370e7
Gideon Okuro <gideonollonde@gmail.com>
2025-05-21 19:30:09 +0300

    ensure parent and planet code is sent

--- app/src/main/java/org/ole/planet/myplanet/MainApplication.kt
+++ app/src/main/java/org/ole/planet/myplanet/MainApplication.kt
@@ -79,13 +79,14 @@ class MainApplication : Application(), Application.ActivityLifecycleCallbacks {
         fun createLog(type: String, error: String) {
             applicationScope.launch(Dispatchers.IO) {
                 val realm = Realm.getDefaultInstance()
+                val settings = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
                 try {
                     realm.executeTransaction { r ->
                         val log = r.createObject(RealmApkLog::class.java, "${UUID.randomUUID()}")
                         val model = UserProfileDbHandler(context).userModel
+                        log.parentCode = settings.getString("parentCode", "")
+                        log.createdOn = settings.getString("planetCode", "")
                         if (model != null) {
-                            log.parentCode = model.parentCode
-                            log.createdOn = model.planetCode
                             log.userId = model.id
                         }
                         log.time = "${Date().time}"
