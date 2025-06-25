9bd1f73f62b1280840dbeba14ee1a04c27a69537
Saksham Nayyar <39906853+Sakshamnayyar@users.noreply.github.com>
2025-06-25 12:23:18 -0400

    added null check for userModel in showMemberDetails

--- app/src/main/java/org/ole/planet/myplanet/ui/news/AdapterNews.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/news/AdapterNews.kt
@@ -9,6 +9,7 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Build
 import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -305,20 +306,20 @@ class AdapterNews(var context: Context, private val list: MutableList<RealmNews?
         }
     }
     private fun showMemberDetails(userModel: RealmUserModel?, it: View){
-        val activity = it.context as AppCompatActivity
-        val userName = if ("${userModel?.firstName} ${userModel?.lastName}".trim().isBlank()) {
-            userModel?.name
-        } else {
-            "${userModel?.firstName} ${userModel?.lastName}".trim()
+        if (userModel == null) {
+            Log.e("AdapterNews", "showMemberDetails() called with null userModel")
+            return
         }
+        val activity = it.context as AppCompatActivity
+        val userName = "${userModel.firstName} ${userModel.lastName}".trim().ifBlank { userModel.name }
         val fragment = MemberDetailFragment.newInstance(
             userName.toString(),
-            userModel?.email.toString(),
-            userModel?.dob.toString().substringBefore("T"),
-            userModel?.language.toString(),
-            userModel?.phoneNumber.toString(),
+            userModel.email.toString(),
+            userModel.dob.toString().substringBefore("T"),
+            userModel.language.toString(),
+            userModel.phoneNumber.toString(),
             profileDbHandler.getOfflineVisits(userModel).toString(),
-            profileDbHandler.getLastVisit(userModel!!),
+            profileDbHandler.getLastVisit(userModel),
             "${userModel.firstName} ${userModel.lastName}",
             userModel.level.toString(),
             userModel.userImage
