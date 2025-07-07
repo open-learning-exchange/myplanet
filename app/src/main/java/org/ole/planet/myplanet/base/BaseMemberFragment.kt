9d93b311e8f45326b372679a1044ed5e9b4c99c5
dogi <dogi@users.noreply.github.com>
2025-07-07 13:08:35 -0400

    Use ViewBinding in BaseMemberFragment

--- app/src/main/java/org/ole/planet/myplanet/base/BaseMemberFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/base/BaseMemberFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.ole.planet.myplanet.R
+import org.ole.planet.myplanet.databinding.FragmentMembersBinding
 import org.ole.planet.myplanet.model.RealmUserModel
 import org.ole.planet.myplanet.ui.team.BaseTeamFragment
 
@@ -14,19 +13,26 @@ abstract class BaseMemberFragment : BaseTeamFragment() {
     abstract val list: List<RealmUserModel?>
     abstract val adapter: RecyclerView.Adapter<*>?
     abstract val layoutManager: RecyclerView.LayoutManager?
-    private lateinit var rvMember: RecyclerView
-    private var tvNoData: TextView? = null
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater.inflate(R.layout.fragment_members, container, false)
-        rvMember = v.findViewById(R.id.rv_member)
-        tvNoData = v.findViewById(R.id.tv_nodata)
-        return v
+    private var _binding: FragmentMembersBinding? = null
+    protected val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMembersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        rvMember.layoutManager = layoutManager
-        rvMember.adapter = adapter
-        showNoData(tvNoData, list.size, "members")
+        binding.rvMember.layoutManager = layoutManager
+        binding.rvMember.adapter = adapter
+        showNoData(binding.tvNodata, list.size, "members")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
