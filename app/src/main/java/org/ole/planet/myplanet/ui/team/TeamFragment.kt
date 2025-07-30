d3b2e6e3a8efe420255a51c882619175ed3acd98
dogi <dogi@users.noreply.github.com>
2025-07-30 08:15:51 -0400

    fix viewmodel init

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
@@ -36,7 +36,7 @@ class TeamFragment : Fragment(), AdapterTeamList.OnClickTeamItem {
     private lateinit var fragmentTeamBinding: FragmentTeamBinding
     private lateinit var alertCreateTeamBinding: AlertCreateTeamBinding
     private lateinit var mRealm: Realm
-    private val teamViewModel: TeamViewModel by viewModels { defaultViewModelProviderFactory }
+    private val teamViewModel: TeamViewModel by viewModels()
     @Inject
     lateinit var databaseService: DatabaseService
     var type: String? = null
