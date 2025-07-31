5ed62076a8a81194f037c16644dab619e1e4029a
dogi <dogi@users.noreply.github.com>
2025-07-31 13:04:40 -0400

    Fix ViewModel initialization

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamFragment.kt
@@ -10,7 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import io.realm.Case
@@ -36,7 +36,13 @@ class TeamFragment : Fragment(), AdapterTeamList.OnClickTeamItem {
     private lateinit var fragmentTeamBinding: FragmentTeamBinding
     private lateinit var alertCreateTeamBinding: AlertCreateTeamBinding
     private lateinit var mRealm: Realm
-    private val teamViewModel: TeamViewModel by activityViewModels()
+    private val teamViewModel: TeamViewModel by lazy {
+        ViewModelProvider(
+            requireActivity(),
+            defaultViewModelProviderFactory,
+            requireActivity().defaultViewModelCreationExtras
+        )[TeamViewModel::class.java]
+    }
     @Inject
     lateinit var databaseService: DatabaseService
     var type: String? = null
