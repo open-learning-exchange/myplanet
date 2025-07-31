c433d580298e0b896335939f9a8466b386985d1d
Gideon Okuro <gideonollonde@gmail.com>
2025-07-31 16:31:10 +0300

    cleanup log

--- app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
@@ -10,7 +10,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
-import android.util.Log
 import android.view.ContextThemeWrapper
 import android.view.KeyEvent
 import android.view.LayoutInflater
