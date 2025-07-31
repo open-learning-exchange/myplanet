593611f1222a7be63a19f8d9d71ee66ed604bc66
dogi <dogi@users.noreply.github.com>
2025-07-31 15:40:19 -0400

    all: smoother user repository usage (fixes #6539) (#6509)

--- app/src/main/java/org/ole/planet/myplanet/ui/team/BaseTeamFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/BaseTeamFragment.kt
@@ -19,7 +19,7 @@ abstract class BaseTeamFragment : BaseNewsFragment() {
         val sParentCode = settings.getString("parentCode", "")
         val communityName = settings.getString("communityName", "")
         teamId = requireArguments().getString("id", "") ?: "$communityName@$sParentCode"
-        mRealm = databaseService.realmInstance
+        mRealm = userRepository.getRealm()
         user = profileDbHandler.userModel?.let { mRealm.copyFromRealm(it) }
 
         if (shouldQueryTeamFromRealm()) {
