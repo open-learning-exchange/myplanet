0e546ed44bd5a8a6d211ac16f57ae3eea760c819
dogi <dogi@users.noreply.github.com>
2025-08-01 15:53:36 -0400

    all: smoother `IntentUtils.kt` formatting (fixes #6563) (#6551)

--- app/src/main/java/org/ole/planet/myplanet/utilities/IntentUtils.kt
+++ app/src/main/java/org/ole/planet/myplanet/utilities/IntentUtils.kt
@@ -1,17 +1,17 @@
-    package org.ole.planet.myplanet.utilities
+package org.ole.planet.myplanet.utilities
 
-    import android.content.Context
-    import android.content.Intent
-    import org.ole.planet.myplanet.ui.viewer.AudioPlayerActivity
+import android.content.Context
+import android.content.Intent
+import org.ole.planet.myplanet.ui.viewer.AudioPlayerActivity
 
-    object IntentUtils {
-        @JvmStatic
-        fun openAudioFile(context: Context, path: String?, resourceTitle: String? = null) {
-            val intent = Intent(context, AudioPlayerActivity::class.java).apply {
-                putExtra("isFullPath", true)
-                putExtra("TOUCHED_FILE", path)
-                putExtra("RESOURCE_TITLE", resourceTitle)
-            }
-            context.startActivity(intent)
+object IntentUtils {
+    @JvmStatic
+    fun openAudioFile(context: Context, path: String?, resourceTitle: String? = null) {
+        val intent = Intent(context, AudioPlayerActivity::class.java).apply {
+            putExtra("isFullPath", true)
+            putExtra("TOUCHED_FILE", path)
+            putExtra("RESOURCE_TITLE", resourceTitle)
         }
+        context.startActivity(intent)
     }
+}
