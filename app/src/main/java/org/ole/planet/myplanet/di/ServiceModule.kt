cc40bb6bb7513cfbde1d61b9047e18f7c175fda1
dogi <dogi@users.noreply.github.com>
2025-08-05 14:21:46 -0400

    resources: smoother video viewing (fixes #6620) (#6581)

--- app/src/main/java/org/ole/planet/myplanet/di/ServiceModule.kt
+++ app/src/main/java/org/ole/planet/myplanet/di/ServiceModule.kt
@@ -8,6 +8,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import javax.inject.Qualifier
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.ole.planet.myplanet.datamanager.ApiInterface
 import org.ole.planet.myplanet.datamanager.DatabaseService
 import org.ole.planet.myplanet.di.AppPreferences
@@ -15,9 +19,20 @@ import org.ole.planet.myplanet.service.SyncManager
 import org.ole.planet.myplanet.service.UploadManager
 import org.ole.planet.myplanet.service.UploadToShelfService
 
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationScope
+
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    }
 
     @Provides
     @Singleton
---
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/VideoPlayerModule.kt
@@ -0,0 +1,17 @@
+package org.ole.planet.myplanet.ui.viewer
+
+import android.app.Activity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import org.ole.planet.myplanet.utilities.AuthSessionUpdater
+
+@Module
+@InstallIn(ActivityComponent::class)
+object VideoPlayerModule {
+    @Provides
+    fun provideAuthSessionUpdaterCallback(activity: Activity): AuthSessionUpdater.AuthCallback {
+        return activity as AuthSessionUpdater.AuthCallback
+    }
+}
