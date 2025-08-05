304e28e47115e16a528f9a6a5ef81c26b4f940fa
dogi <dogi@users.noreply.github.com>
2025-08-05 14:21:46 -0400

    resources: smoother video viewing (fixes #6620) (#6581)

--- app/src/main/java/org/ole/planet/myplanet/utilities/AuthSessionUpdater.kt
+++ app/src/main/java/org/ole/planet/myplanet/utilities/AuthSessionUpdater.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import java.io.DataOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -12,11 +13,13 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
+import org.ole.planet.myplanet.di.AppPreferences
+import org.ole.planet.myplanet.di.ApplicationScope
 
-class AuthSessionUpdater(
+class AuthSessionUpdater @Inject constructor(
     private val callback: AuthCallback,
-    private val settings: SharedPreferences,
-    private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    @AppPreferences private val settings: SharedPreferences,
+    @ApplicationScope private val scope: CoroutineScope
 ) {
 
     interface AuthCallback {
