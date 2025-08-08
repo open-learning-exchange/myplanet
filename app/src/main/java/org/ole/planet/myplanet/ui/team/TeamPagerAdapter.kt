f07659ecf7a2434be0288fd860062946fd76a75f
Saksham Nayyar <39906853+Sakshamnayyar@users.noreply.github.com>
2025-08-08 18:29:34 -0400

    teams: smoother tabbing (fixes #6685) (#6585)
    Co-authored-by: Gideon Okuro <gideonollonde@gmail.com>
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamPagerAdapter.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamPagerAdapter.kt
@@ -5,91 +5,75 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.ole.planet.myplanet.MainApplication
-import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.callback.MemberChangeListener
-import org.ole.planet.myplanet.model.RealmMyTeam
-import org.ole.planet.myplanet.ui.enterprises.FinanceFragment
-import org.ole.planet.myplanet.ui.enterprises.ReportsFragment
-import org.ole.planet.myplanet.ui.survey.SurveyFragment
-import org.ole.planet.myplanet.ui.team.teamCourse.TeamCourseFragment
-import org.ole.planet.myplanet.ui.team.teamDiscussion.DiscussionListFragment
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.ApplicantsPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.CalendarPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.ChatPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.CoursesPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.DocumentsPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.FinancesPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.JoinRequestsPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.MembersPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.MissionPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.PlanPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.ReportsPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.ResourcesPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.SurveyPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.TasksPage
+import org.ole.planet.myplanet.ui.team.TeamPageConfig.TeamPage
 import org.ole.planet.myplanet.ui.team.teamMember.JoinedMemberFragment
 import org.ole.planet.myplanet.ui.team.teamMember.MembersFragment
 import org.ole.planet.myplanet.ui.team.teamResource.TeamResourceFragment
-import org.ole.planet.myplanet.ui.team.teamTask.TeamTaskFragment
 
 class TeamPagerAdapter(
     private val fm: FragmentActivity,
-    team: RealmMyTeam?,
-    isInMyTeam: Boolean,
+    private val pages: List<TeamPageConfig>,
+    private val teamId: String?,
     private val memberChangeListener: MemberChangeListener
 ) : FragmentStateAdapter(fm) {
-    private val teamId = team?._id
-    private val isEnterprise = team?.type == "enterprise"
 
-    private val pages = mutableListOf<TeamPage>()
-    private val fragments: List<Fragment>
-
-    init {
-        if (isInMyTeam || team?.isPublic == true) {
-            pages += TeamPage.CHAT
-            pages += if (isEnterprise) TeamPage.MISSION else TeamPage.PLAN
-            pages += if (isEnterprise) TeamPage.TEAM else TeamPage.MEMBERS
-            pages += TeamPage.TASKS
-            pages += TeamPage.CALENDAR
-            pages += TeamPage.SURVEY
-            pages += if (isEnterprise) TeamPage.FINANCES else TeamPage.COURSES
-            if (isEnterprise) pages += TeamPage.REPORTS
-            pages += if (isEnterprise) TeamPage.DOCUMENTS else TeamPage.RESOURCES
-            pages += if (isEnterprise) TeamPage.APPLICANTS else TeamPage.JOIN_REQUESTS
-        } else {
-            pages += if (isEnterprise) TeamPage.MISSION else TeamPage.PLAN
-            pages += if (isEnterprise) TeamPage.TEAM else TeamPage.MEMBERS
-        }
-
-        fragments = pages.map { page ->
-            when (page) {
-                TeamPage.CHAT -> DiscussionListFragment()
-                TeamPage.PLAN, TeamPage.MISSION -> PlanFragment()
-                TeamPage.MEMBERS, TeamPage.TEAM -> JoinedMemberFragment().apply { setMemberChangeListener(memberChangeListener) }
-                TeamPage.TASKS -> TeamTaskFragment()
-                TeamPage.CALENDAR -> TeamCalendarFragment()
-                TeamPage.SURVEY -> SurveyFragment().apply {
-                    arguments = Bundle().apply {
-                        putBoolean("isTeam", true)
-                        putString("teamId", teamId)
-                    }
-                }
-                TeamPage.COURSES -> TeamCourseFragment()
-                TeamPage.FINANCES -> FinanceFragment()
-                TeamPage.REPORTS -> ReportsFragment()
-                TeamPage.RESOURCES, TeamPage.DOCUMENTS -> TeamResourceFragment().apply {
-                    MainApplication.listener = this
-                }
-                TeamPage.JOIN_REQUESTS,
-                TeamPage.APPLICANTS -> MembersFragment().apply { setMemberChangeListener(memberChangeListener) }
-            }.apply {
-                if (arguments == null) {
-                    arguments = Bundle().apply { putString("id", teamId) }
-                }
-            }
-        }
-    }
-
-    override fun getItemCount(): Int = fragments.size
+    override fun getItemCount(): Int = pages.size
 
     fun getPageTitle(position: Int): CharSequence =
-        fm.getString(pages[position].getTitleRes())
+        fm.getString(pages[position].titleRes)
 
-    fun TeamPage.getTitleRes(): Int =
-        R.string::class.java.getDeclaredField(this.name.lowercase()).getInt(null)
-
-    override fun getItemId(position: Int): Long =
-        pages[position].ordinal.toLong()
+    override fun getItemId(position: Int): Long = position.toLong()
 
     override fun containsItem(itemId: Long): Boolean =
-        pages.any { it.ordinal.toLong() == itemId }
+        itemId in 0 until pages.size
 
-    override fun createFragment(position: Int): Fragment =
-        fragments[position]
+    override fun createFragment(position: Int): Fragment {
+        val page = pages[position]
+        val fragment = page.createFragment()
+
+        when (page) {
+            TeamPage -> if (fragment is JoinedMemberFragment) {
+                fragment.setMemberChangeListener(memberChangeListener)
+            }
+            MembersPage -> if (fragment is JoinedMemberFragment) {
+                fragment.setMemberChangeListener(memberChangeListener)
+            }
+            ApplicantsPage, JoinRequestsPage -> if (fragment is MembersFragment) {
+                fragment.setMemberChangeListener(memberChangeListener)
+            }
+            DocumentsPage, ResourcesPage -> if (fragment is TeamResourceFragment) {
+                MainApplication.listener = fragment
+            }
+            SurveyPage -> {
+                fragment.arguments = (fragment.arguments ?: Bundle()).apply {
+                    putBoolean("isTeam", true)
+                    putString("teamId", teamId)
+                }
+            }
+            else -> {}
+        }
+
+        if (fragment.arguments == null) {
+            fragment.arguments = Bundle().apply { putString("id", teamId) }
+        } else if (!fragment.arguments!!.containsKey("id")) {
+            fragment.arguments!!.putString("id", teamId)
+        }
+
+        return fragment
+    }
 }
