318878f37713dd84d974b05f8c0b41279320f727
dogi <dogi@users.noreply.github.com>
2025-08-11 21:22:50 -0400

    resources: smoother collections realm closing (fixes #6710) (#6697)

--- app/src/main/java/org/ole/planet/myplanet/ui/resources/CollectionsFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/resources/CollectionsFragment.kt
@@ -130,6 +130,13 @@ class CollectionsFragment : DialogFragment(), TagExpandableAdapter.OnClickTagIte
         fragmentCollectionsBinding.btnOk.visibility = if (b) View.VISIBLE else View.GONE
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (::mRealm.isInitialized && !mRealm.isClosed) {
+            mRealm.close()
+        }
+    }
+
     companion object {
         private lateinit var recentList: MutableList<RealmTag>
         @JvmStatic
