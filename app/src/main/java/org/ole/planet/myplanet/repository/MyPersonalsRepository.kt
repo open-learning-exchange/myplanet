0dd8b4ed8347e19f1d4e59e50fe709acf2fb94f5
dogi <dogi@users.noreply.github.com>
2025-08-25 15:01:20 -0400

    Update MyPersonalsRepository.kt

--- app/src/main/java/org/ole/planet/myplanet/repository/MyPersonalsRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/MyPersonalsRepository.kt
@@ -2,10 +2,8 @@ package org.ole.planet.myplanet.repository
 
 import org.ole.planet.myplanet.model.RealmMyPersonal
 
-import org.ole.planet.myplanet.model.RealmMyPersonal
-
 interface MyPersonalsRepository {
-    fun getMyPersonals(userId: String): List<org.ole.planet.myplanet.model.RealmMyPersonal>
+    fun getMyPersonals(userId: String): List<RealmMyPersonal>
     fun deletePersonal(personalId: String)
-    fun updatePersonal(personal: org.ole.planet.myplanet.model.RealmMyPersonal)
+    fun updatePersonal(personal: RealmMyPersonal)
 }
