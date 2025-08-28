8a4ca98d7de3a80ebf2bd6acc4599d63c0a4a2a4
dogi <dogi@users.noreply.github.com>
2025-08-28 03:41:12 -0400

    all: smoother feedback detail view model (fixes #7133) (#7108)

--- app/src/main/java/org/ole/planet/myplanet/repository/FeedbackRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/FeedbackRepository.kt
@@ -1,5 +1,8 @@
 package org.ole.planet.myplanet.repository
 
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import io.realm.RealmChangeListener
 import io.realm.RealmResults
 import io.realm.Sort
@@ -13,6 +16,9 @@ import org.ole.planet.myplanet.model.RealmUserModel
 
 interface FeedbackRepository {
     fun getFeedback(userModel: RealmUserModel?): Flow<List<RealmFeedback>>
+    suspend fun getFeedbackById(id: String?): RealmFeedback?
+    suspend fun closeFeedback(id: String?)
+    suspend fun addReply(id: String?, obj: JsonObject)
 }
 
 class FeedbackRepositoryImpl @Inject constructor(
@@ -44,4 +50,34 @@ class FeedbackRepositoryImpl @Inject constructor(
                 }
             }
         }
+
+    override suspend fun getFeedbackById(id: String?): RealmFeedback? =
+        databaseService.withRealmAsync { realm ->
+            realm.where(RealmFeedback::class.java)
+                .equalTo("id", id)
+                .findFirst()
+                ?.let { realm.copyFromRealm(it) }
+        }
+
+    override suspend fun closeFeedback(id: String?) {
+        databaseService.executeTransactionAsync { realm ->
+            realm.where(RealmFeedback::class.java)
+                .equalTo("id", id)
+                .findFirst()?.status = "Closed"
+        }
+    }
+
+    override suspend fun addReply(id: String?, obj: JsonObject) {
+        databaseService.executeTransactionAsync { realm ->
+            val feedback = realm.where(RealmFeedback::class.java)
+                .equalTo("id", id)
+                .findFirst()
+            if (feedback != null) {
+                val con = Gson()
+                val msgArray = con.fromJson(feedback.messages, JsonArray::class.java)
+                msgArray.add(obj)
+                feedback.setMessages(msgArray)
+            }
+        }
+    }
 }
