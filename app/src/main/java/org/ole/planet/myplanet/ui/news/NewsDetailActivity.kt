c6d8e03ae0b85c75a163bcb1fbd68d0fd440e977
dogi <dogi@users.noreply.github.com>
2025-09-01 14:50:29 -0400

    Update NewsDetailActivity.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/news/NewsDetailActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/news/NewsDetailActivity.kt
@@ -18,11 +18,11 @@ import org.ole.planet.myplanet.model.RealmMyLibrary
 import org.ole.planet.myplanet.model.RealmNews
 import org.ole.planet.myplanet.model.RealmNewsLog
 import org.ole.planet.myplanet.service.UserProfileDbHandler
+import org.ole.planet.myplanet.utilities.EdgeToEdgeUtils
+import org.ole.planet.myplanet.utilities.FileUtils
 import org.ole.planet.myplanet.utilities.JsonUtils
 import org.ole.planet.myplanet.utilities.NetworkUtils
-import org.ole.planet.myplanet.utilities.FileUtils
 import org.ole.planet.myplanet.utilities.Utilities
-import org.ole.planet.myplanet.utilities.EdgeToEdgeUtils
 
 @AndroidEntryPoint
 class NewsDetailActivity : BaseActivity() {
