364150dad0a990f82bd62df7b1cb2d43da01f2e1
dogi <dogi@users.noreply.github.com>
2025-09-01 14:54:55 -0400

    Update ImageViewerActivity.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/viewer/ImageViewerActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/ImageViewerActivity.kt
@@ -10,8 +10,8 @@ import java.io.File
 import java.util.regex.Pattern
 import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.databinding.ActivityImageViewerBinding
-import org.ole.planet.myplanet.utilities.FileUtils
 import org.ole.planet.myplanet.utilities.EdgeToEdgeUtils
+import org.ole.planet.myplanet.utilities.FileUtils
 
 class ImageViewerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImageViewerBinding
