e35059b8bed9694af7dc4d36a301ee8270c69a38
dogi <dogi@users.noreply.github.com>
2025-08-31 23:29:20 -0400

    Update RealtimeLibraryFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/resources/RealtimeLibraryFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/resources/RealtimeLibraryFragment.kt
@@ -2,10 +2,10 @@ package org.ole.planet.myplanet.ui.resources
 
 import android.os.Bundle
 import android.view.View
-import org.ole.planet.myplanet.utilities.DiffUtils
 import org.ole.planet.myplanet.base.BaseRealtimeFragment
 import org.ole.planet.myplanet.callback.TableDataUpdate
 import org.ole.planet.myplanet.model.RealmMyLibrary
+import org.ole.planet.myplanet.utilities.DiffUtils
 import org.ole.planet.myplanet.utilities.Utilities
 
 abstract class RealtimeLibraryFragment : BaseRealtimeFragment<RealmMyLibrary>() {
