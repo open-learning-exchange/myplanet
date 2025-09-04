b9fb2204844d27bf8ed921496415935f5adc8fa2
dogi <dogi@users.noreply.github.com>
2025-09-04 09:28:30 -0400

    Refactor feedback repository to use queryListFlow

--- app/src/main/java/org/ole/planet/myplanet/repository/RealmRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/RealmRepository.kt
@@ -1,8 +1,13 @@
 package org.ole.planet.myplanet.repository
 
 import io.realm.Realm
+import io.realm.RealmChangeListener
 import io.realm.RealmObject
 import io.realm.RealmQuery
+import io.realm.RealmResults
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import org.ole.planet.myplanet.datamanager.DatabaseService
 import org.ole.planet.myplanet.datamanager.applyEqualTo
 import org.ole.planet.myplanet.datamanager.findCopyByField
@@ -12,9 +17,24 @@ open class RealmRepository(private val databaseService: DatabaseService) {
 
     protected suspend fun <T : RealmObject> queryList(
         clazz: Class<T>,
-        builder: RealmQuery<T>.() -> Unit = {}
+        builder: RealmQuery<T>.() -> Unit = {},
     ): List<T> = databaseService.withRealmAsync { realm ->
         realm.queryList(clazz, builder)
+    }
+
+    protected fun <T : RealmObject> queryListFlow(
+        clazz: Class<T>,
+        builder: RealmQuery<T>.() -> Unit = {},
+    ): Flow<List<T>> = callbackFlow {
+        withRealm { realm ->
+            val results = realm.where(clazz).apply(builder).findAllAsync()
+            val listener = RealmChangeListener<RealmResults<T>> {
+                trySend(realm.queryList(clazz, builder))
+            }
+            results.addChangeListener(listener)
+            trySend(realm.queryList(clazz, builder))
+            awaitClose { results.removeChangeListener(listener) }
+        }
     }
 
     protected suspend fun <T : RealmObject, V : Any> findByField(
