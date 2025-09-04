0b1380782a73131a1ed08bdf5aa664a540189946
dogi <dogi@users.noreply.github.com>
2025-09-04 10:28:06 -0400

    Fix queryListFlow to manage realm lifecycle

--- app/src/main/java/org/ole/planet/myplanet/repository/RealmRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/RealmRepository.kt
@@ -5,6 +5,7 @@ import io.realm.RealmChangeListener
 import io.realm.RealmObject
 import io.realm.RealmQuery
 import io.realm.RealmResults
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -25,16 +26,14 @@ open class RealmRepository(private val databaseService: DatabaseService) {
     protected fun <T : RealmObject> queryListFlow(
         clazz: Class<T>,
         builder: RealmQuery<T>.() -> Unit = {},
-    ): Flow<List<T>> = callbackFlow {
-        withRealm { realm ->
-            val results = realm.where(clazz).apply(builder).findAllAsync()
-            val listener = RealmChangeListener<RealmResults<T>> {
-                trySend(realm.queryList(clazz, builder))
-            }
-            results.addChangeListener(listener)
-            trySend(realm.queryList(clazz, builder))
-            awaitClose { results.removeChangeListener(listener) }
+    ): Flow<List<T>> = withRealmFlow { realm, scope ->
+        val results = realm.where(clazz).apply(builder).findAllAsync()
+        val listener = RealmChangeListener<RealmResults<T>> {
+            scope.trySend(realm.queryList(clazz, builder))
         }
+        results.addChangeListener(listener)
+        scope.trySend(realm.queryList(clazz, builder))
+        scope.awaitClose { results.removeChangeListener(listener) }
     }
 
     protected suspend fun <T : RealmObject, V : Any> findByField(
@@ -78,6 +77,12 @@ open class RealmRepository(private val databaseService: DatabaseService) {
 
     protected suspend fun <T> withRealm(operation: (Realm) -> T): T {
         return databaseService.withRealmAsync(operation)
+    }
+
+    protected fun <T> withRealmFlow(block: suspend (Realm, ProducerScope<T>) -> Unit): Flow<T> = callbackFlow {
+        val realm = Realm.getDefaultInstance()
+        block(realm, this)
+        awaitClose { realm.close() }
     }
 
     protected suspend fun executeTransaction(transaction: (Realm) -> Unit) {
