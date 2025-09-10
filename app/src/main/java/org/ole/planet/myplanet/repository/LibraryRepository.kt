fbd528794621d4b8475cf980809c148406851e05
dogi <dogi@users.noreply.github.com>
2025-09-10 05:58:58 -0400

    resources: smoother library repository lookup (fixes #7452) (#7435)

--- app/src/main/java/org/ole/planet/myplanet/repository/LibraryRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/LibraryRepository.kt
@@ -5,6 +5,8 @@ import org.ole.planet.myplanet.model.RealmMyLibrary
 interface LibraryRepository {
     suspend fun getAllLibraryItems(): List<RealmMyLibrary>
     suspend fun getLibraryItemById(id: String): RealmMyLibrary?
+    suspend fun getLibraryItemByResourceId(resourceId: String): RealmMyLibrary?
+    suspend fun getLibraryItemsByLocalAddress(localAddress: String): List<RealmMyLibrary>
     suspend fun getOfflineLibraryItems(): List<RealmMyLibrary>
     suspend fun getLibraryListForUser(userId: String?): List<RealmMyLibrary>
     suspend fun getAllLibraryList(): List<RealmMyLibrary>
