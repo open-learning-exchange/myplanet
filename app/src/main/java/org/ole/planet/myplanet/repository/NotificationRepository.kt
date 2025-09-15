89b8723f9ef16c9103b4ff3914043a75a9131e16
dogi <dogi@users.noreply.github.com>
2025-09-15 14:39:42 -0400

    all: smoother notification repository (fixes #7593) (#7556)

--- app/src/main/java/org/ole/planet/myplanet/repository/NotificationRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/NotificationRepository.kt
@@ -1,6 +1,11 @@
 package org.ole.planet.myplanet.repository
 
+import org.ole.planet.myplanet.model.RealmNotification
+
 interface NotificationRepository {
     suspend fun getUnreadCount(userId: String?): Int
     suspend fun updateResourceNotification(userId: String?)
+    suspend fun getNotifications(userId: String, filter: String): List<RealmNotification>
+    suspend fun markAsRead(notificationId: String)
+    suspend fun markAllAsRead(userId: String)
 }
