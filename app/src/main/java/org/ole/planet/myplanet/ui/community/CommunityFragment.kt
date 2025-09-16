3ea9135d47addabc33a7e531bc0db08e5e609d22
dogi <dogi@users.noreply.github.com>
2025-09-16 14:43:42 -0400

    Refresh community news after reply submissions

--- app/src/main/java/org/ole/planet/myplanet/ui/community/CommunityFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/community/CommunityFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.ole.planet.myplanet.base.BaseContainerFragment
 import org.ole.planet.myplanet.databinding.FragmentCommunityBinding
@@ -28,6 +29,7 @@ class CommunityFragment : BaseContainerFragment(), AdapterNews.OnNewsItemClickLi
     private var _binding: FragmentCommunityBinding? = null
     private val binding get() = _binding!!
     private var newList: List<RealmNews> = emptyList()
+    private var loadNewsJob: Job? = null
 
     @Inject
     lateinit var userProfileDbHandler: UserProfileDbHandler
@@ -37,7 +39,6 @@ class CommunityFragment : BaseContainerFragment(), AdapterNews.OnNewsItemClickLi
     override fun addImage(llImage: LinearLayout?) {}
     override fun onNewsItemClick(news: RealmNews?) {}
     override fun clearImages() {}
-    override fun onDataChanged() {}
 
     override fun showReply(news: RealmNews?, fromLogin: Boolean, nonTeamMember: Boolean) {
         if (news != null) {
@@ -59,21 +60,17 @@ class CommunityFragment : BaseContainerFragment(), AdapterNews.OnNewsItemClickLi
         }
         val orientation = resources.configuration.orientation
         changeLayoutManager(orientation)
-        val planetCode = user?.planetCode
-        if (planetCode != null) {
-            viewLifecycleOwner.lifecycleScope.launch {
-                newList = newsRepository.getCommunityNews(planetCode)
-                updatedNewsList(newList)
-            }
-        } else {
-            updatedNewsList(emptyList())
-        }
+        loadCommunityNews()
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         val orientation = newConfig.orientation
         changeLayoutManager(orientation)
+    }
+
+    override fun onDataChanged() {
+        loadCommunityNews()
     }
 
     private fun changeLayoutManager(orientation: Int) {
@@ -84,10 +81,20 @@ class CommunityFragment : BaseContainerFragment(), AdapterNews.OnNewsItemClickLi
         }
     }
 
+    private fun loadCommunityNews() {
+        val planetCode = user?.planetCode
+        loadNewsJob?.cancel()
+        loadNewsJob = viewLifecycleOwner.lifecycleScope.launch {
+            newList = planetCode?.let { newsRepository.getCommunityNews(it) } ?: emptyList()
+            updatedNewsList(newList)
+        }
+    }
+
     private fun updatedNewsList(updatedList: List<RealmNews>) {
         viewLifecycleOwner.lifecycleScope.launch {
             val updatedListAsMutable: MutableList<RealmNews?> = updatedList.toMutableList()
             val adapter = activity?.let { AdapterNews(it, user, null, "", null, userProfileDbHandler) }
+            adapter?.setmRealm(mRealm)
             adapter?.setListener(this@CommunityFragment)
             adapter?.setFromLogin(requireArguments().getBoolean("fromLogin", false))
             adapter?.updateList(updatedListAsMutable)
@@ -98,6 +105,7 @@ class CommunityFragment : BaseContainerFragment(), AdapterNews.OnNewsItemClickLi
 
     override fun onDestroyView() {
         super.onDestroyView()
+        loadNewsJob?.cancel()
         _binding = null
     }
 }
