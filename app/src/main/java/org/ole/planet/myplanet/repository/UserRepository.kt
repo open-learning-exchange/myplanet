23b02c097537d1bfac18c710efca9bfda76dc24a
dogi <dogi@users.noreply.github.com>
2025-09-22 05:21:20 -0400

    Remove unused current user repository method

--- app/src/main/java/org/ole/planet/myplanet/repository/UserRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/UserRepository.kt
@@ -3,7 +3,6 @@ package org.ole.planet.myplanet.repository
 import org.ole.planet.myplanet.model.RealmUserModel
 
 interface UserRepository {
-    suspend fun getCurrentUser(): RealmUserModel?
     suspend fun getUserById(userId: String): RealmUserModel?
     suspend fun getUserByName(username: String): RealmUserModel?
     suspend fun getAllUsers(): List<RealmUserModel>
