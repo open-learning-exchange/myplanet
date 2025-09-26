e3ec09a4bf5e723cac9619046d2f9f0b9db08fe6
dogi <dogi@users.noreply.github.com>
2025-09-26 15:47:49 -0400

    Refactor chat detail fragment to use repositories

--- app/src/main/java/org/ole/planet/myplanet/repository/ChatRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/ChatRepository.kt
@@ -1,7 +1,16 @@
 package org.ole.planet.myplanet.repository
 
+import com.google.gson.JsonObject
 import org.ole.planet.myplanet.model.RealmChatHistory
 
 interface ChatRepository {
     suspend fun getChatHistoryForUser(userName: String?): List<RealmChatHistory>
+    suspend fun getLatestRevision(chatId: String): String?
+    suspend fun insertChatHistory(chatHistory: JsonObject?)
+    suspend fun appendConversationToHistory(
+        chatId: String,
+        query: String?,
+        response: String?,
+        newRev: String?,
+    )
 }
--- app/src/main/java/org/ole/planet/myplanet/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/ChatRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package org.ole.planet.myplanet.repository
 
+import com.google.gson.JsonObject
 import io.realm.Sort
 import javax.inject.Inject
 import org.ole.planet.myplanet.datamanager.DatabaseService
 import org.ole.planet.myplanet.model.RealmChatHistory
+import org.ole.planet.myplanet.model.RealmChatHistory.Companion.addConversationToChatHistory
 
 class ChatRepositoryImpl @Inject constructor(
     databaseService: DatabaseService,
@@ -16,6 +18,40 @@ class ChatRepositoryImpl @Inject constructor(
         return queryList(RealmChatHistory::class.java) {
             equalTo("user", userName)
             sort("id", Sort.DESCENDING)
+        }
+    }
+
+    override suspend fun getLatestRevision(chatId: String): String? {
+        if (chatId.isEmpty()) {
+            return null
+        }
+        return queryList(RealmChatHistory::class.java) {
+            equalTo("_id", chatId)
+        }.maxByOrNull { rev ->
+            rev._rev?.substringBefore("-")?.toIntOrNull() ?: 0
+        }?._rev
+    }
+
+    override suspend fun insertChatHistory(chatHistory: JsonObject?) {
+        if (chatHistory == null) {
+            return
+        }
+        executeTransaction { realm ->
+            RealmChatHistory.insert(realm, chatHistory)
+        }
+    }
+
+    override suspend fun appendConversationToHistory(
+        chatId: String,
+        query: String?,
+        response: String?,
+        newRev: String?,
+    ) {
+        if (chatId.isEmpty()) {
+            return
+        }
+        executeTransaction { realm ->
+            addConversationToChatHistory(realm, chatId, query, response, newRev)
         }
     }
 }
