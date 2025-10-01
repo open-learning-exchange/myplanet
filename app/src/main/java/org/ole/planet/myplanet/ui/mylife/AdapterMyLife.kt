50ee7f12c4746a522539c50bf34393dacebf4ea1
dogi <dogi@users.noreply.github.com>
2025-10-01 17:20:25 -0400

    Remove My Messages entry from My Life dashboard

--- app/src/main/java/org/ole/planet/myplanet/ui/mylife/AdapterMyLife.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/mylife/AdapterMyLife.kt
@@ -2,18 +2,13 @@ package org.ole.planet.myplanet.ui.mylife
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.ContextWrapper
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
 import kotlinx.coroutines.Dispatchers
@@ -23,19 +18,9 @@ import org.ole.planet.myplanet.R
 import org.ole.planet.myplanet.model.RealmMyLife
 import org.ole.planet.myplanet.model.RealmMyLife.Companion.updateVisibility
 import org.ole.planet.myplanet.model.RealmMyLife.Companion.updateWeight
-import org.ole.planet.myplanet.ui.calendar.CalendarFragment
-import org.ole.planet.myplanet.ui.helpwanted.HelpWantedFragment
-import org.ole.planet.myplanet.ui.myPersonals.MyPersonalsFragment
-import org.ole.planet.myplanet.ui.myhealth.MyHealthFragment
 import org.ole.planet.myplanet.ui.mylife.helper.ItemTouchHelperAdapter
 import org.ole.planet.myplanet.ui.mylife.helper.ItemTouchHelperViewHolder
 import org.ole.planet.myplanet.ui.mylife.helper.OnStartDragListener
-import org.ole.planet.myplanet.ui.navigation.NavigationHelper
-import org.ole.planet.myplanet.ui.news.NewsFragment
-import org.ole.planet.myplanet.ui.references.ReferenceFragment
-import org.ole.planet.myplanet.ui.submission.MySubmissionFragment
-import org.ole.planet.myplanet.ui.submission.MySubmissionFragment.Companion.newInstance
-import org.ole.planet.myplanet.ui.userprofile.AchievementFragment
 import org.ole.planet.myplanet.utilities.Utilities
 
 class AdapterMyLife(private val context: Context, private val myLifeList: List<RealmMyLife>, private var mRealm: Realm, private val mDragStartListener: OnStartDragListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), ItemTouchHelperAdapter {
@@ -50,15 +35,6 @@ class AdapterMyLife(private val context: Context, private val myLifeList: List<R
     @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is ViewHolderMyLife) {
-            holder.title.text = myLifeList[position].title
-            holder.imageView.setImageResource(context.resources.getIdentifier(myLifeList[position].imageId, "drawable", context.packageName))
-            holder.imageView.contentDescription = context.getString(R.string.icon, myLifeList[position].title)
-            val fragment = findFragment(myLifeList[position].imageId)
-            if (fragment != null) {
-                holder.imageView.setOnClickListener { view: View ->
-                    transactionFragment(fragment, view)
-                }
-            }
             holder.dragImageButton.setOnTouchListener { _: View?, event: MotionEvent ->
                 holder.dragImageButton.contentDescription = context.getString(R.string.drag, myLifeList[position].title)
                 if (event.actionMasked == MotionEvent.ACTION_DOWN) {
@@ -117,8 +93,6 @@ class AdapterMyLife(private val context: Context, private val myLifeList: List<R
 
     internal inner class ViewHolderMyLife(itemView: View) : RecyclerView.ViewHolder(itemView),
         ItemTouchHelperViewHolder {
-        var title: TextView = itemView.findViewById(R.id.titleTextView)
-        var imageView: ImageView = itemView.findViewById(R.id.itemImageView)
         var dragImageButton: ImageButton = itemView.findViewById(R.id.drag_image_button)
         var visibility: ImageButton = itemView.findViewById(R.id.visibility_image_button)
         var rvItemContainer: LinearLayout = itemView.findViewById(R.id.rv_item_parent_layout)
@@ -137,42 +111,4 @@ class AdapterMyLife(private val context: Context, private val myLifeList: List<R
         }
     }
 
-    companion object {
-        fun findFragment(frag: String?): Fragment? {
-            when (frag) {
-                "ic_mypersonals" -> return MyPersonalsFragment()
-                "ic_news" -> return NewsFragment()
-                "ic_submissions" -> return MySubmissionFragment()
-                "ic_my_survey" -> return newInstance("survey")
-                "ic_myhealth" -> return MyHealthFragment()
-                "ic_calendar" -> return CalendarFragment()
-                "ic_help_wanted" -> return HelpWantedFragment()
-                "ic_references" -> return ReferenceFragment()
-                "my_achievement" -> return AchievementFragment()
-            }
-            return null
-        }
-
-        fun transactionFragment(f: Fragment?, view: View) {
-            var context = view.context
-            while (context is ContextWrapper) {
-                if (context is AppCompatActivity) {
-                    break
-                }
-                context = context.baseContext
-            }
-            
-            val activity = context as? AppCompatActivity
-            activity?.let { act ->
-                f?.let {
-                    NavigationHelper.replaceFragment(
-                        act.supportFragmentManager,
-                        R.id.fragment_container,
-                        it,
-                        addToBackStack = true
-                    )
-                }
-            }
-        }
-    }
 }
