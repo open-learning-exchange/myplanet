c1eb2533b4fca8fea52c859355e6dc46bbef7477
dogi <dogi@users.noreply.github.com>
2025-10-01 15:04:43 -0400

    Import coroutine launch extension

--- app/src/main/java/org/ole/planet/myplanet/ui/courses/CourseStepFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/courses/CourseStepFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 import org.ole.planet.myplanet.MainApplication
