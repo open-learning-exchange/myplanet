c925613d0c07b9734e7d030542469631d5db121c
dogi <dogi@users.noreply.github.com>
2025-10-06 11:32:06 -0400

    Use repository for community-visible news

--- app/src/main/java/org/ole/planet/myplanet/repository/NewsRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/NewsRepository.kt
@@ -4,4 +4,5 @@ import org.ole.planet.myplanet.model.RealmNews
 
 interface NewsRepository {
     suspend fun getNewsWithReplies(newsId: String): Pair<RealmNews?, List<RealmNews>>
+    suspend fun getCommunityVisibleNews(userIdentifier: String): List<RealmNews>
 }
