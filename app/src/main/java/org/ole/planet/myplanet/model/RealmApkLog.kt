b7c57ebf4d261656f3fec76b46e7e28fbade2c4a
dogi <dogi@users.noreply.github.com>
2025-10-09 14:39:44 -0400

    chore: remove unused helper functions

--- app/src/main/java/org/ole/planet/myplanet/model/RealmApkLog.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/RealmApkLog.kt
@@ -20,14 +20,6 @@ open class RealmApkLog : RealmObject() {
     var createdOn: String? = null
     var time: String? = null
 
-    fun setError(e: Throwable) {
-        error += "--------- Stack trace ---------\n\n"
-        appendReport(e)
-        error += "--------- Cause ---------\n\n"
-        val cause = e.cause
-        appendReport(cause)
-    }
-
     private fun appendReport(cause: Throwable?) {
         if (cause != null) {
             error += """
