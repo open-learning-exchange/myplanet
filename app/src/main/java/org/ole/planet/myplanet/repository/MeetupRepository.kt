dd33be79b33d0a790788c27be3c1509c002cb9f4
dogi <dogi@users.noreply.github.com>
2025-10-20 15:40:39 -0400

    Refactor meetup detail to use repository

--- app/src/main/java/org/ole/planet/myplanet/repository/MeetupRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/MeetupRepository.kt
@@ -1,7 +1,11 @@
 package org.ole.planet.myplanet.repository
 
 import org.ole.planet.myplanet.model.RealmMeetup
+import org.ole.planet.myplanet.model.RealmUserModel
 
 interface MeetupRepository {
     suspend fun getMeetupsForTeam(teamId: String): List<RealmMeetup>
+    suspend fun getMeetupById(meetupId: String): RealmMeetup?
+    suspend fun getJoinedMembers(meetupId: String): List<RealmUserModel>
+    suspend fun toggleAttendance(meetupId: String, currentUserId: String?): RealmMeetup?
 }
