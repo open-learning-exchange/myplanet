0ea270f352ca7aa79c5daafc4bebca6d74b79379
dogi <dogi@users.noreply.github.com>
2025-10-22 12:54:24 -0400

    Refactor chat detail realm access into repository

--- app/src/main/java/org/ole/planet/myplanet/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/ChatRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.ole.planet.myplanet.repository
 
+import com.google.gson.JsonObject
 import io.realm.Case
 import io.realm.Sort
 import javax.inject.Inject
@@ -28,6 +29,42 @@ class ChatRepositoryImpl @Inject constructor(
         return queryList(RealmNews::class.java) {
             equalTo("docType", "message", Case.INSENSITIVE)
             equalTo("createdOn", planetCode, Case.INSENSITIVE)
+        }
+    }
+
+    override suspend fun getLatestRevision(chatId: String?): String? {
+        if (chatId.isNullOrEmpty()) {
+            return null
+        }
+        return withRealmAsync { realm ->
+            realm.refresh()
+            realm.where(RealmChatHistory::class.java)
+                .equalTo("_id", chatId)
+                .findAll()
+                .maxByOrNull { chatHistory ->
+                    chatHistory._rev?.substringBefore("-")?.toIntOrNull() ?: 0
+                }
+                ?._rev
+        }
+    }
+
+    override suspend fun saveChatHistory(chatHistory: JsonObject) {
+        executeTransaction { realm ->
+            RealmChatHistory.insert(realm, chatHistory)
+        }
+    }
+
+    override suspend fun appendConversation(
+        chatId: String?,
+        query: String?,
+        response: String?,
+        newRev: String?,
+    ) {
+        if (chatId.isNullOrEmpty()) {
+            return
+        }
+        executeTransaction { realm ->
+            RealmChatHistory.addConversationToChatHistory(realm, chatId, query, response, newRev)
         }
     }
 }
