84e2a3c873f247c8c5817adaf212ade9c65fa9c8
dogi <dogi@users.noreply.github.com>
2025-10-23 08:30:27 -0400

    Remove unused companion object from RealmSearchActivity

--- app/src/main/java/org/ole/planet/myplanet/model/RealmSearchActivity.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/RealmSearchActivity.kt
@@ -37,18 +37,4 @@ open class RealmSearchActivity(
         obj.add("filter", Gson().fromJson(filter, JsonObject::class.java))
         return obj
     }
-
-    companion object {
-        @JvmStatic
-        fun insert(log: RealmNewsLog): JsonObject {
-            val ob = JsonObject()
-            ob.addProperty("user", log.userId)
-            ob.addProperty("type", log.type)
-            ob.addProperty("time", log.time)
-            ob.addProperty("androidId", NetworkUtils.getUniqueIdentifier())
-            ob.addProperty("deviceName", NetworkUtils.getDeviceName())
-            ob.addProperty("customDeviceName", NetworkUtils.getCustomDeviceName(MainApplication.context))
-            return ob
-        }
-    }
 }
