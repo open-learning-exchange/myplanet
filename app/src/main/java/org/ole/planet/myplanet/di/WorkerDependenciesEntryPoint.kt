a10d93d0406677a7e37caf7b666df2ca030f2cc3
dogi <dogi@users.noreply.github.com>
2025-10-23 20:34:27 -0400

    teams: smoother repository task notifying (fixes #8661) (#8637)

--- app/src/main/java/org/ole/planet/myplanet/di/WorkerDependenciesEntryPoint.kt
+++ app/src/main/java/org/ole/planet/myplanet/di/WorkerDependenciesEntryPoint.kt
@@ -4,6 +4,7 @@ import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.ole.planet.myplanet.datamanager.DatabaseService
+import org.ole.planet.myplanet.repository.TeamRepository
 import org.ole.planet.myplanet.repository.SubmissionRepository
 import org.ole.planet.myplanet.service.UploadManager
 import org.ole.planet.myplanet.service.UserProfileDbHandler
@@ -14,5 +15,6 @@ interface WorkerDependenciesEntryPoint {
     fun databaseService(): DatabaseService
     fun userProfileDbHandler(): UserProfileDbHandler
     fun uploadManager(): UploadManager
+    fun teamRepository(): TeamRepository
     fun submissionRepository(): SubmissionRepository
 }
