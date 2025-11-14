a6ad48566eb095df03ba800c6edb7e3830f8c4a2
dogi <dogi@users.noreply.github.com>
2025-11-13 19:30:32 -0500

    teams: smoother news repository realm querying (fixes #9150) (#9109)

--- app/src/main/java/org/ole/planet/myplanet/repository/NewsRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/NewsRepository.kt
@@ -1,10 +1,12 @@
 package org.ole.planet.myplanet.repository
 
 import java.util.HashMap
+import kotlinx.coroutines.flow.Flow
 import org.ole.planet.myplanet.model.RealmNews
 import org.ole.planet.myplanet.model.RealmUserModel
 
 interface NewsRepository {
+    fun getCommunityNews(userIdentifier: String): Flow<List<RealmNews>>
     suspend fun getNewsWithReplies(newsId: String): Pair<RealmNews?, List<RealmNews>>
     suspend fun getCommunityVisibleNews(userIdentifier: String): List<RealmNews>
     suspend fun createNews(map: HashMap<String?, String>, user: RealmUserModel?): RealmNews
