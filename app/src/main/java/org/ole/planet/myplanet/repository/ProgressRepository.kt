059c9588c1c2032f1a7d8ffc9af2792f71765a73
dogi <dogi@users.noreply.github.com>
2025-11-24 06:55:23 -0500

    Update ProgressRepository.kt

--- app/src/main/java/org/ole/planet/myplanet/repository/ProgressRepository.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/ProgressRepository.kt
@@ -1,16 +1,7 @@
 package org.ole.planet.myplanet.repository
 
 import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import io.realm.Realm
-import org.ole.planet.myplanet.model.RealmSubmission
 
 interface ProgressRepository {
     suspend fun fetchCourseData(userId: String?): JsonArray
-    private fun submissionMap(
-        submissions: List<RealmSubmission>,
-        realm: Realm,
-        examIds: List<String>,
-        obj: JsonObject
-    )
 }
