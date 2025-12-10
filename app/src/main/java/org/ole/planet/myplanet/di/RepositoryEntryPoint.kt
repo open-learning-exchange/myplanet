7fac54d706dc3295ccf2e5efec7a8bdc0606b5fa
dogi <dogi@users.noreply.github.com>
2025-12-10 14:54:42 -0500

    all: less planet repository is more (fixes #9823) (#9813)

--- app/src/main/java/org/ole/planet/myplanet/di/RepositoryEntryPoint.kt
+++ app/src/main/java/org/ole/planet/myplanet/di/RepositoryEntryPoint.kt
@@ -3,12 +3,10 @@ package org.ole.planet.myplanet.di
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.ole.planet.myplanet.repository.PlanetRepository
 import org.ole.planet.myplanet.repository.UserRepository
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
 interface RepositoryEntryPoint {
     fun userRepository(): UserRepository
-    fun planetRepository(): PlanetRepository
 }
