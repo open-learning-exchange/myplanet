ce762aef7f22b6a7d3e8eae0d570927c3e4de576
dogi <dogi@users.noreply.github.com>
2025-12-11 11:04:40 -0500

    courses: smoother collection diffing (fixes #9842) (#9826)

--- app/src/main/java/org/ole/planet/myplanet/ui/resources/TagData.kt
+++
@@ -1,8 +0,0 @@
-package org.ole.planet.myplanet.ui.resources
-
-import org.ole.planet.myplanet.model.RealmTag
-
-sealed class TagData {
-    data class Parent(val tag: RealmTag, var isExpanded: Boolean = false) : TagData()
-    data class Child(val tag: RealmTag) : TagData()
-}
