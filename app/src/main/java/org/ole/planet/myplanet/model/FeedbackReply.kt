3810c93d8c6891904e32da77ac641192617f3adc
dogi <dogi@users.noreply.github.com>
2025-12-22 01:53:25 -0500

    all: smoother feedback list diffing (fixes #10037) (#10009)

--- app/src/main/java/org/ole/planet/myplanet/model/FeedbackReply.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/FeedbackReply.kt
@@ -1,3 +1,3 @@
 package org.ole.planet.myplanet.model
 
-class FeedbackReply(var message: String, var user: String, var date: String)
+data class FeedbackReply(var message: String, var user: String, var date: String)
