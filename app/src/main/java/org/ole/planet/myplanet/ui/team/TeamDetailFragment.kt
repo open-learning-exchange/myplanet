32c16a156241e35edf84afbca40776bd1f69805d
dogi <dogi@users.noreply.github.com>
2025-12-23 13:14:38 -0500

    Update TeamDetailFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/team/TeamDetailFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/team/TeamDetailFragment.kt
@@ -27,7 +27,6 @@ import org.ole.planet.myplanet.databinding.FragmentTeamDetailBinding
 import org.ole.planet.myplanet.model.RealmMyTeam.Companion.getJoinedMemberCount
 import org.ole.planet.myplanet.model.RealmNews
 import org.ole.planet.myplanet.model.RealmUserModel
-import org.ole.planet.myplanet.repository.TeamsRepository
 import org.ole.planet.myplanet.service.SyncManager
 import org.ole.planet.myplanet.service.UserProfileDbHandler
 import org.ole.planet.myplanet.service.sync.RealtimeSyncCoordinator
