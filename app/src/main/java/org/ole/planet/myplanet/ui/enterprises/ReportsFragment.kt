e7a6ed32446e6a459d8a4052f1b707c1ca93cc9b
dogi <dogi@users.noreply.github.com>
2025-12-23 13:09:04 -0500

    Update ReportsFragment.kt

--- app/src/main/java/org/ole/planet/myplanet/ui/enterprises/ReportsFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/enterprises/ReportsFragment.kt
@@ -31,7 +31,6 @@ import org.ole.planet.myplanet.databinding.DialogAddReportBinding
 import org.ole.planet.myplanet.databinding.FragmentReportsBinding
 import org.ole.planet.myplanet.model.RealmMyTeam
 import org.ole.planet.myplanet.model.RealmNews
-import org.ole.planet.myplanet.repository.TeamsRepository
 import org.ole.planet.myplanet.ui.team.BaseTeamFragment
 import org.ole.planet.myplanet.utilities.SharedPrefManager
 import org.ole.planet.myplanet.utilities.Utilities
