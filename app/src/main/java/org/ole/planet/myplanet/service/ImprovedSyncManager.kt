270d6cc1cdf65a8f9815d29a769cd87003af8843
dogi <dogi@users.noreply.github.com>
2025-12-30 13:43:20 -0500

    sync: smoother login managing (fixes #10233) (#10214)

--- app/src/main/java/org/ole/planet/myplanet/service/ImprovedSyncManager.kt
+++ app/src/main/java/org/ole/planet/myplanet/service/ImprovedSyncManager.kt
@@ -18,7 +18,7 @@ import kotlinx.coroutines.launch
 import org.ole.planet.myplanet.MainApplication.Companion.createLog
 import org.ole.planet.myplanet.callback.SyncListener
 import org.ole.planet.myplanet.data.DatabaseService
-import org.ole.planet.myplanet.data.ManagerSync
+import org.ole.planet.myplanet.data.LoginSyncManager
 import org.ole.planet.myplanet.di.AppPreferences
 import org.ole.planet.myplanet.service.sync.AdaptiveBatchProcessor
 import org.ole.planet.myplanet.service.sync.RealmPoolManager
@@ -126,7 +126,7 @@ class ImprovedSyncManager @Inject constructor(
 
         // Post-sync operations
         logger.startProcess("admin_sync")
-        ManagerSync.instance.syncAdmin()
+        LoginSyncManager.instance.syncAdmin()
         logger.endProcess("admin_sync")
 
         poolManager.useRealm { realm ->
