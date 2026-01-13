578ca0ad8fea2710578b33ad735b8006c111ea8b
Gideon Okuro <gideonollonde@gmail.com>
2026-01-13 14:46:07 +0300

    sync: smoother configuration managing (fixes #10530) (#10531)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/data/ConfigurationManager.kt
+++ app/src/main/java/org/ole/planet/myplanet/data/ConfigurationManager.kt
@@ -68,7 +68,9 @@ class ConfigurationManager(
                 when (result) {
                     is UrlCheckResult.Success -> {
                         val isAlternativeUrl = result.url != url
-                        listener?.onConfigurationIdReceived(result.id, result.code, result.url, url, isAlternativeUrl, callerActivity)
+                        withContext(Dispatchers.Main) {
+                            listener?.onConfigurationIdReceived(result.id, result.code, result.url, url, isAlternativeUrl, callerActivity)
+                        }
                         activity.syncFailed = false
                     }
                     is UrlCheckResult.Failure -> {
@@ -78,7 +80,9 @@ class ConfigurationManager(
                             context.getString(R.string.https_protocol) -> context.getString(R.string.device_couldn_t_reach_nation_server)
                             else -> context.getString(R.string.device_couldn_t_reach_local_server)
                         }
-                        showAlertDialog(errorMessage, false)
+                        withContext(Dispatchers.Main) {
+                            showAlertDialog(errorMessage, false)
+                        }
                     }
                 }
             } catch (e: Exception) {
