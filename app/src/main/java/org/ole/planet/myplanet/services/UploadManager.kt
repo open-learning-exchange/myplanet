6e9d01374efd0e4422e50c44c723999d50339b91
Gideon Okuro <gideonollonde@gmail.com>
2026-01-26 17:51:04 +0300

    sync: smoother learner joining (fixes #10014) (#10883)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/services/UploadManager.kt
+++ app/src/main/java/org/ole/planet/myplanet/services/UploadManager.kt
@@ -504,7 +504,7 @@ class UploadManager @Inject constructor(
                     } else {
                         val copiedActivity = realm.copyFromRealm(activity)
                         ActivityData(
-                            activityId = copiedActivity._id,
+                            activityId = copiedActivity.id,
                             userId = copiedActivity.userId,
                             serialized = RealmOfflineActivity.serializeLoginActivities(copiedActivity, context)
                         )
@@ -521,7 +521,7 @@ class UploadManager @Inject constructor(
 
                         databaseService.executeTransactionAsync { transactionRealm ->
                             transactionRealm.where(RealmOfflineActivity::class.java)
-                                .equalTo("_id", activityData.activityId)
+                                .equalTo("id", activityData.activityId)
                                 .findFirst()?.changeRev(`object`)
                         }
                     } catch (e: IOException) {
