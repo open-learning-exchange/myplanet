690c8b5a1984cc0b08c7771fc836734eb6b81ba7
dogi <dogi@users.noreply.github.com>
2026-03-03 23:03:03 -0500

    Delete revert.sh

--- revert.sh
+++
@@ -1,2 +0,0 @@
-git checkout -- app/src/main/java/org/ole/planet/myplanet/repository/TeamsRepository.kt app/src/main/java/org/ole/planet/myplanet/repository/TeamsRepositoryImpl.kt app/src/main/java/org/ole/planet/myplanet/ui/teams/TeamViewModel.kt
-rm app/src/main/java/org/ole/planet/myplanet/model/CreateTeamRequest.kt
