3f7dc62537bd76f471996f2f98239a96ffca8c3d
dogi <dogi@users.noreply.github.com>
2026-03-03 22:56:27 -0500

    resources: less realm array list is more (fixes #11703) (#11672)

--- app/src/main/java/org/ole/planet/myplanet/model/RealmMyLibrary.kt
+++ app/src/main/java/org/ole/planet/myplanet/model/RealmMyLibrary.kt
@@ -339,12 +339,6 @@ open class RealmMyLibrary : RealmObject() {
             }
             return list
         }
-
-        @Deprecated("Use ResourcesRepository.getFilterFacets instead")
-        @JvmStatic
-        fun getArrayList(libraries: List<RealmMyLibrary>, type: String): Set<String?> {
-            return libraries.mapNotNull { if (type == "mediums") it.mediaType else it.language }.filterNot { it.isBlank() }.toSet()
-        }
     }
 }
 
