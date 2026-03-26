2391c6fcc66570bc91dd67e969345c9b65e7933e
dogi <dogi@users.noreply.github.com>
2026-03-26 05:10:19 -0400

    life: smoother url utils health access testing (fixes #12428) (#12356)

--- app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
@@ -3,10 +3,51 @@ package org.ole.planet.myplanet.utils
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.ole.planet.myplanet.services.SharedPrefManager
 
 class UrlUtilsTest {
+    private lateinit var sharedPrefManager: SharedPrefManager
+
+    @Before
+    fun setup() {
+        sharedPrefManager = mockk(relaxed = true)
+        every { sharedPrefManager.isAlternativeUrl() } returns false
+        every { sharedPrefManager.getCouchdbUrl() } returns "http://example.com"
+        every { sharedPrefManager.getProcessedAlternativeUrl() } returns "http://alternative.com"
+    }
+
+    @Test
+    fun testGetHealthAccessUrl_withEmptyPin() {
+        every { sharedPrefManager.getServerPin() } returns ""
+        val url = UrlUtils.getHealthAccessUrl(sharedPrefManager)
+        assertEquals("http://example.com/healthaccess?p=0000", url)
+    }
+
+    @Test
+    fun testGetHealthAccessUrl_withCustomPin() {
+        every { sharedPrefManager.getServerPin() } returns "1234"
+        val url = UrlUtils.getHealthAccessUrl(sharedPrefManager)
+        assertEquals("http://example.com/healthaccess?p=1234", url)
+    }
+
+    @Test
+    fun testGetHealthAccessUrl_withAlternativeUrl() {
+        every { sharedPrefManager.isAlternativeUrl() } returns true
+        every { sharedPrefManager.getServerPin() } returns "5678"
+        val url = UrlUtils.getHealthAccessUrl(sharedPrefManager)
+        assertEquals("http://alternative.com/healthaccess?p=5678", url)
+    }
+
+    @Test
+    fun testGetHealthAccessUrl_withDbSuffix() {
+        every { sharedPrefManager.getCouchdbUrl() } returns "http://example.com/db"
+        every { sharedPrefManager.getServerPin() } returns "4321"
+        val url = UrlUtils.getHealthAccessUrl(sharedPrefManager)
+        assertEquals("http://example.com/healthaccess?p=4321", url)
+    }
+
     @Test
     fun `getApkVersionUrl uses alternative url`() {
         val mockSpm = mockk<SharedPrefManager>()
@@ -91,7 +132,6 @@ class UrlUtilsTest {
         val spm = mockk<SharedPrefManager>()
         every { spm.isAlternativeUrl() } returns true
         every { spm.getProcessedAlternativeUrl() } returns "http://alt.example.com"
-
         val expected = "http://alt.example.com/db"
         val result = UrlUtils.dbUrl(spm)
         assertEquals(expected, result)
@@ -102,7 +142,6 @@ class UrlUtilsTest {
         val spm = mockk<SharedPrefManager>()
         every { spm.isAlternativeUrl() } returns false
         every { spm.getCouchdbUrl() } returns "http://couch.example.com"
-
         val expected = "http://couch.example.com/db"
         val result = UrlUtils.dbUrl(spm)
         assertEquals(expected, result)
