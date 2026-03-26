d791c62457609c10d3a5f1be25776bbf06e145f7
dogi <dogi@users.noreply.github.com>
2026-03-26 09:20:24 -0400

    all: smoother url utils user image testing (fixes #12430) (#12353)
    Co-authored-by: Gideon Okuro <gideonollonde@gmail.com>

--- app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
@@ -2,6 +2,9 @@ package org.ole.planet.myplanet.utils
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -12,10 +15,62 @@ class UrlUtilsTest {
 
     @Before
     fun setup() {
+        mockkObject(UrlUtils)
         sharedPrefManager = mockk(relaxed = true)
         every { sharedPrefManager.isAlternativeUrl() } returns false
         every { sharedPrefManager.getCouchdbUrl() } returns "http://example.com"
         every { sharedPrefManager.getProcessedAlternativeUrl() } returns "http://alternative.com"
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testGetUserImageUrlReturnsCorrectFormattedString() {
+        val mockBaseUrl = "http://mockurl.com/db"
+        every { UrlUtils.getUrl() } returns mockBaseUrl
+
+        val userId = "user123"
+        val imageName = "profile.jpg"
+
+        val result = UrlUtils.getUserImageUrl(userId, imageName)
+
+        assertEquals("$mockBaseUrl/_users/$userId/$imageName", result)
+    }
+
+    @Test
+    fun testGetUserImageUrlWithNullUserId() {
+        val userId: String? = null
+        val imageName = "profile.jpg"
+
+        val result = UrlUtils.getUserImageUrl(userId, imageName)
+
+        assertEquals(null, result)
+    }
+
+    @Test
+    fun testGetUserImageUrlWithEmptyStrings() {
+        val userId = ""
+        val imageName = ""
+
+        val result = UrlUtils.getUserImageUrl(userId, imageName)
+
+        assertEquals(null, result)
+    }
+
+    @Test
+    fun testGetUserImageUrlWithSpecialCharacters() {
+        val mockBaseUrl = "http://mockurl.com/db"
+        every { UrlUtils.getUrl() } returns mockBaseUrl
+
+        val userId = "user@123"
+        val imageName = "my image (1).jpg"
+
+        val result = UrlUtils.getUserImageUrl(userId, imageName)
+
+        assertEquals("$mockBaseUrl/_users/user%40123/my%20image%20%281%29.jpg", result)
     }
 
     @Test
