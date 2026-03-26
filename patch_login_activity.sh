f99588f1ce266e0f33186a2ab5b5fd7ad3510b63
dogi <dogi@users.noreply.github.com>
2026-03-26 11:01:53 -0400

    Delete patch_login_activity.sh

--- patch_login_activity.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/users = fetchAndSaveTeamMembers(selectedTeamId!!)/users = teamsRepository.getJoinedMembersAndSave(selectedTeamId!!)/' app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
-sed -i '/private suspend fun fetchAndSaveTeamMembers(teamId: String) = withContext(Dispatchers.IO) {/,/    }/d' app/src/main/java/org/ole/planet/myplanet/ui/sync/LoginActivity.kt
