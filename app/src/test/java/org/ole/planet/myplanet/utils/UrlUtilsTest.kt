53c7f182b1a3d23f3a862a509e8923c62f310366
dogi <dogi@users.noreply.github.com>
2026-03-26 05:02:26 -0400

    login: smoother url utils apk version testing (fixes #12427) (#12357)

--- app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
@@ -7,6 +7,60 @@ import org.junit.Test
 import org.ole.planet.myplanet.services.SharedPrefManager
 
 class UrlUtilsTest {
+    @Test
+    fun `getApkVersionUrl uses alternative url`() {
+        val mockSpm = mockk<SharedPrefManager>()
+        every { mockSpm.isAlternativeUrl() } returns true
+        every { mockSpm.getProcessedAlternativeUrl() } returns "http://192.168.1.2:5000"
+
+        val result = UrlUtils.getApkVersionUrl(mockSpm)
+
+        assertEquals("http://192.168.1.2:5000/apkversion", result)
+    }
+
+    @Test
+    fun `getApkVersionUrl removes suffix db and appends apkversion`() {
+        val mockSpm = mockk<SharedPrefManager>()
+        every { mockSpm.isAlternativeUrl() } returns false
+        every { mockSpm.getCouchdbUrl() } returns "http://192.168.1.1:5000/db"
+
+        val result = UrlUtils.getApkVersionUrl(mockSpm)
+
+        assertEquals("http://192.168.1.1:5000/apkversion", result)
+    }
+
+    @Test
+    fun `getApkVersionUrl appends apkversion`() {
+        val mockSpm = mockk<SharedPrefManager>()
+        every { mockSpm.isAlternativeUrl() } returns false
+        every { mockSpm.getCouchdbUrl() } returns "http://192.168.1.1:5000"
+
+        val result = UrlUtils.getApkVersionUrl(mockSpm)
+
+        assertEquals("http://192.168.1.1:5000/apkversion", result)
+    }
+
+    @Test
+    fun `getApkVersionUrl with empty couchdb url returns apkversion`() {
+        val mockSpm = mockk<SharedPrefManager>()
+        every { mockSpm.isAlternativeUrl() } returns false
+        every { mockSpm.getCouchdbUrl() } returns ""
+
+        val result = UrlUtils.getApkVersionUrl(mockSpm)
+
+        assertEquals("/apkversion", result)
+    }
+
+    @Test
+    fun `getApkVersionUrl with alternative url empty returns apkversion`() {
+        val mockSpm = mockk<SharedPrefManager>()
+        every { mockSpm.isAlternativeUrl() } returns true
+        every { mockSpm.getProcessedAlternativeUrl() } returns ""
+
+        val result = UrlUtils.getApkVersionUrl(mockSpm)
+
+        assertEquals("/apkversion", result)
+    }
 
     @Test
     fun testDbUrl_withDbSuffix() {
