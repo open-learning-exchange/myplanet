f6677dc54c9e1e93d9249d40dd9627de66f4b03a
dogi <dogi@users.noreply.github.com>
2026-03-26 05:17:48 -0400

    all: smoother url utils update testing (fixes #12429) (#12355)

--- app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/utils/UrlUtilsTest.kt
@@ -19,6 +19,42 @@ class UrlUtilsTest {
     }
 
     @Test
+    fun `getUpdateUrl should append versions to base url when not alternative`() {
+        val spm = mockk<SharedPrefManager>()
+        every { spm.isAlternativeUrl() } returns false
+        every { spm.getCouchdbUrl() } returns "http://example.com"
+        val result = UrlUtils.getUpdateUrl(spm)
+        assertEquals("http://example.com/versions", result)
+    }
+
+    @Test
+    fun `getUpdateUrl should append versions to base url when alternative`() {
+        val spm = mockk<SharedPrefManager>()
+        every { spm.isAlternativeUrl() } returns true
+        every { spm.getProcessedAlternativeUrl() } returns "http://alt.example.com"
+        val result = UrlUtils.getUpdateUrl(spm)
+        assertEquals("http://alt.example.com/versions", result)
+    }
+
+    @Test
+    fun `getUpdateUrl should remove trailing db before appending versions`() {
+        val spm = mockk<SharedPrefManager>()
+        every { spm.isAlternativeUrl() } returns false
+        every { spm.getCouchdbUrl() } returns "http://example.com/db"
+        val result = UrlUtils.getUpdateUrl(spm)
+        assertEquals("http://example.com/versions", result)
+    }
+
+    @Test
+    fun `getUpdateUrl should remove trailing db from alternative url before appending versions`() {
+        val spm = mockk<SharedPrefManager>()
+        every { spm.isAlternativeUrl() } returns true
+        every { spm.getProcessedAlternativeUrl() } returns "http://alt.example.com/db"
+        val result = UrlUtils.getUpdateUrl(spm)
+        assertEquals("http://alt.example.com/versions", result)
+    }
+
+    @Test
     fun testGetHealthAccessUrl_withEmptyPin() {
         every { sharedPrefManager.getServerPin() } returns ""
         val url = UrlUtils.getHealthAccessUrl(sharedPrefManager)
@@ -53,9 +89,7 @@ class UrlUtilsTest {
         val mockSpm = mockk<SharedPrefManager>()
         every { mockSpm.isAlternativeUrl() } returns true
         every { mockSpm.getProcessedAlternativeUrl() } returns "http://192.168.1.2:5000"
-
         val result = UrlUtils.getApkVersionUrl(mockSpm)
-
         assertEquals("http://192.168.1.2:5000/apkversion", result)
     }
 
@@ -64,9 +98,7 @@ class UrlUtilsTest {
         val mockSpm = mockk<SharedPrefManager>()
         every { mockSpm.isAlternativeUrl() } returns false
         every { mockSpm.getCouchdbUrl() } returns "http://192.168.1.1:5000/db"
-
         val result = UrlUtils.getApkVersionUrl(mockSpm)
-
         assertEquals("http://192.168.1.1:5000/apkversion", result)
     }
 
@@ -75,9 +107,7 @@ class UrlUtilsTest {
         val mockSpm = mockk<SharedPrefManager>()
         every { mockSpm.isAlternativeUrl() } returns false
         every { mockSpm.getCouchdbUrl() } returns "http://192.168.1.1:5000"
-
         val result = UrlUtils.getApkVersionUrl(mockSpm)
-
         assertEquals("http://192.168.1.1:5000/apkversion", result)
     }
 
@@ -86,9 +116,7 @@ class UrlUtilsTest {
         val mockSpm = mockk<SharedPrefManager>()
         every { mockSpm.isAlternativeUrl() } returns false
         every { mockSpm.getCouchdbUrl() } returns ""
-
         val result = UrlUtils.getApkVersionUrl(mockSpm)
-
         assertEquals("/apkversion", result)
     }
 
@@ -97,9 +125,7 @@ class UrlUtilsTest {
         val mockSpm = mockk<SharedPrefManager>()
         every { mockSpm.isAlternativeUrl() } returns true
         every { mockSpm.getProcessedAlternativeUrl() } returns ""
-
         val result = UrlUtils.getApkVersionUrl(mockSpm)
-
         assertEquals("/apkversion", result)
     }
 
