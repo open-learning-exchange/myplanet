12c0ff58e3311ed83b4e9d3f70dbc7c02dfdc891
dogi <dogi@users.noreply.github.com>
2026-03-26 06:08:10 -0400

    courses: smoother repository ids fetching (fixes #12438) (#12378)

--- app/src/main/java/org/ole/planet/myplanet/repository/CoursesRepositoryImpl.kt
+++ app/src/main/java/org/ole/planet/myplanet/repository/CoursesRepositoryImpl.kt
@@ -512,9 +512,7 @@ class CoursesRepositoryImpl @Inject constructor(
                 .findAll()
 
             val ids = JsonArray()
-            for (course in myCourses) {
-                ids.add(course.courseId)
-            }
+            myCourses.asSequence().mapNotNull { it.courseId }.forEach { ids.add(it) }
             ids
         }
     }
