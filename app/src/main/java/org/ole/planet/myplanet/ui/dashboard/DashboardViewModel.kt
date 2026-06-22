c5f42e4d64cc45d592ba9b70d20866fcf9849fe3
dogi <dogi@users.noreply.github.com>
2026-06-22 17:06:34 -0400

    Remove unnecessary blank line in DashboardViewModel

--- app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/dashboard/DashboardViewModel.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-
 import javax.inject.Inject
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
