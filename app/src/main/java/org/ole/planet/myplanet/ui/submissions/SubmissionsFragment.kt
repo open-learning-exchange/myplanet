8b28471be142a3645288d626934265d7eab9e150
dogi <dogi@users.noreply.github.com>
2026-06-25 04:21:52 -0400

    teams: smoother survey submission searching (fixes #14189) (#14115)

--- app/src/main/java/org/ole/planet/myplanet/ui/submissions/SubmissionsFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/submissions/SubmissionsFragment.kt
@@ -16,6 +16,8 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.ole.planet.myplanet.base.BaseRecyclerFragment.Companion.showNoData
 import org.ole.planet.myplanet.databinding.FragmentMySubmissionBinding
@@ -31,6 +33,7 @@ class SubmissionsFragment : Fragment(), CompoundButton.OnCheckedChangeListener {
     lateinit var userSessionManager: UserSessionManager
 
     private lateinit var textWatcher: TextWatcher
+    private var searchJob: Job? = null
     private lateinit var adapter: SubmissionsAdapter
     var type: String? = ""
 
@@ -75,7 +78,11 @@ class SubmissionsFragment : Fragment(), CompoundButton.OnCheckedChangeListener {
         textWatcher = object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-                viewModel.setFilter(type ?: "", charSequence.toString())
+                searchJob?.cancel()
+                searchJob = viewLifecycleOwner.lifecycleScope.launch {
+                    delay(300)
+                    viewModel.setFilter(type ?: "", charSequence.toString())
+                }
             }
             override fun afterTextChanged(editable: Editable) {}
         }
