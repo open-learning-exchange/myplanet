87805a3952afc460183788b2416a43c2dbc843ef
J-S-webskas <shijenny1304@gmail.com>
2026-07-27 13:30:02 -0700

    removed many survey-related code

--- app/src/main/java/org/ole/planet/myplanet/ui/notifications/NotificationsAdapter.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/notifications/NotificationsAdapter.kt
@@ -156,7 +156,6 @@ internal fun iconResFor(type: String): Int = when (type.lowercase()) {
     "join_request" -> R.drawable.ic_join_request
     "team_join" -> R.drawable.ic_activity
     "task" -> R.drawable.ic_date
-    "survey" -> R.drawable.ic_my_survey
     "chat" -> R.drawable.ic_mic
     "voice_reply" -> R.drawable.ic_send
     "resource" -> R.drawable.ic_folder
