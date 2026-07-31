be8a150bb4d00ee0dd45774c636c3f934866ebf3
Viafskaco <shijenny1304@gmail.com>
2026-07-31 08:40:59 -0700

    Update toast message for member removal sync

--- app/src/main/java/org/ole/planet/myplanet/ui/teams/members/MembersFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/teams/members/MembersFragment.kt
@@ -156,8 +156,8 @@ class MembersFragment : BaseTeamFragment() {
                     }
                 }
                 teamsRepository.removeMember(teamId, memberId)
-                teamsSyncRepository.syncTeamActivities() // Sync immediately so that the user is now deleted.
-                Toast.makeText(requireContext(), "When sync is finished, user will be deleted", Toast.LENGTH_LONG).show();
+                teamsSyncRepository.syncTeamActivities() // Sync immediately so that the user is now deleted
+                Toast.makeText(requireContext(), remove_user_from_team_sync_pending, Toast.LENGTH_LONG).show();
                 loadMembers()
                 onMemberChangeListener?.onMemberChanged()
             } catch (e: Exception) {
