b40d668b31b1b0453d0d57b48972ccadc0399ddb
Viafskaco <shijenny1304@gmail.com>
2026-08-14 13:08:22 -0700

    Remove unnecessary blank line before audio player initialization

--- app/src/main/java/org/ole/planet/myplanet/ui/viewer/ResourceViewerFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/viewer/ResourceViewerFragment.kt
@@ -390,7 +390,6 @@ class ResourceViewerFragment : Fragment(), AuthSessionUpdater.AuthCallback {
         Glide.with(this).load(bgRes).into(backgroundImage)
 
         initializeAudioPlayer(playerView)
-
     }
 
     @OptIn(UnstableApi::class)
