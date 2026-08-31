90c0f12e59f79377b746e1d69aa2813724b1ef2e
ragilzakaria <ragil.zakaria@gmail.com>
2026-08-31 18:24:59 +0700

    all: smoother flexbox library migrating

--- app/src/test/java/org/ole/planet/myplanet/data/room/dao/UserDaoTest.kt
+++ app/src/test/java/org/ole/planet/myplanet/data/room/dao/UserDaoTest.kt
@@ -5,6 +5,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
