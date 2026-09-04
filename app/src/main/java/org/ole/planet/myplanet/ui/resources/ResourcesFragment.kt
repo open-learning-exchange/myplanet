4bebe6e62f93f3530d0d840360d344b8f07b9964
ragilzakaria <ragil.zakaria@gmail.com>
2026-09-04 23:23:46 +0700

    resources: smoother collections button opening (fixes #16090)

--- app/src/main/java/org/ole/planet/myplanet/ui/resources/ResourcesFragment.kt
+++ app/src/main/java/org/ole/planet/myplanet/ui/resources/ResourcesFragment.kt
@@ -478,6 +478,7 @@ class ResourcesFragment : BaseRecyclerFragment<MyLibrary?>(), OnLibraryItemSelec
 
     private fun setupCollectionsButton() {
         binding.btnCollections.setOnClickListener {
+            binding.cardFilter.visibility = View.GONE
             val f = CollectionsFragment.getInstance(searchTags, "resources")
             f.setListener(this@ResourcesFragment)
             f.show(childFragmentManager, "")
@@ -873,9 +874,7 @@ class ResourcesFragment : BaseRecyclerFragment<MyLibrary?>(), OnLibraryItemSelec
         binding.root.findViewById<View>(R.id.btn_close_filter)?.setOnClickListener {
             bottomSheet.visibility = View.GONE
         }
-        binding.btnCollections.setOnClickListener {
-            bottomSheet.visibility = View.GONE
-        }
+
         binding.filterCategories.setOnClickListener {
             val f = ResourcesFilterFragment()
             f.setListener(this)
