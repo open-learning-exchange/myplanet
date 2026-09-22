df8678b268bd60611c3ecf66487509675d77af2b
Gideon Okuro <gideonollonde@gmail.com>
2026-09-23 01:00:34 +0300

    all: smoother api allowlist converter providing (fixes #17261) (#17262)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- app/src/main/java/org/ole/planet/myplanet/di/NetworkModule.kt
+++ app/src/main/java/org/ole/planet/myplanet/di/NetworkModule.kt
@@ -8,17 +8,23 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import java.lang.reflect.Modifier
+import java.lang.reflect.Type
 import java.net.InetAddress
 import java.net.Socket
 import java.util.concurrent.TimeUnit
 import javax.inject.Qualifier
 import javax.inject.Singleton
 import javax.net.SocketFactory
+import kotlinx.serialization.json.Json
 import okhttp3.ConnectionPool
 import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import org.ole.planet.myplanet.data.api.ApiInterface
 import org.ole.planet.myplanet.data.api.RetryInterceptor
+import org.ole.planet.myplanet.data.api.AllowlistFactory
+import org.ole.planet.myplanet.model.ChatResponse
+import org.ole.planet.myplanet.model.DocumentResponse
+import org.ole.planet.myplanet.model.MyPlanet
 import org.ole.planet.myplanet.utils.Constants.NETWORK_TRAFFIC_TAG
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -71,6 +77,22 @@ object NetworkModule {
     fun providePlainGson(): Gson {
         return Gson()
     }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            coerceInputValues = true
+        }
+    }
+
+    private val kotlinxHandledTypes: Set<Type> = setOf(
+        MyPlanet::class.java,
+        ChatResponse::class.java,
+        DocumentResponse::class.java
+    )
 
     private const val MAX_REQUESTS_PER_HOST = 20
 
@@ -140,11 +162,13 @@ object NetworkModule {
     @StandardRetrofit
     fun provideStandardRetrofit(
         @StandardHttpClient okHttpClient: OkHttpClient,
-        gson: Gson
+        gson: Gson,
+        json: Json
     ): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://vi.media.mit.edu/")
             .client(okHttpClient)
+            .addConverterFactory(AllowlistFactory(json, kotlinxHandledTypes))
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
